7c5aa798a9844062c00f862e36e0efbfcd67aa11
svn_jspeh <svn_jspeh@e128ff1e-fa79-4151-a8cb-522299a2c450>
2014-12-09 15:07:29 +0000

    adding tutorialInputOutput and changing some indentation in tutorialBSplineCurve...
    git-svn-id: https://ricamsvn.ricam.oeaw.ac.at/gismo/stable@3954 e128ff1e-fa79-4151-a8cb-522299a2c450

---
+++ examples/tutorialInputOutput.cpp
@@ -0,0 +1,108 @@
+/** @file tutorialInputOutput.cpp
+
+    @brief Tutorial on how to use input output facilities of G+Smo library.
+
+    This file is part of the G+Smo library.
+
+    This Source Code Form is subject to the terms of the Mozilla Public
+    License, v. 2.0. If a copy of the MPL was not distributed with this
+    file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+    Author(s): J. Speh
+*/
+
+// For detailed options about visualization look at gsView.cpp example. 
+
+
+
+#include <iostream>
+#include <gismo.h>
+
+using namespace gismo;
+
+
+int main(int argc, char* argv[])
+{
+
+    std::string input("");
+    std::string output("");
+    
+    try
+    {
+        // for information about command line arguments 
+        // look at tutorialCommandLineArg.cpp
+        
+        gsCmdLine cmd("Tutorial Input Output");
+        
+        gsArgValPlain<std::string> inArg("input", "G+Smo input geometry file.", 
+                                         false, "", "file", cmd);
+        
+        gsArgVal<std::string> outArg("o", "output", "Name of the output file.",
+                                     false, "out", "string", cmd);
+
+        cmd.parse(argc, argv);
+        input = inArg.getValue();
+        output = outArg.getValue();
+    }
+    catch (gsArgException& e)
+    {
+        std::cout << "Error: " << e.error() << " " << e.argId() << std::endl;
+        return -1;
+    }
+    
+    if (input == "")
+    {
+        std::cout << "Provide G+Smo input geometry file." << std::endl;
+        return -1;
+    }
+
+    // reading the geometry
+    
+    gsFileData<> fileData(input);
+    
+    gsGeometry<>* pGeom = NULL;
+    if (fileData.has< gsGeometry<> >())
+    {
+        pGeom = fileData.getFirst< gsGeometry<> >();
+    }
+    else
+    {
+        std::cout << "Input file doesn't have a geometry inside." << std::endl;
+        return -1;
+    }
+
+        
+    if (pGeom == NULL)
+    {
+        std::cout << "Didn't find any geometry." << std::endl;
+        return -1;
+    }
+
+
+    
+    // printing the geometry
+
+    std::cout << "The file contains: \n" << *pGeom << std::endl;
+            
+
+    // writing a paraview file
+            
+    const std::string out = output + "Paraview";
+    gsWriteParaview(*pGeom, out);
+    std::cout << "Wrote a paraview file: " << out << std::endl;
+            
+
+    // writing a G+Smo .xml file
+            
+    gsFileData<> fd;
+    fd << *pGeom;
+    fd.dump(output);
+    std::cout << "Wrote G+Smo file: " << output << std::endl;
+    
+
+    delete pGeom;
+    return 0;
+}
+
+
+
