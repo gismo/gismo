d31b719dd08861e3c70526e0bf52ff97c10d0f38
skleiss <skleiss@e128ff1e-fa79-4151-a8cb-522299a2c450>
2014-12-18 23:44:16 +0000

    Bugfix
    git-svn-id: https://ricamsvn.ricam.oeaw.ac.at/gismo/stable@4098 e128ff1e-fa79-4151-a8cb-522299a2c450

--- src/gsHSplines/gsHTensorBasis.h
+++ src/gsHSplines/gsHTensorBasis.h
@@ -751,7 +751,6 @@ public:
 
     virtual void increaseMultiplicity(index_t lvl, int dir, T knotValue, int mult = 1);
 
-
 protected:
 
     /// @brief Updates the basis structure (eg. charact. matrices, etc), to
