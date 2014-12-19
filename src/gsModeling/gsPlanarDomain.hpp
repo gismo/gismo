d4297fec33f8d10e613b3ab3e5541fcbf297f307
svn_mpauley <svn_mpauley@e128ff1e-fa79-4151-a8cb-522299a2c450>
2014-12-19 08:46:07 +0000

    Header bugfix
    git-svn-id: https://ricamsvn.ricam.oeaw.ac.at/gismo/stable@4106 e128ff1e-fa79-4151-a8cb-522299a2c450

--- src/gsModeling/gsPlanarDomain.hpp
+++ src/gsModeling/gsPlanarDomain.hpp
@@ -8,8 +8,7 @@
     License, v. 2.0. If a copy of the MPL was not distributed with this
     file, You can obtain one at http://mozilla.org/MPL/2.0/.
     
-    Author(s): A. Falini, C. Hofreither, A. Mantzaflaris, D. Mayer,
-            D.-M. Nguyen, M. Pauley
+    Author(s): A. Falini, A. Mantzaflaris, D. Mayer, D.-M. Nguyen, M. Pauley
 */
 
 #pragma once
