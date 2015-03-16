4656dd1cbc7475b4f745b30ff231f8497eca0cfb
amantzaflaris <amantzaflaris@e128ff1e-fa79-4151-a8cb-522299a2c450>
2015-03-16 20:49:59 +0000

    small fix
    git-svn-id: https://ricamsvn.ricam.oeaw.ac.at/gismo/stable@4659 e128ff1e-fa79-4151-a8cb-522299a2c450

--- src/gsCore/gsTemplateTools.h
+++ src/gsCore/gsTemplateTools.h
@@ -13,8 +13,6 @@
 
 #pragma once
 
-#include <gsCore/gsExport.h>
-
 namespace gismo
 {
 
@@ -43,9 +41,15 @@ struct choose<false, IsTrue, IsFalse> {
 
 
 /*
-  Macros for instantiating structs, classes or functions
+  Macros for instantiating templated structs, classes or functions
 */
 
-#define STRUCT_TEMPLATE_INST template struct GISMO_EXPORT
-#define CLASS_TEMPLATE_INST  template class  GISMO_EXPORT
-#define TEMPLATE_INST        template        GISMO_EXPORT
+#if defined(_WIN32)
+#define STRUCT_TEMPLATE_INST template struct __declspec(dllexport)
+#define CLASS_TEMPLATE_INST  template class  __declspec(dllexport)
+#define TEMPLATE_INST        template        __declspec(dllexport)
+#else
+#define STRUCT_TEMPLATE_INST template struct
+#define CLASS_TEMPLATE_INST  template class 
+#define TEMPLATE_INST        template       
+#endif
