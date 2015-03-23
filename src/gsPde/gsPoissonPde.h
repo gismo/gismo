c034133f8f22c33a981ee3211c3b3c0cf502a879
amantzaflaris <amantzaflaris@e128ff1e-fa79-4151-a8cb-522299a2c450>
2015-03-23 10:41:26 +0000

    dox fix
    git-svn-id: https://ricamsvn.ricam.oeaw.ac.at/gismo/stable@4703 e128ff1e-fa79-4151-a8cb-522299a2c450

--- src/gsPde/gsPoissonPde.h
+++ src/gsPde/gsPoissonPde.h
@@ -1,3 +1,16 @@
+/** @file gsPoissonPde.h
+
+    @brief Describes a Poisson PDE.
+
+    This file is part of the G+Smo library.
+
+    This Source Code Form is subject to the terms of the Mozilla Public
+    License, v. 2.0. If a copy of the MPL was not distributed with this
+    file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+    Author(s): A. Mantzaflaris
+*/
+
 
 #pragma once
 
@@ -15,6 +28,7 @@ template <class T> class gsFunction;
     function and optionally a known solution.
 
     \ingroup Pde
+    \ingroup pdeclass
  */
 
 template<class T>
