01bf560850cc0b3ce44af639576eed3818436094
amantzaflaris <amantzaflaris@e128ff1e-fa79-4151-a8cb-522299a2c450>
2015-07-03 17:10:11 +0000

    split file
    git-svn-id: https://ricamsvn.ricam.oeaw.ac.at/gismo/stable@5287 e128ff1e-fa79-4151-a8cb-522299a2c450

--- src/gsCore/gsGeometry.h
+++ src/gsCore/gsGeometry.h
@@ -16,45 +16,8 @@
 #include <gsCore/gsFunction.h>
 #include <gsCore/gsBoundary.h>
 
-#include <gsCore/gsCurve.h>
-#include <gsCore/gsSurface.h>
-#include <gsCore/gsVolume.h>
-#include <gsCore/gsBulk.h>
-
-
 namespace gismo
 {
-
-template <unsigned d, typename T>
-struct gsGeoTraits
-{
-    typedef gsGeometry<T> GeometryBase;
-};
-
-template <typename T>
-struct gsGeoTraits<1,T>
-{
-    typedef gsCurve<T> GeometryBase;
-};
-
-template <typename T>
-struct gsGeoTraits<2,T>
-{
-    typedef gsSurface<T> GeometryBase;
-};
-
-template <typename T>
-struct gsGeoTraits<3,T>
-{
-    typedef gsVolume<T> GeometryBase;
-};
-
-template <typename T>
-struct gsGeoTraits<4,T>
-{
-    typedef gsBulk<T> GeometryBase;
-};
-
 
 /** 
     \brief
@@ -530,70 +493,10 @@ template<class T>
 std::ostream &operator<<(std::ostream &os, const gsGeometry<T>& b)
 {return b.print(os); };
 
-
-////////////////////////////////////////////////////////////////////////////////
-
-
-/** \brief  Base class for geometries with statically known basis types.
- *
- * This is an internal implementation class which all concrete geometries
- * derive from. It allows the compile-time specification of the associated
- * basis type. Note that the basis dimension is also statically known.
- *
- * \tparam Basis_t  type of the basis for this geometry
- *
- *
- * \ingroup Core
- */
-template<class Basis_t>
-class gsGenericGeometry : 
-        public gsGeoTraits<Basis_t::Dim, 
-                           typename Basis_t::Scalar_t>::GeometryBase
-{
-public: 
-    typedef typename Basis_t::Scalar_t Scalar_t;
-
-    typedef typename gsGeoTraits<Basis_t::Dim,Scalar_t>::GeometryBase Base;
-
-public:
-
-    // Look at gsGeometry base constructor for a brief description
-    gsGenericGeometry() : Base() { }
-
-    // Look at gsGeometry base constructor for a brief description
-    gsGenericGeometry(const Basis_t & basis, const gsMatrix<Scalar_t> & coefs )
-    : Base (basis,coefs)
-    {
-        //todo: gsBasis as constructor argument, dynamic cast in assertion
-
-        GISMO_ASSERT( basis.size() == coefs.rows(), 
-                      "The coefficient matrix of the geometry (rows="<<coefs.rows()<<") does not match the number of basis functions in its basis("<< basis.size() <<").");
-    }
-    
-    // Look at gsGeometry base constructor for a brief description
-    gsGenericGeometry(const Basis_t & basis, gsMovable< gsMatrix<Scalar_t> > coefs )
-    : Base (basis,coefs)
-    { 
-        GISMO_ASSERT( basis.size() == this->m_coefs.rows(), 
-                      "The coefficient matrix of the geometry (rows="<<this->m_coefs.rows()<<") does not match the number of basis functions in its basis("<< basis.size() <<").");
-    }
-        
-public:
-    
-    // for rational bases, we WILL store the coefficients in projective form later
-    bool isProjective() const
-    { return Basis_t::IsRational; }
-    
-    // Look at gsGeometry base constructor for a brief description
-    Basis_t & basis()       { return static_cast<Basis_t&>(*this->m_basis); }
-    
-    // Look at gsGeometry base constructor for a brief description
-    const Basis_t & basis() const { return static_cast<const Basis_t&>(*this->m_basis); }
-    
-}; // class gsGenericGeometry
-
-
 } // namespace gismo
+
+
+#include <gsCore/gsGenericGeometry.h>
 
 
 #ifndef GISMO_BUILD_LIB
