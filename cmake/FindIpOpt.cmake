d953ce738ca587fe6c06abdbb11a02f2264d9923
amantzaflaris <amantzaflaris@e128ff1e-fa79-4151-a8cb-522299a2c450>
2015-08-07 19:05:56 +0000

    typo
    git-svn-id: https://ricamsvn.ricam.oeaw.ac.at/gismo/stable@5631 e128ff1e-fa79-4151-a8cb-522299a2c450

--- cmake/FindIpOpt.cmake
+++ cmake/FindIpOpt.cmake
@@ -1,9 +1,9 @@
 
-  find_path(IPOPT_INCLUDE_DIR NAMES IpTNLP.hpp HINTS /usr/include/coin ${CMAKE_CURRENT_BINARY_DIR}/IpOpt-prefix/include/coin ${IpOpt_DIR}/include/coin)
+find_path(IPOPT_INCLUDE_DIR NAMES IpTNLP.hpp HINTS /usr/include/coin ${CMAKE_CURRENT_BINARY_DIR}/IpOpt-prefix/include/coin ${IpOpt_DIR}/include/coin)
 
-  find_library(IPOPT_LIBRARY NAMES ipopt libipopt HINTS ${CMAKE_BINARY_DIR}/lib ${IpOpt_DIR}/lib ${IpOpt_DIR}/lib64 ${IpOpt_DIR}/Ipopt/src/Interfaces/.libs)
+find_library(IPOPT_LIBRARY NAMES ipopt libipopt HINTS ${CMAKE_BINARY_DIR}/lib ${IpOpt_DIR}/lib ${IpOpt_DIR}/lib64 ${IpOpt_DIR}/Ipopt/src/Interfaces/.libs)
 
-libcoinblas.laif(IPOPT_INCLUDE_DIR AND IPOPT_LIBRARY)
+if(IPOPT_INCLUDE_DIR AND IPOPT_LIBRARY)
 	get_filename_component(IPOPT_LIBRARY_DIR ${IPOPT_LIBRARY} PATH)
    set(IPOPT_FOUND TRUE)
 endif()
@@ -16,7 +16,7 @@ if(IPOPT_FOUND)
    endif()
 elseif(NOT IPOPT_FOUND)
    set(IpOpt_DIR "IpOpt_DIR-NOTFOUND " CACHE PATH "The path to the IPOPT library")
-   if(IPOPT_FIND_REQUIRED) ## TO DO, fix
+   if(IPOPT_FIND_REQUIRED)
       message("Could not find IpOpt library.")
       message(FATAL_ERROR "Set the variable IpOpt_DIR and try again.")
    endif()
