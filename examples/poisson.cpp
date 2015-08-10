5b478d7824bfd34622f9ecf5d75c5183227e1a34
amantzaflaris <amantzaflaris@e128ff1e-fa79-4151-a8cb-522299a2c450>
2015-08-10 17:33:58 +0000

    update I/O poisson.cpp
    git-svn-id: https://ricamsvn.ricam.oeaw.ac.at/gismo/stable@5653 e128ff1e-fa79-4151-a8cb-522299a2c450

--- examples/poisson.cpp
+++ examples/poisson.cpp
@@ -1,155 +1,150 @@
-// example solving the poisson equation with gismo
+/** @file poisson.cpp
 
-#include <iostream>
-#include <ctime>
+    @brief Reads the data for a Poisson boundary value problem
+     (coefficients, domain, boundary conditions) from an XML file and
+     solves the BVP
+
+    This file is part of the G+Smo library.
+
+    This Source Code Form is subject to the terms of the Mozilla Public
+    License, v. 2.0. If a copy of the MPL was not distributed with this
+    file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+    Author(s): A. Mantzaflaris
+*/
 
 #include <gismo.h>
 
-
 using namespace gismo;
-using std::cout;
-using  std::endl;
 
-// Getting the input (defined after the main function)
-bool parse_input( int argc, char *argv[], int & numRefine, int & numElevate,  
-                  int & Dirichlet, bool & plot, int & plot_pts, 
-                  gsMultiPatch<> * & geo, gsPoissonPde<> * & ppde);
-
+bool read_input( int argc, char *argv[], int & numRefine, int & numElevate,  
+                 int & Dirichlet, bool & plot, int & plot_pts, 
+                 gsMultiPatch<> & geo, gsPoissonPde<> * & ppde,
+                 gsBoundaryConditions<> & BCs);
 
 int main(int argc, char *argv[])
 {
 
-  /////////////////// Input ///////////////////
-  int numRefine;  // defaults to 2
-  int numElevate; // defaults to -1
-  int Dirichlet;  // defaults to 0
-  bool plot;      // defaults to false
-  int plot_pts;   // defaults to 1000
-  gsMultiPatch<> * geo ; // defaults to BSplineCube
-  gsPoissonPde<> * ppde ;
-  
-  bool success = parse_input(argc, argv, numRefine, numElevate, Dirichlet, 
-                             plot, plot_pts, geo, ppde);
-  if ( ! success )
-  {
-      cout<<"Input failed, quitting.\n";
-      return 1;
-  }
-  /////////////////// Print info ///////////////////
-  cout<<"Type "<< argv[0]<< " -h, to get the list of command line options.\n\n";
-  cout<<"Domain: "<< *geo <<"\n";
-  cout<<"p-refinent steps before solving: "<< numElevate <<"\n";  
-  cout<<"h-refinent steps before solving: "<< numRefine <<"\n";  
+    /////////////////// Input ///////////////////
+    int numRefine;  // defaults to 2
+    int numElevate; // defaults to -1
+    int Dirichlet;  // defaults to 0
+    bool plot;      // defaults to false
+    int plot_pts;   // defaults to 1000
+    gsMultiPatch<> geo ; // defaults to BSplineCube
+    gsPoissonPde<> * ppde ;
+    gsBoundaryConditions<> BCs;
 
-  cout<< * ppde <<"\n"; 
-
-  /////////////////// Setup boundary value problem ///////////////////  
-  // Boundary conditions
-  gsBoundaryConditions<> BCs;
-  // Create Dirichlet boundary conditions for all boundaries
-    for (gsMultiPatch<>::const_biterator 
-             bit = geo->bBegin(); bit != geo->bEnd(); ++bit)
+    bool success = read_input(argc, argv, numRefine, numElevate, Dirichlet, 
+                              plot, plot_pts, geo, ppde, BCs);
+    if ( ! success )
     {
-        BCs.addCondition( *bit, condition_type::dirichlet, ppde->solution() );
+        gsWarn <<"Input reading failed, quitting.\n";
+        return 1;
     }
+    /////////////////// Print info ///////////////////
+    gsInfo<<"Type "<< argv[0]<< " -h, to get the list of command line options.\n\n";
+    gsInfo<<"Domain: "<< geo <<"\n";
+    gsInfo<<"p-refinent steps before solving: "<< numElevate <<"\n";  
+    gsInfo<<"h-refinent steps before solving: "<< numRefine <<"\n";  
+
+    gsInfo<< * ppde <<"\n"; 
+
+    gsMultiBasis<> bases(geo);
   
-    gsMultiBasis<> bases(*geo);
-  
-  // Elevate and refine the solution space
-  if ( numElevate > -1 )
-  {
-      // get maximun degree
-      int tmp = bases.maxDegree(0);
+    // Elevate and refine the solution space
+    if ( numElevate > -1 )
+    {
+        // get maximun degree
+        int tmp = bases.maxDegree(0);
       
-      // Elevate all degrees uniformly
-      tmp += numElevate;
-      for (size_t j = 0; j < bases.nBases(); ++j )
-              bases[j].setDegree(tmp);
-  }
+        // Elevate all degrees uniformly
+        tmp += numElevate;
+        for (size_t j = 0; j < bases.nBases(); ++j )
+            bases[j].setDegree(tmp);
+    }
 
-  for (size_t j = 0; j < bases.nBases(); ++j )
-      for (int i = 0; i < numRefine; ++i)
-          bases[j].uniformRefine();
+    for (size_t j = 0; j < bases.nBases(); ++j )
+        for (int i = 0; i < numRefine; ++i)
+            bases[j].uniformRefine();
   
-  cout << "Discret. Space 0: "<< bases[0] << endl;
-
+    gsInfo << "Discret. Space 0: "<< bases[0] << "\n";
+    gsInfo << BCs<<"\n";
   
-  /////////////////// Setup solver ///////////////////
-  gsPoissonAssembler<real_t> poisson(*geo,bases,BCs,*ppde->rhs(),
-                                     ( Dirichlet==1 ? dirichlet::nitsche 
-                                       : dirichlet::elimination) );
+    /////////////////// Setup solver ///////////////////
+    gsPoissonAssembler<real_t> poisson(geo, bases, BCs, *ppde->rhs(),
+                                       ( Dirichlet==1 ? dirichlet::nitsche 
+                                         : dirichlet::elimination) );
 
-  cout<<"System size: "<< poisson.numDofs() << endl;
+    gsInfo<<"System size: "<< poisson.numDofs() << "\n";
   
-  // Assemble and solve
-  gsStopwatch time;
-  poisson.assemble();
-  const double assmTime = time.stop();
-  gsSparseSolver<>::CGDiagonal solver( poisson.matrix() );
-  gsMatrix<> solVector = solver.solve( poisson.rhs() );
-  const double solveTime = time.stop();
-  gsField<>* x = poisson.constructSolution(solVector);
+    // Assemble and solve
+    gsStopwatch time;
+    poisson.assemble();
+    const double assmTime = time.stop();
+    gsSparseSolver<>::CGDiagonal solver( poisson.matrix() );
+    gsMatrix<> solVector = solver.solve( poisson.rhs() );
+    const double solveTime = time.stop();
+    gsField<>* x = poisson.constructSolution(solVector);
 
-  //cout <<  poisson.linearSystem() <<"\n";
+    //gsInfo <<  poisson.linearSystem() <<"\n";
 
-  cout << "Assembling time: " <<  assmTime << " s" << "\n";    
-  cout << "Solver time:     " <<  solveTime - assmTime << " s" << "\n";    
-  cout << "Total time:      " <<  solveTime << " s" << "\n";    
-  cout << "Solution: " << x->function()  << endl;
-  if ( ppde->solution() )
-      cout << "L2 error: " << x->distanceL2(*ppde->solution() ) 
-           << endl;
+    gsInfo << "Assembling time: " <<  assmTime << " s" << "\n";    
+    gsInfo << "Solver time:     " <<  solveTime - assmTime << " s" << "\n";    
+    gsInfo << "Total time:      " <<  solveTime << " s" << "\n";    
+    gsInfo << "Solution: " << x->function()  << "\n";
+    if ( ppde->solution() )
+        gsInfo << "L2 error: " << x->distanceL2(*ppde->solution() ) 
+             << "\n";
 
-  // Optionally plot solution in paraview
-  if (plot)
-  {
-      cout<<"Plotting in Paraview..." << endl;
-      gsWriteParaview<>( *x, "poisson_sol" , plot_pts) ;
+    // Optionally plot solution in paraview
+    if (plot)
+    {
+        gsInfo<<"Plotting in Paraview..." << "\n";
+        gsWriteParaview<>( *x, "poisson_sol" , plot_pts) ;
       
-      //Plot exact solution in paraview
-      gsField<> exact( *geo , *ppde->solution() , false ) ;
-      gsWriteParaview<>( exact, "poisson_sol_exact", plot_pts) ;
-  }
+        //Plot exact solution in paraview
+        gsField<> exact( geo , *ppde->solution() , false ) ;
+        gsWriteParaview<>( exact, "poisson_sol_exact", plot_pts) ;
+    }
 
-  delete x;
-  delete geo;
-  delete ppde;
+    delete x;
+    delete ppde;
 
-  return ( plot ? system("paraview poisson_sol.pvd&") : 0 ); 
+    return ( plot ? system("paraview poisson_sol.pvd&") : 0 ); 
 }
 
-
-bool parse_input( int argc, char *argv[], int & numRefine, int & numElevate,  
-                  int & Dirichlet, bool & plot, int & plot_pts, 
-                  gsMultiPatch<> *& geo, gsPoissonPde<> *& ppde)
+bool read_input( int argc, char *argv[], 
+                 int & numRefine, // opts
+                 int & numElevate, // opts  
+                 int & Dirichlet, // opts
+                 bool & plot, int & plot_pts, //args
+                 gsMultiPatch<> & geo, 
+                 gsPoissonPde<> *& ppde,
+                 gsBoundaryConditions<> & BCs)
 {
-  std::string fn_pde("");
-  bool arg_dirich = false;
-  plot = false;
-  plot_pts = 1000;
-  numElevate = -1;
-  numRefine = 2;
-  std::string fn_basis("");
-  std::string fn("");
+    bool arg_dirich = false;
+    plot = false;
+    plot_pts = 1000;
+    numElevate = -1;
+    numRefine = 2;
+    std::string fn( GISMO_DATA_DIR "/planar/two_squares.xml");
   
-  gsCmdLine cmd("Solves Poisson's equation with an isogeometric discretization.");
-  cmd.addString("p", "pde", "File containing a poisson PDE (.xml)", fn_pde);
-  cmd.addSwitch("nitsche", "Use the Nitsche's method for Dirichlet sides", arg_dirich);
-  cmd.addSwitch("plot", "Plot result in ParaView format", plot);
-  cmd.addInt("s", "plotSamples", "Number of sample points to use for plotting", plot_pts);
-  cmd.addInt("e", "degreeElevation", "Number of degree elevation steps to perform before solving (0: equalize degree in all directions)",
-             numElevate);
-  cmd.addInt("r", "uniformRefine", "Number of Uniform h-refinement steps to perform before solving", 
-             numRefine);
-  cmd.addString("b", "basis", "File containing basis for discretization (.xml)", 
-                fn_basis); 
-  cmd.addString("g", "geometry", "File containing Geometry (.xml, .axl, .txt)",fn);
-  
-    
+    gsCmdLine cmd("Solves Poisson's equation with an isogeometric discretization.");
+    cmd.addSwitch("nitsche", "Use the Nitsche's method for Dirichlet sides", arg_dirich);
+    cmd.addSwitch("plot", "Plot result in ParaView format", plot);
+    cmd.addInt("s", "plotSamples", "Number of sample points to use for plotting", plot_pts);
+    cmd.addInt("e", "degreeElevation", "Number of degree elevation steps to perform before solving (0: equalize degree in all directions)",
+               numElevate);
+    cmd.addInt("r", "uniformRefine", "Number of Uniform h-refinement steps to perform before solving", 
+               numRefine);
+    cmd.addString("g", "geometry", "File containing Geometry (.xml, .axl, .txt)", fn);
+      
     bool ok = cmd.getValues(argc,argv);
-    if (!ok) {
-      cout << "Error during parsing!";
-      return 0;
+    if (!ok) 
+    {
+        gsWarn << "Error during parsing!";
+        return 0;
     }
     
     if ( arg_dirich )
@@ -157,81 +152,44 @@ bool parse_input( int argc, char *argv[], int & numRefine, int & numElevate,
     else
         Dirichlet  = 0;
 
-    if ( ! fn_basis.empty() )
-	{
-        cout << "basis reading not implemented.\n"; 
-	}
-
     if (numRefine<0)
     { 
-      cout << "Number of refinements must be non-negative, setting to zero.\n"; 
-      numRefine = 0;
-    }
-    if (numElevate<-1)
-    { 
-      cout << "Number of elevations must be non-negative, ignoring parameter.\n"; 
-      numElevate = -1;
+        gsWarn << "Number of refinements must be non-negative, setting to zero.\n"; 
+        numRefine = 0;
     }
 
-    if ( fn_pde.empty() )
-	{
-	    fn_pde = GISMO_DATA_DIR;
-	    if ( !fn.empty() )
-		{
-		    geo = gsReadFile<>( fn );
-		    if ( ! geo )
-		      {
-			gsWarn<< "Did not find any geometry in "<<fn<<", quitting.\n";
-			return false;
-		      }
-		    switch ( geo->geoDim() )
-			{
-			case 1:
-			    fn_pde+="/pde/poisson1d_sin.xml";
-			    break;
-			case 2:
-			    fn_pde+="/pde/poisson2d_sin.xml";
-			    break;
-			case 3:
-			    fn_pde+="/pde/poisson3d_sin.xml";
-			    break;
-			default:
-			    return false;
-			}
-		}
-	    else
-            fn_pde+="/pde/poisson2d_sin.xml";
-	}
-    ppde = gsReadFile<>(fn_pde);
-    if ( !ppde )
-      {
-	gsWarn<< "Did not find any PDE in "<< fn<<", quitting.\n";
-	return false;
-      }
-    
-    if ( fn.empty() )
-	{
-	  fn = GISMO_DATA_DIR;
-      switch ( ppde->m_compat_dim )
-	    {
-	    case 1:
-	      fn+= "domain1d/bspline1d_01.xml";
-	      break;
-	    case 2:
-	      fn+= "domain2d/square.xml";
-	      break;
-	    case 3:
-	      fn+= "volumes/cube.xml";
-	      break;
-	    default:
-	      return false;
-	    }
-	}
-    geo = gsReadFile<>( fn );
-    if ( !geo )
-      {
-	cout << "Did not find any geometries in "<< fn<<", quitting.\n";
-	return false;
-      }
-  return true;
+    if (numElevate<-1)
+    { 
+        gsWarn << "Number of elevations must be non-negative, ignoring parameter.\n"; 
+        numElevate = -1;
+    }
+
+    // Load all data from the XML file
+    gsFileData<> xmlData(fn);
+
+    // Check if all needed data exist in the file
+    if ( !xmlData.has< gsMultiPatch<> >() )
+    {
+        gsWarn <<"Did not find a multipatch domain in this file.\n";  
+        return false;
+    }
+
+    if ( !xmlData.has<gsPoissonPde<> >() )
+    {
+        gsWarn <<"Did not find a poisson PDE domain in this file.\n"; 
+        return false;
+    }
+
+    if ( !xmlData.has<gsBoundaryConditions<> >() )
+    {
+        gsWarn <<"Did not find boundary conditions in this this file.\n"; 
+        // default to homogeneous dirichlet ?
+        return false;
+    }
+
+    ppde = xmlData.getFirst<gsPoissonPde<> >();
+    xmlData.getFirst(geo);
+    xmlData.getFirst(BCs);
+
+    return true;
 }
