edc712073474526b8c50789e8dee7176ac05ec02
amantzaflaris <amantzaflaris@e128ff1e-fa79-4151-a8cb-522299a2c450>
2015-09-07 12:06:54 +0000

    better config for pardiso
    git-svn-id: https://ricamsvn.ricam.oeaw.ac.at/gismo/stable@5802 e128ff1e-fa79-4151-a8cb-522299a2c450

--- cmake/FindPardiso.cmake
+++ cmake/FindPardiso.cmake
@@ -1,6 +1,11 @@
 
-find_library(PARDISO_LIBRARY NAMES pardiso500-INTEL1301-X86-64 pardiso500-GNU481-X86-64 pardiso500-GNU472-X86-64 pardiso500-MACOS-X86-64 libpardiso500-WIN-X86-64
+
+if (CMAKE_CXX_COMPILER_ID MATCHES "Intel")
+   find_library(PARDISO_LIBRARY NAMES pardiso500-INTEL1301-X86-64 pardiso412-INTEL120-X86-64 pardiso411-INTEL101-X86-64 HINTS ${CMAKE_BINARY_DIR}/lib ${Pardiso_DIR} ${Pardiso_DIR}/lib)
+else()
+   find_library(PARDISO_LIBRARY NAMES pardiso500-GNU481-X86-64 pardiso500-GNU472-X86-64 pardiso412-GNU450-X86-64 pardiso412-GNU430-X86-64 pardiso411-GNU443-X86-64 pardiso500-MACOS-X86-64 libpardiso500-WIN-X86-64 libpardiso412-WIN-X86-64 libpardiso412-WIN-X86
              HINTS ${CMAKE_BINARY_DIR}/lib ${Pardiso_DIR} ${Pardiso_DIR}/lib)
+endif()
 
 if(PARDISO_LIBRARY)
 	get_filename_component(PARDISO_LIBRARY_DIR ${PARDISO_LIBRARY} PATH)
