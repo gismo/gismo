ab6137dd07b20c79a8c64c2696fa6dff14f02004
Harald Timeraider <timeraider@gmx.at>
2015-10-13 14:32:50 +0200

    refactored setting of data_dir to external file

---
+++ cmake/gsDataDir.cmake
@@ -0,0 +1,16 @@
+### gsDataDir.cmake ---
+## 
+## set GISMO_DATA_DIR based on compiler-toolchain
+## 
+## Author: Harald Weiner
+## Copyright (C) 2015 - RICAM-Linz.
+######################################################################
+
+# Data directory
+if (CMAKE_CXX_COMPILER MATCHES ".*em[+][+]")
+	set(GISMO_DATA_DIR "filedata/" )
+	message("GISMO_DATA_DIR set to relative path '${GISMO_DATA_DIR}'")
+	set(GISMO_ORIG_DATA_DIR "${PROJECT_SOURCE_DIR}/filedata/") 
+else()
+	set (GISMO_DATA_DIR   "${PROJECT_SOURCE_DIR}/filedata/") 
+endif()
