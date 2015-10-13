4fc2670431e7baa3ea069de25ac7e6480b7cf1ad
Harald Timeraider <timeraider@gmx.at>
2015-10-13 14:58:19 +0200

    update of gsDataDir

--- cmake/gsDataDir.cmake
+++ cmake/gsDataDir.cmake
@@ -7,10 +7,4 @@
 ######################################################################
 
 # Data directory
-if (CMAKE_CXX_COMPILER MATCHES ".*em[+][+]")
-	set(GISMO_DATA_DIR "filedata/" )
-	message("GISMO_DATA_DIR set to relative path '${GISMO_DATA_DIR}'")
-	set(GISMO_ORIG_DATA_DIR "${PROJECT_SOURCE_DIR}/filedata/") 
-else()
-	set (GISMO_DATA_DIR   "${PROJECT_SOURCE_DIR}/filedata/") 
-endif()
+set (GISMO_DATA_DIR   "${PROJECT_SOURCE_DIR}/filedata/") 
