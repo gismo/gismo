7c679404f7abb42b3a52e07bb98eaaeddd7fc319
svn_kbirner <svn_kbirner@e128ff1e-fa79-4151-a8cb-522299a2c450>
2016-01-11 12:15:48 +0000

    change for rational numbers
    git-svn-id: https://ricamsvn.ricam.oeaw.ac.at/gismo/stable@6547 e128ff1e-fa79-4151-a8cb-522299a2c450

--- src/gsNurbs/gsBSplineAlgorithms.h
+++ src/gsNurbs/gsBSplineAlgorithms.h
@@ -283,7 +283,7 @@ void degreeElevateBSpline(Basis_t &basis,
     gsVector<T> factor = gsVector<T>::Ones(p+1);
     for(int j=0; j<=p; j++)
         for(int l=1; l<=j; l++)
-            factor[j] *= static_cast<double>((p+1-l)) / (p_new+1-l);
+            factor[j] *= static_cast<T>((p+1-l)) / (p_new+1-l);
 
     //set known coefficients
     // for(int j=0; j<=p; ++j)
