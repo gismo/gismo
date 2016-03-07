c8a8c3f5ca617a1ea64f0ad4dbfe3e8c212b2a57
amantzaflaris <amantzaflaris@e128ff1e-fa79-4151-a8cb-522299a2c450>
2016-03-07 08:36:49 +0000

    add one more instance
    git-svn-id: https://ricamsvn.ricam.oeaw.ac.at/gismo/stable@6888 e128ff1e-fa79-4151-a8cb-522299a2c450

--- src/gsTensor/gsGridIterator.h
+++ src/gsTensor/gsGridIterator.h
@@ -54,7 +54,10 @@ public:
 public:
 
     gsGridIterator()
-    { GISMO_STATIC_ASSERT(std::numeric_limits<Z>::is_integer,INCONSISTENT_INSTANTIZATION); }
+    {
+        GISMO_STATIC_ASSERT(std::numeric_limits<Z>::is_integer,INCONSISTENT_INSTANTIZATION);
+        GISMO_STATIC_ASSERT(mode > -1 && mode<3, INCONSISTENT_INSTANTIZATION);
+    }
             
     gsGridIterator(point const & a, point const & b, bool open = true)
     { reset(a, b, open); }
@@ -65,6 +68,7 @@ public:
     gsGridIterator(gsMatrix<Z,d,2> const & ab, bool open = true)
     { reset(ab.col(0), ab.col(1), open); }
 
+    
     inline void reset(point const & a, point const & b, bool open = true)
     {
         GISMO_ASSERT(a.rows() == b.rows(), "Invalid endpoint dimensions");
@@ -88,11 +92,11 @@ public:
     
     inline gsGridIterator & operator++()
     {
-        // Multiple implementation according to mode
+        // Multiple implementations according to mode
         switch (mode)
         {
-        case 0: // ----------- Iteration over [m_low, m_upp)]
-        case 2: // iteration over vertices of the cube [m_low, m_upp)]
+        case 0: // ----------- Iteration over [m_low, m_upp]
+        case 2: // iteration over vertices of the cube [m_low, m_upp]
             for (int i = 0; i != (d==-1?m_dim:d); ++i)
                 if ( m_cur[i] != m_upp[i] )
                 {
@@ -104,7 +108,7 @@ public:
             m_dim = 0;//done
             return *this;
 
-        case 1: // ----------- Iteration over boundary of [m_low, m_upp)]
+        case 1: // ----------- Iteration over boundary of [m_low, m_upp]
             for (int i = 0; i != (d==-1?m_dim:d); ++i)
             {        
                 if ( m_cur[i] != m_upp[i] )
@@ -132,9 +136,6 @@ public:
             }
             m_dim = 0;//done
             return *this;
-
-        default:
-            GISMO_STATIC_ASSERT(mode > -1 && mode<3, INCONSISTENT_INSTANTIZATION);
         }
     }
 
@@ -149,8 +150,27 @@ public:
             (m_cur.array() == m_upp.array()).any() ;
     }
     
-    point numPoints() const {return m_upp - m_low;}
+    index_t numPoints() const
+    {
+        switch (mode)
+        {
+        case 0: return ((m_upp-m_low).array() + 1).prod();
+        case 1: return ((m_upp-m_low).array() + 1).prod() - ((m_upp-m_low).array()-1).prod();
+        case 2: return ( 1 << m_low.rows() );
+        }
+    }
 
+    point numPointsCwise() const { return (m_upp-m_low).array() + 1;}
+
+    point strides() const
+    {
+        point res(m_low.rows());
+        res[0] = 1;
+        for (index_t i = 1; i != res.size(); ++i )
+            res[i] = res[i-1] * ( m_upp[i-1] - m_low[i-1] + 1 );
+        return res;
+    }
+    
 private:
     
     point m_low, m_upp;
@@ -164,15 +184,21 @@ private:
 /** 
     \brief Iterator over uniformly distributed numeric points inside a
     (hyper-)cube.
+
+    - mode = 0 : iteration over uniform samples in [a, b]
+    - mode = 1 : iteration over uniform samples on the boundry points [a, b]
+    - mode = 2 : iteration over the vertices [a, b]
+
+    \ingroup Tensor
 */
-template<class T, int d>
-class gsGridIterator<T,d,0,false>
-{   // note: the iteration is over the volume of [a,b]
+template<class T, int d, int mode>
+class gsGridIterator<T,d,mode,false>
+{
 public:
 
     typedef gsVector<T,d> point;
 
-    typedef gsGridIterator<index_t, d, 0> integer_iterator;
+    typedef gsGridIterator<index_t, d, mode> integer_iterator;
 
     typedef typename integer_iterator::point point_index;
 public:
@@ -183,6 +209,7 @@ public:
     : m_iter(np, 1)
     {
         reset(a, b);
+        GISMO_STATIC_ASSERT(mode > -1 && mode<3, INCONSISTENT_INSTANTIZATION);
     }
 
     gsGridIterator(gsMatrix<T,2> const & ab, 
@@ -212,10 +239,9 @@ public:
     void reset(point const & a, 
                point const & b)
     {
-        GISMO_ASSERT( (m_iter.numPoints().array()>0).all(), "Number of points is zero.");
         m_cur = m_low = a;
         m_upp = b;
-        m_step = (b-a).array() / (m_iter.numPoints().array() - 1)
+        m_step = (b-a).array() / (m_iter.numPointsCwise().array() - 1)
             .matrix().cwiseMax(1).template cast<T>().array() ;
         m_iter.reset();
     }
@@ -230,17 +256,15 @@ public:
     inline gsGridIterator & operator++()
     {
         if ( ++m_iter )
+        {
             for (int i = 0; i != (d==-1?m_low.size():d); ++i)
                 if ( m_iter.isFloor(i) )
                     m_cur.at(i) = m_low[i]; // avoid numerical error at first val
+                else if ( m_iter.isCeil(i) )
+                    m_cur.at(i) = m_upp[i]; // avoid numerical error at last val
                 else
-                {
-                    if ( m_iter.isCeil(i) )
-                        m_cur.at(i) = m_upp[i]; // avoid numerical error at last val
-                    else
-                        m_cur.at(i) = m_low[i] + m_iter->at(i) * m_step[i];
-                }
-        
+                    m_cur.at(i) = m_low[i] + m_iter->at(i) * m_step[i];
+        }
         return *this;        
     }
     
@@ -252,9 +276,9 @@ public:
     
     const point & step() const {return m_step;}
     
-    index_t size() const {return numPoints().prod();}
+    index_t numPoints() const {return m_iter.numPoints();}
 
-    point_index numPoints() const {return m_iter.numPoints();}
+    point_index numPointsCwise() const {return m_iter.numPointsCwise();}
 
     inline bool isFloor(int i) const { return m_iter.isFloor(i);}
     
@@ -269,6 +293,78 @@ private:
     point  m_low, m_upp, m_step;
 
     integer_iterator m_iter;    
+    gsMatrix<T>      m_cur;
+};
+
+
+/** 
+    \brief Iterator over a Cartesian product of points.
+
+    \ingroup Tensor
+*/
+template<class T, int d>
+class gsGridIterator<T,d,3,false>
+{
+public:
+
+    typedef gsGridIterator<index_t, d, 0> integer_iterator;
+
+    typedef typename integer_iterator::point point_index;
+
+    typedef std::vector<gsMatrix<T> > CwiseContainer;
+    
+public:
+    
+    gsGridIterator(const CwiseContainer & cwise)
+    : m_cwise(cwise)
+    {
+        gsVector<index_t,d> npts(cwise.size());
+        for (index_t i = 0; i != (d!=-1?d:npts.rows()); ++i)
+        {
+            npts[i] = cwise[i].rows() - 1;
+            GISMO_ASSERT(cwise[i].cols()==1 || cwise[i].rows()==1, "Invalid input");
+        }
+        m_iter = integer_iterator(npts, 0);
+        m_cur.derived().resize(cwise.size());
+        update();
+    }
+
+    void reset() { m_iter.reset(); update();}
+
+public:
+
+    operator bool() const {return m_iter;}
+    
+    inline gsGridIterator & operator++()
+    {
+        if (++m_iter) update();
+        return *this;
+    }
+    
+    const gsMatrix<T> & operator*() const {return m_cur;}
+
+    const gsMatrix<T> * operator->() const {return &m_cur;}
+
+    const point_index & tensorIndex() const { return *m_iter;}
+    
+    index_t numPoints() const {return m_iter.numPoints();}
+    
+    const integer_iterator & index_iterator() const { return m_iter;}
+
+private:
+
+    inline void update()
+    {
+        for (int i = 0; i != (d==-1?m_cur.rows():d); ++i)
+            m_cur.at(i) = m_cwise[i].at(m_iter->at(i));
+    }
+
+private:
+
+    const CwiseContainer & m_cwise;
+    
+    integer_iterator m_iter;    
+
     gsMatrix<T>      m_cur;
 };
 
