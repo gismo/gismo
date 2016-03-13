79096cb26fc126fcd951a44ba95046399dfbd14a
amantzaflaris <amantzaflaris@e128ff1e-fa79-4151-a8cb-522299a2c450>
2016-03-13 14:51:19 +0000

    testing extern template
    git-svn-id: https://ricamsvn.ricam.oeaw.ac.at/gismo/stable@6960 e128ff1e-fa79-4151-a8cb-522299a2c450

--- src/gsNurbs/gsKnotVector.h
+++ src/gsNurbs/gsKnotVector.h
@@ -854,6 +854,19 @@ std::ostream& operator << (std::ostream& out, const gsKnotVector<T> KV )
 
 } // namespace gismo
 
+
+// *****************************************************************
 #ifndef GISMO_BUILD_LIB
 #include GISMO_HPP_HEADER(gsKnotVector.hpp)
+#else
+#ifdef gsKnotVector_EXPORT
+#undef  EXTERN_CLASS_TEMPLATE
+#define EXTERN_CLASS_TEMPLATE CLASS_TEMPLATE_INST
 #endif
+namespace gismo
+{
+EXTERN_CLASS_TEMPLATE gsKnotVector<real_t>;
+//EXTERN_CLASS_TEMPLATE internal::gsXml<gsKnotVector<real_t> >;
+}
+#endif
+// *****************************************************************
--- src/gsNurbs/gsKnotVector_.cpp
+++ src/gsNurbs/gsKnotVector_.cpp
@@ -1,21 +1,15 @@
-#include <gsCore/gsTemplateTools.h>
+/* Symbol export for G+Smo shared object */
 
-#ifdef _MSC_VER
-//non-standard extension: extern before explicit inst.
-#pragma warning(push)
-#pragma warning(disable : 4231)
-#endif
+#define gsKnotVector_EXPORT
 
+#include <gsCore/gsExport.h>
 #include <gsNurbs/gsKnotVector.h>
 #include <gsNurbs/gsKnotVector.hpp>
 
 namespace gismo
 {
-    CLASS_TEMPLATE_INST gsKnotVector<real_t>;
 
-    CLASS_TEMPLATE_INST internal::gsXml< gsKnotVector<real_t> >;
+//CLASS_TEMPLATE_INST gsKnotVector<real_t>;
+CLASS_TEMPLATE_INST internal::gsXml< gsKnotVector<real_t> >;
+
 }
-
-#ifdef _MSC_VER
-#pragma warning(pop)
-#endif
