d7415ba053376c79086181811cd9bd437f81911f
amantzaflaris <amantzaflaris@e128ff1e-fa79-4151-a8cb-522299a2c450>
2016-09-10 18:53:00 +0000

    add return
    git-svn-id: https://ricamsvn.ricam.oeaw.ac.at/gismo/stable@7930 e128ff1e-fa79-4151-a8cb-522299a2c450

--- src/gsIO/gsOptionList.h
+++ src/gsIO/gsOptionList.h
@@ -101,6 +101,7 @@ public:
             m_reals    = other.m_reals;
             m_switches = other.m_switches;            
         }
+        return *this;
     }
 
 private:
@@ -234,6 +235,59 @@ public:
         	optionList->insert_node(0, node_str);
         }
         // */
+        
+        /*
+        gsXmlNode * tmp;
+        gsXmlAttribute * atr;
+        gsOptionList::StringTable::const_iterator it1;
+        for ( it1 = obj.m_strings.begin(); it1 != obj.m_strings.end(); it1++ )
+        {
+            tmp = internal::makeNode("string", data);
+        	atr = internal::makeAttribute("label", it1->first, data);
+            tmp->insert_attribute(0, atr);
+        	atr = internal::makeAttribute("desc", it1->second.second, data);
+            tmp->insert_attribute(0, atr);
+            atr = internal::makeAttribute("value", it1->second.first, data);
+            tmp->insert_attribute(0, atr);
+            optionList->insert_node(0, tmp);
+        }
+        gsOptionList::IntTable::const_iterator it2;
+        for ( it2 = obj.m_ints.begin(); it2 != obj.m_ints.end(); it2++ )
+        {
+            tmp = internal::makeNode("int", data);
+        	atr = internal::makeAttribute("label", it2->first, data);
+            tmp->insert_attribute(0, atr);
+        	atr = internal::makeAttribute("desc", it2->second.second, data);
+            tmp->insert_attribute(0, atr);
+            atr = internal::makeAttribute("value", it2->second.first, data);
+            tmp->insert_attribute(0, atr);
+            optionList->insert_node(0, tmp);
+        }
+        gsOptionList::RealTable::const_iterator it3;
+        for ( it3 = obj.m_reals.begin(); it3 != obj.m_reals.end(); it3++ )
+        {
+            tmp = internal::makeNode("real", data);
+        	atr = internal::makeAttribute("label", it3->first, data);
+            tmp->insert_attribute(0, atr);
+        	atr = internal::makeAttribute("desc", it3->second.second, data);
+            tmp->insert_attribute(0, atr);
+            atr = internal::makeAttribute("value", it3->second.first, data);
+            tmp->insert_attribute(0, atr);
+            optionList->insert_node(0, tmp);
+        }
+        gsOptionList::SwitchTable::const_iterator it4;
+        for ( it4 = obj.m_switches.begin(); it4 != obj.m_switches.end(); it4++ )
+        {
+            tmp = internal::makeNode("switch", data);
+        	atr = internal::makeAttribute("label", it4->first, data);
+            tmp->insert_attribute(0, atr);
+        	atr = internal::makeAttribute("desc", it4->second.second, data);
+            tmp->insert_attribute(0, atr);
+            atr = internal::makeAttribute("value", it4->second.first, data);
+            tmp->insert_attribute(0, atr);
+            optionList->insert_node(0, tmp);
+        }
+        */
         
         return optionList;
     }
