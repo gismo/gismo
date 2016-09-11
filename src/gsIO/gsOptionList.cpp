e25278355c699f0ac12a49aa0719e67622f2d95a
amantzaflaris <amantzaflaris@e128ff1e-fa79-4151-a8cb-522299a2c450>
2016-09-11 13:41:44 +0000

    move io to cpp file
    git-svn-id: https://ricamsvn.ricam.oeaw.ac.at/gismo/stable@7932 e128ff1e-fa79-4151-a8cb-522299a2c450

--- src/gsIO/gsOptionList.cpp
+++ src/gsIO/gsOptionList.cpp
@@ -166,16 +166,16 @@ void gsOptionList::addInt(const std::string& label,
     if ( exists(label) )
     {
         /* // allow adding multiple times ?
-        IntTable::iterator it = m_ints.find(label);
-        if ( it != m_ints.end() )
-        {
-            if (it->second.second != desc)
-                gsWarn<< "Description changed for "<<label <<"from:\n"
-                      << it->second.second <<" to:\n"<< desc <<"\n";
-            // option already exists, so we update the value
-            it->second.first = value;
-            return;
-        } */
+           IntTable::iterator it = m_ints.find(label);
+           if ( it != m_ints.end() )
+           {
+           if (it->second.second != desc)
+           gsWarn<< "Description changed for "<<label <<"from:\n"
+           << it->second.second <<" to:\n"<< desc <<"\n";
+           // option already exists, so we update the value
+           it->second.first = value;
+           return;
+           } */
         printInfo(label);
         GISMO_ERROR("Option "<<label<<" already exists.");        
     }
@@ -183,8 +183,8 @@ void gsOptionList::addInt(const std::string& label,
 }
 
 void gsOptionList::addReal(const std::string& label,
-                          const std::string& desc,
-                          const real_t & value)
+                           const std::string& desc,
+                           const real_t & value)
 {
     if ( exists(label) )
     {
@@ -286,9 +286,9 @@ std::vector<gsOptionList::OptionListEntry> gsOptionList::getAllEntries() const
 }
 //*/
 
-#define OL_PRINT_INFO(it,type) \
+#define OL_PRINT_INFO(it,type)                                          \
     os<<"* "<<std::setw(17)<<std::left<<it->first <<std::setw(12)<<std::right<<" ("#type") = " \
-      <<std::setw(7)<<std::left<<it->second.first<<" "<<it->second.second<<"\n";
+    <<std::setw(7)<<std::left<<it->second.first<<" "<<it->second.second<<"\n";
 //<<std::boolalpha
 
 std::ostream & gsOptionList::print(std::ostream & os) const
@@ -296,13 +296,13 @@ std::ostream & gsOptionList::print(std::ostream & os) const
     os<<"Options ("<<size()<<"):\n";
     for (StringTable::const_iterator it1 = m_strings.begin();it1!=m_strings.end();++it1)
         OL_PRINT_INFO(it1,string)
-    for (IntTable::const_iterator it2 = m_ints.begin();it2!=m_ints.end();++it2)
-        OL_PRINT_INFO(it2,int)
-    for (RealTable::const_iterator it3 = m_reals.begin();it3!=m_reals.end();++it3)
-        OL_PRINT_INFO(it3,real)
-    for (SwitchTable::const_iterator it4 = m_switches.begin();it4!=m_switches.end();++it4)
-        OL_PRINT_INFO(it4,switch)
-    return os;
+            for (IntTable::const_iterator it2 = m_ints.begin();it2!=m_ints.end();++it2)
+                OL_PRINT_INFO(it2,int)
+                    for (RealTable::const_iterator it3 = m_reals.begin();it3!=m_reals.end();++it3)
+                        OL_PRINT_INFO(it3,real)
+                            for (SwitchTable::const_iterator it4 = m_switches.begin();it4!=m_switches.end();++it4)
+                                OL_PRINT_INFO(it4,switch)
+                                    return os;
 }
 
 void gsOptionList::printInfo(const std::string& label) const
@@ -312,25 +312,25 @@ void gsOptionList::printInfo(const std::string& label) const
     if ( it1 != m_strings.end() )
     {
         OL_PRINT_INFO(it1,string)
-        return;
+            return;
     }
     IntTable::const_iterator it2 = m_ints.find(label);
     if ( it2 != m_ints.end() )
     {
         OL_PRINT_INFO(it2,int)
-        return;
+            return;
     }
     RealTable::const_iterator it3 = m_reals.find(label);
     if ( it3 != m_reals.end() )
     {
         OL_PRINT_INFO(it3,real)
-        return;
+            return;
     }
     SwitchTable::const_iterator it4 = m_switches.find(label);
     if ( it4 != m_switches.end() )
     {
         OL_PRINT_INFO(it4,switch)
-        os<<"* "<<it4->second.second<<" (switch) \n  "<<it4->first <<" = "<<(it4->second.first ? "ON" : "OFF")<<"\n";
+            os<<"* "<<it4->second.second<<" (switch) \n  "<<it4->first <<" = "<<(it4->second.first ? "ON" : "OFF")<<"\n";
         return;
     }
     gsInfo <<"Problem: "<< label <<" does not exist.\n";
@@ -338,5 +338,136 @@ void gsOptionList::printInfo(const std::string& label) const
 
 #undef OL_PRINT_INFO
 
+
+
+namespace internal
+{
+
+void gsXml<gsOptionList>::get_into(gsXmlNode * node, gsOptionList & result)
+{
+    // get all child-nodes
+    gsXmlNode * tmp = node->first_node();
+    while ( tmp )
+    {
+        const char* name = tmp->name();
+
+        const std::string label = tmp->first_attribute("label")->value();
+        const std::string desc = tmp->first_attribute("desc")->value();
+        const std::string val = tmp->first_attribute("value")->value();
+
+        if (strcmp("int", name) == 0)
+        {
+            std::istringstream str;
+            str.str( val );
+            int myVal;
+            gsGetInt(str, myVal);
+            result.addInt(label, desc, myVal);
+        }
+        else if (strcmp("real", name) == 0)
+        {
+            std::istringstream str;
+            str.str( val );
+            real_t myVal;
+            gsGetReal(str, myVal);
+            result.addReal(label, desc, myVal);
+        }
+        else if (strcmp("bool", name) == 0)
+        {
+            std::istringstream str;
+            str.str( val );
+            int myVal;
+            gsGetInt(str, myVal);
+            result.addSwitch(label, desc, (0 != myVal) );
+        }
+        else
+        {
+            result.addString(label, desc, val);
+        }
+        tmp =  tmp->next_sibling();
+    }
+}
+
+gsXmlNode *
+gsXml<gsOptionList>::put (const gsOptionList & obj, gsXmlTree & data)
+{
+    // Append data
+    gsXmlNode * optionList = internal::makeNode("OptionList", data);
+
+    // /*
+    // iterate over all strings
+    std::vector<gsOptionList::OptionListEntry> entries = obj.getAllEntries();
+    std::vector<gsOptionList::OptionListEntry>::const_iterator it;
+    for (it = entries.begin(); it != entries.end(); it++)
+    {
+        const gsOptionList::OptionListEntry & entry = *it;
+        gsXmlNode * node_str = internal::makeNode(entry.type, data);
+        gsXmlAttribute * attr_label = internal::makeAttribute("label", entry.label, data);
+        gsXmlAttribute * attr_desc = internal::makeAttribute("desc", entry.desc, data);
+        gsXmlAttribute * attr_val = internal::makeAttribute("value", entry.val, data);
+        node_str->insert_attribute(0, attr_label);
+        node_str->insert_attribute(0, attr_desc);
+        node_str->insert_attribute(0, attr_val);
+        optionList->insert_node(0, node_str);
+    }
+    // */
+        
+    /*
+      gsXmlNode * tmp;
+      gsXmlAttribute * atr;
+      gsOptionList::StringTable::const_iterator it1;
+      for ( it1 = obj.m_strings.begin(); it1 != obj.m_strings.end(); it1++ )
+      {
+      tmp = internal::makeNode("string", data);
+      atr = internal::makeAttribute("label", it1->first, data);
+      tmp->insert_attribute(0, atr);
+      atr = internal::makeAttribute("desc", it1->second.second, data);
+      tmp->insert_attribute(0, atr);
+      atr = internal::makeAttribute("value", it1->second.first, data);
+      tmp->insert_attribute(0, atr);
+      optionList->insert_node(0, tmp);
+      }
+      gsOptionList::IntTable::const_iterator it2;
+      for ( it2 = obj.m_ints.begin(); it2 != obj.m_ints.end(); it2++ )
+      {
+      tmp = internal::makeNode("int", data);
+      atr = internal::makeAttribute("label", it2->first, data);
+      tmp->insert_attribute(0, atr);
+      atr = internal::makeAttribute("desc", it2->second.second, data);
+      tmp->insert_attribute(0, atr);
+      atr = internal::makeAttribute("value", it2->second.first, data);
+      tmp->insert_attribute(0, atr);
+      optionList->insert_node(0, tmp);
+      }
+      gsOptionList::RealTable::const_iterator it3;
+      for ( it3 = obj.m_reals.begin(); it3 != obj.m_reals.end(); it3++ )
+      {
+      tmp = internal::makeNode("real", data);
+      atr = internal::makeAttribute("label", it3->first, data);
+      tmp->insert_attribute(0, atr);
+      atr = internal::makeAttribute("desc", it3->second.second, data);
+      tmp->insert_attribute(0, atr);
+      atr = internal::makeAttribute("value", it3->second.first, data);
+      tmp->insert_attribute(0, atr);
+      optionList->insert_node(0, tmp);
+      }
+      gsOptionList::SwitchTable::const_iterator it4;
+      for ( it4 = obj.m_switches.begin(); it4 != obj.m_switches.end(); it4++ )
+      {
+      tmp = internal::makeNode("switch", data);
+      atr = internal::makeAttribute("label", it4->first, data);
+      tmp->insert_attribute(0, atr);
+      atr = internal::makeAttribute("desc", it4->second.second, data);
+      tmp->insert_attribute(0, atr);
+      atr = internal::makeAttribute("value", it4->second.first, data);
+      tmp->insert_attribute(0, atr);
+      optionList->insert_node(0, tmp);
+      }
+    */
+        
+    return optionList;
+}
+
+
+} // namespace internal
 
 } //namespace gismo
