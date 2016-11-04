be055d8bddf50ad59d802b850f2cc74baf9d24d1
amantzaflaris <amantzaflaris@e128ff1e-fa79-4151-a8cb-522299a2c450>
2016-11-04 11:52:02 +0000

    update simple ops
    git-svn-id: https://ricamsvn.ricam.oeaw.ac.at/gismo/stable@8352 e128ff1e-fa79-4151-a8cb-522299a2c450

--- src/gsSolver/gsSimpleOps.h
+++ src/gsSolver/gsSimpleOps.h
@@ -1,6 +1,6 @@
 /** @file gsSimpleOps.h
 
-    @brief Colloction of some simple preconditioners.
+    @brief Collection of some simple preconditioners.
 
     This file is part of the G+Smo library.
 
@@ -8,7 +8,7 @@
     License, v. 2.0. If a copy of the MPL was not distributed with this
     file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-    Author(s): J. Sogn, C. Hofreither
+    Author(s): J. Sogn, C. Hofreither, A. Mantzaflaris
 */
 #pragma once
 
@@ -49,6 +49,9 @@ GISMO_EXPORT void gaussSeidelSingleBlock(const gsSparseMatrix<real_t>& A, gsMatr
 template <typename MatrixType>
 class gsRichardsonOp : public gsLinearOperator<typename MatrixType::Scalar>
 {
+    typedef typename memory::shared<MatrixType>::ptr MatrixPtr;
+    typedef typename MatrixType::Nested              NestedMatrix;
+
 public:
     typedef typename MatrixType::Scalar T;
     
@@ -57,13 +60,20 @@ public:
 
     /// Unique pointer for gsRichardsonOp   
     typedef typename memory::unique< gsRichardsonOp >::ptr uPtr;    
+
+    /// @brief Constructor with given matrix
+    explicit gsRichardsonOp(const MatrixType& _mat)
+    : m_mat(), m_expr(_mat.derived()), m_numOfSweeps(1), m_tau(1) {}
     
-    /// @brief Contructor with given matrix
-    gsRichardsonOp(const MatrixType& _mat, T tau = 1.)
-        : m_mat(_mat), m_numOfSweeps(1), m_tau(tau) {}
-        
-    static Ptr make(const MatrixType& _mat, T tau = 1.)
-    { return memory::make_shared( new gsRichardsonOp(_mat,tau) ); }
+    /// @brief Constructor with shared pointer to matrix
+    explicit gsRichardsonOp(const MatrixPtr& _mat)
+    : m_mat(_mat), m_expr(m_mat->derived()), m_numOfSweeps(1), m_tau(1) { }
+    
+    static Ptr make(const MatrixType& _mat) 
+    { return memory::make_shared( new gsRichardsonOp(_mat) ); }
+
+    static Ptr make(const MatrixPtr& _mat) 
+    { return memory::make_shared( new gsRichardsonOp(_mat) ); }
 
     void apply(const gsMatrix<T> & input, gsMatrix<T> & x) const
     {
@@ -71,14 +81,10 @@ public:
                       && input.cols() == 1, "Dimensions do not match");
 
         // For the first sweep, we do not need to multiply with the matrix
-        x = m_tau * input;
-
-        gsMatrix<T> temp;
+        x.noalias() = m_tau * input;
+        
         for (index_t k = 1; k < m_numOfSweeps; ++k)
-        {
-            temp.noalias() = input - m_mat * x;
-            x += m_tau * temp;
-        }
+            x += m_tau * ( input - m_expr * x );
     }
 
     index_t rows() const {return m_mat.rows();}
@@ -91,14 +97,26 @@ public:
         m_numOfSweeps=n;
     }
 
+    /// Set scaling parameter
+    void setScaling(const T tau) { m_tau = tau; }
+
     ///Returns the matrix
     const MatrixType & matrix() const { return m_mat; }
 
 private:
-    MatrixType m_mat; // expensive copy
+    const MatrixPtr m_mat;  ///< Shared pointer to matrix (if needed)
+    NestedMatrix    m_expr; ///< Nested Eigen expression
+
     index_t m_numOfSweeps;
     T m_tau;
 };
+
+/**
+   \brief Returns a shared pointer to a Richardson operator referring on \a mat
+*/
+template <class Derived>
+typename gsRichardsonOp<Derived>::Ptr makeRichardsonOp(const Eigen::EigenBase<Derived>& mat)
+{ return gsRichardsonOp<Derived>::make(mat.derived()); }
 
 /// @brief Jacobi preconditioner
 ///
@@ -108,6 +126,9 @@ private:
 template <typename MatrixType>
 class gsJacobiOp : public gsLinearOperator<typename MatrixType::Scalar>
 {
+    typedef typename memory::shared<MatrixType>::ptr MatrixPtr;
+    typedef typename MatrixType::Nested              NestedMatrix;
+
 public:
     typedef typename MatrixType::Scalar T;
     
@@ -117,46 +138,61 @@ public:
     /// Unique pointer for gsJacobiOp   
     typedef typename memory::unique< gsJacobiOp >::ptr uPtr;    
 
-    /// @brief Contructor with given matrix
-    gsJacobiOp(const MatrixType& _mat, T tau = 1.)
-        : m_mat(_mat), m_numOfSweeps(1), m_tau(tau) {}
-        
-    static Ptr make(const MatrixType& _mat, T tau = 1.)
-    { return memory::make_shared( new gsJacobiOp(_mat,tau) ); }
+    /// @brief Constructor with given matrix
+    explicit gsJacobiOp(const MatrixType& _mat)
+    : m_mat(), m_expr(_mat.derived()), m_numOfSweeps(1), m_tau(1) {}
+    
+    /// @brief Constructor with shared pointer to matrix
+    explicit gsJacobiOp(const MatrixPtr& _mat)
+    : m_mat(_mat), m_expr(m_mat->derived()), m_numOfSweeps(1), m_tau(1) { }
+    
+    static Ptr make(const MatrixType& _mat) 
+    { return memory::make_shared( new gsJacobiOp(_mat) ); }
 
+    static Ptr make(const MatrixPtr& _mat) 
+    { return memory::make_shared( new gsJacobiOp(_mat) ); }
+        
     void apply(const gsMatrix<T> & input, gsMatrix<T> & x) const
     {
-        GISMO_ASSERT( m_mat.rows() == input.rows() && m_mat.cols() == m_mat.rows() && input.cols() == 1, "Dimensions do not match.");
+        GISMO_ASSERT( m_mat.rows() == input.rows() && m_mat.cols() == m_mat.rows()
+                      && input.cols() == 1, "Dimensions do not match.");
 
         // For the first sweep, we do not need to multiply with the matrix
-        x.array() = m_tau * input.array() / m_mat.diagonal().array();
+        x.array() = m_tau * input.array() / m_expr.diagonal().array();
         
         for (index_t k = 1; k < m_numOfSweeps; ++k)
-            x.array() += m_tau * ( input - m_mat * x ).array() / m_mat.diagonal().array();
+            x.array() += m_tau * ( input - m_expr * x ).array() / m_expr.diagonal().array();
     }
 
-    index_t rows() const {return m_mat.rows();}
-    index_t cols() const {return m_mat.cols();}
+    index_t rows() const {return m_expr.rows();}
+    index_t cols() const {return m_expr.cols();}
 
     /// Set number of sweeps.
-    void setNumOfSweeps(index_t n) {
+    void setNumOfSweeps(index_t n)
+    {
         GISMO_ASSERT ( n > 0, "Number of sweeps needs to be positive. ");
-        m_numOfSweeps=n;
+        m_numOfSweeps = n;
     }
 
+    /// Set scaling parameter
+    void setScaling(const T tau) { m_tau = tau; }
+
     ///Returns the matrix
-    const MatrixType & matrix() const { return m_mat; }
+    NestedMatrix matrix() const { return m_expr; }
 
 private:
-    MatrixType m_mat; // expensive copy
+    const MatrixPtr m_mat;  ///< Shared pointer to matrix (if needed)
+    NestedMatrix    m_expr; ///< Nested Eigen expression
     index_t m_numOfSweeps;
     T m_tau;
 };
 
+/**
+   \brief Returns a shared pointer to a Jacobi operator referring on \a mat
+*/
 template <class Derived>
-typename gsJacobiOp<Derived>::Ptr makeJacobiOp(const Eigen::EigenBase<Derived>& mat,
-                                               typename Derived::Scalar tau = 1)
-{ return gsJacobiOp<Derived>::make(mat.derived(), tau); }
+typename gsJacobiOp<Derived>::Ptr makeJacobiOp(const Eigen::EigenBase<Derived>& mat)
+{ return gsJacobiOp<Derived>::make(mat.derived()); }
 
 
 /// @brief Gauss-Seidel preconditioner
@@ -167,6 +203,9 @@ typename gsJacobiOp<Derived>::Ptr makeJacobiOp(const Eigen::EigenBase<Derived>& 
 template <typename MatrixType>
 class gsGaussSeidelOp : public gsLinearOperator<typename MatrixType::Scalar>
 {
+    typedef typename memory::shared<MatrixType>::ptr MatrixPtr;
+    typedef typename MatrixType::Nested              NestedMatrix;
+
 public:
     typedef typename MatrixType::Scalar T;
 
@@ -176,11 +215,18 @@ public:
     /// Unique pointer for gsGaussSeidelOp   
     typedef typename memory::unique< gsGaussSeidelOp >::ptr uPtr;   
     
-    /// @brief Contructor with given matrix
-    gsGaussSeidelOp(const MatrixType& _mat)
-        : m_mat(_mat), m_numOfSweeps(1) {}
-        
-    static Ptr make(const MatrixType& _mat)
+    /// @brief Constructor with given matrix
+    explicit gsGaussSeidelOp(const MatrixType& _mat)
+    : m_mat(), m_expr(_mat.derived()), m_numOfSweeps(1) {}
+    
+    /// @brief Constructor with shared pointer to matrix
+    explicit gsGaussSeidelOp(const MatrixPtr& _mat)
+    : m_mat(_mat), m_expr(m_mat->derived()), m_numOfSweeps(1) { }
+    
+    static Ptr make(const MatrixType& _mat) 
+    { return memory::make_shared( new gsGaussSeidelOp(_mat) ); }
+
+    static Ptr make(const MatrixPtr& _mat) 
     { return memory::make_shared( new gsGaussSeidelOp(_mat) ); }
 
     void apply(const gsMatrix<T> & input, gsMatrix<T> & x) const
@@ -197,7 +243,8 @@ public:
     index_t cols() const {return m_mat.cols();}
 
     /// Set number of sweeps of to symmetric Gauss-Seidel perform (default is 1).
-    void setNumOfSweeps(index_t n) {
+    void setNumOfSweeps(index_t n)
+    {
         GISMO_ASSERT ( n > 0, "Number of sweeps needs to be positive. ");
         m_numOfSweeps=n;
     }
@@ -206,10 +253,17 @@ public:
     const MatrixType & matrix() const { return m_mat; }
     
 private:
-    MatrixType m_mat; // expensive copy
+    const MatrixPtr m_mat;  ///< Shared pointer to matrix (if needed)
+    NestedMatrix    m_expr; ///< Nested Eigen expression
     index_t m_numOfSweeps;
 };
 
+/**
+   \brief Returns a shared pointer to a Gauss-Seidel operator referring on \a mat
+*/
+template <class Derived>
+typename gsGaussSeidelOp<Derived>::Ptr makeGaussSeidelOp(const Eigen::EigenBase<Derived>& mat)
+{ return gsGaussSeidelOp<Derived>::make(mat.derived()); }
 
 /// @brief Symmetric Gauss-Seidel preconditioner
 ///
@@ -222,7 +276,7 @@ template <typename MatrixType>
 class gsSymmetricGaussSeidelOp : public gsLinearOperator<typename MatrixType::Scalar>
 {
     typedef typename memory::shared<MatrixType>::ptr MatrixPtr;
-    typedef typename MatrixType::Nested     NestedMatrix;
+    typedef typename MatrixType::Nested          NestedMatrix;
 
 public:
     typedef typename MatrixType::Scalar T;
@@ -233,11 +287,11 @@ public:
     /// Unique pointer for gsSymmetricGaussSeidelOp   
     typedef typename memory::unique< gsSymmetricGaussSeidelOp >::ptr uPtr; 
     
-    /// @brief Contructor with given matrix
+    /// @brief Constructor with given matrix
     explicit gsSymmetricGaussSeidelOp(const MatrixType& _mat)
     : m_mat(), m_expr(_mat.derived()), m_numOfSweeps(1) {}
 
-    /// @brief Contructor with shared pointer to matrix
+    /// @brief Constructor with shared pointer to matrix
     explicit gsSymmetricGaussSeidelOp(const MatrixPtr& _mat)
     : m_mat(_mat), m_expr(m_mat->derived()), m_numOfSweeps(1) {}
     
@@ -276,6 +330,14 @@ private:
 
     index_t m_numOfSweeps;
 };
+
+/**
+   \brief Returns a shared pointer to a Symmetric Gauss-Seidel operator referring on \a mat
+*/
+template <class Derived>
+typename gsSymmetricGaussSeidelOp<Derived>::Ptr
+makeSymmetricGaussSeidelOp(const Eigen::EigenBase<Derived>& mat)
+{ return gsSymmetricGaussSeidelOp<Derived>::make(mat.derived()); }
 
 
 } // namespace gismo
