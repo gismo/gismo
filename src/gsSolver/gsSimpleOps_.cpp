928981cf76533418ddd09813caa5a9132e12c111
stakacs <stakacs@e128ff1e-fa79-4151-a8cb-522299a2c450>
2017-01-31 09:47:31 +0000

    fix
    git-svn-id: https://ricamsvn.ricam.oeaw.ac.at/gismo/stable@8825 e128ff1e-fa79-4151-a8cb-522299a2c450

--- src/gsSolver/gsSimpleOps_.cpp
+++ src/gsSolver/gsSimpleOps_.cpp
@@ -8,7 +8,7 @@
     License, v. 2.0. If a copy of the MPL was not distributed with this
     file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-    Author(s): J. Sogn, C. Hofreither, A. Mantzaflaris
+    Author(s): J. Sogn, C. Hofreither, A. Mantzaflaris, S. Takacs
 */
 
 #include <gsSolver/gsSimpleOps.hpp>
@@ -16,11 +16,11 @@
 namespace gismo
 {
     
-TEMPLATE_INST void dampedRichardsonSweep(const gsSparseMatrix<real_t>& A, gsMatrix<real_t>& x, const gsMatrix<real_t>& f, real_t tau = (real_t)(1.));
+TEMPLATE_INST void dampedRichardsonSweep(const gsSparseMatrix<real_t>& A, gsMatrix<real_t>& x, const gsMatrix<real_t>& f, real_t tau);
 
 TEMPLATE_INST void jacobiSweep(const gsSparseMatrix<real_t>& A, gsMatrix<real_t>& x, const gsMatrix<real_t>& f);
 
-TEMPLATE_INST void dampedJacobiSweep(const gsSparseMatrix<real_t>& A, gsMatrix<real_t>& x, const gsMatrix<real_t>& f, real_t tau = (real_t)(0.5));
+TEMPLATE_INST void dampedJacobiSweep(const gsSparseMatrix<real_t>& A, gsMatrix<real_t>& x, const gsMatrix<real_t>& f, real_t tau);
 
 TEMPLATE_INST void gaussSeidelSweep(const gsSparseMatrix<real_t>& A, gsMatrix<real_t>& x, const gsMatrix<real_t>& f);
 
