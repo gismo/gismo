76f27ce59b158bc65e1f25060de99b1a918c73cb
Angelos Mantzaflaris <filiatra@gmail.com>
2017-11-18 18:25:33 +0100

    update dllmain

--- src/misc/gsDllMain.cpp
+++ src/misc/gsDllMain.cpp
@@ -1,9 +1,21 @@
+/** @file gsDllMain.cpp
 
-// Making an empty library in windows, also any library initialization
-// code goes here
-#ifdef WIN32
+    @brief Required function for MS windows DLL
+
+    This file is part of the G+Smo library. 
+
+    This Source Code Form is subject to the terms of the Mozilla Public
+    License, v. 2.0. If a copy of the MPL was not distributed with this
+    file, You can obtain one at http://mozilla.org/MPL/2.0/.
+    
+    Author(s): A. Mantzaflaris
+*/
+
+#ifdef _WIN32
 
 #include <windows.h>
+
+// Any library initialization code goes here
 BOOL APIENTRY 
 DllMain( HANDLE /* hModule */,
          DWORD  /* ul_reason_for_call */, 
