0622ea42e45d77d6c432ceec9cd81534dc771000
Angelos Mantzaflaris <filiatra@gmail.com>
2017-11-18 18:22:51 +0100

    avoid explicit long double specification of pi

--- external/Eigen/src/Core/MathFunctions.h
+++ external/Eigen/src/Core/MathFunctions.h
@@ -12,7 +12,7 @@
 
 // source: http://www.geom.uiuc.edu/~huberty/math5337/groupe/digits.html
 // TODO this should better be moved to NumTraits
-#define EIGEN_PI 3.141592653589793238462643383279502884197169399375105820974944592307816406L
+#define EIGEN_PI 3.141592653589793238462643383279502884197169399375105820974944592307816406 // G+Smo
 
 
 namespace Eigen {
