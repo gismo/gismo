3f21cba75a455e105bbed0ada0352cabb01535fb
Stefan Takacs <stefan.takacs@ricam.oeaw.ac.at>
2017-11-22 22:40:26 +0100

    wintest

---
+++ examples/example_bspline_basis.cpp
@@ -0,0 +1,19 @@
+
+# include <gismo.h>
+
+using namespace gismo;
+
+
+int main(int argc, char *argv[])
+{
+
+    gsKnotVector<>   kv (-1, 0, 3,3, 1 );
+    gsBSplineBasis<> bsp(kv);
+    gsInfo << bsp.detail() << "\n";
+
+    bsp.uniformRefine();
+    gsInfo << bsp.detail() << "\n";
+
+
+    return 0;
+}
