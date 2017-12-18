1e824192e27e497bf9b20d7a33e27f9b5407088e
Stefan Takacs <stefan.takacs@ricam.oeaw.ac.at>
2017-12-18 10:56:42 +0100

    add names

--- src/gsNurbs/gsKnotVector.h
+++ src/gsNurbs/gsKnotVector.h
@@ -8,7 +8,7 @@
     License, v. 2.0. If a copy of the MPL was not distributed with this
     file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-    Author(s): D. Mokris, A. Bressan, A. Mantzaflaris
+    Author(s): D. Mokris, A. Bressan, A. Mantzaflaris, S. Takacs
 */
 
 #pragma once
