9dd7e5a57fd90bec8e1e8cba970db3924dc949e2
Juergen Vogl <29753931+codewerfer@users.noreply.github.com>
2018-02-23 15:43:25 +0100

    readablity of gsCurveLoop.h

--- src/gsModeling/gsCurveLoop.h
+++ src/gsModeling/gsCurveLoop.h
@@ -21,7 +21,7 @@
 
 namespace gismo
 {
-  
+
 /** @brief
     A closed loop given by a collection of curves.
     
@@ -31,7 +31,7 @@ namespace gismo
     
     \ingroup Modeling
 */
-  
+
 template<class T> class gsCurveLoop
 {
 
@@ -46,56 +46,55 @@ public:
     typedef memory::unique_ptr< gsCurveLoop > uPtr;
 
 public:
-      
-      /// Default empty constructor
-      gsCurveLoop() { }
-      
-      /// Constructor by one curve
-      explicit gsCurveLoop( gsCurve<T> * cc ) { this->insertCurve(cc); }
-      
-      /// Constructor by a list of curves forming a loop
-      explicit gsCurveLoop(std::vector< gsCurve<T> *> const & curves ) : m_curves(curves) { }
-      
-      /// Constructor from a sequence of points in 3D. Produces a polygon in 2D
-      /// whose angles are in proportion to the corresponding angles in space.
-      /// You need to pass in a sequence of bools indicating which angles are
-      /// convex.
-      gsCurveLoop(const std::vector<gsVector3d<T> *> points3D, const std::vector<bool> isConvex, T margin, gsVector3d<T> *outNormal);
-      
-      /// Constructor from a sequence of angles and lengths (measured in 3D).
-      /// If \a unitSquare==true, when there are four vertices and isConvex are all true, then return the spline curve
-      /// loop of degree 1 of the unit square with each edge being one spline curve.
-      gsCurveLoop(const std::vector<T> angles3D, const std::vector<T> lengths3D, const std::vector<bool> isConvex, T margin, const bool unitSquare=false);
+    /// Default empty constructor
+    gsCurveLoop() { }
 
-      gsCurveLoop(const gsCurveLoop & other)
-          : m_curves( other.m_curves.size() )
-      {
-          cloneAll( other.m_curves.begin(), other.m_curves.end(),
-                    this->m_curves.begin() );
-      }
+    /// Constructor by one curve
+    explicit gsCurveLoop( gsCurve<T> * cc ) { this->insertCurve(cc); }
 
-      gsCurveLoop& operator=(const gsCurveLoop & other)
-      {
-          freeAll( m_curves );
-          m_curves.resize( other.m_curves.size() );
-          cloneAll( other.m_curves.begin(), other.m_curves.end(),
-                    this->m_curves.begin() );
-          return *this;
-      }
-      
-      ~gsCurveLoop()
-      {
-          freeAll( m_curves );
-      }
+    /// Constructor by a list of curves forming a loop
+    explicit gsCurveLoop(std::vector< gsCurve<T> *> const & curves ) : m_curves(curves) { }
 
-      //GISMO_CLONE_FUNCTION(gsCurveLoop)
-      uPtr clone() const
-      {
-          return uPtr(new gsCurveLoop(*this));
-      }
-      
+    /// Constructor from a sequence of points in 3D. Produces a polygon in 2D
+    /// whose angles are in proportion to the corresponding angles in space.
+    /// You need to pass in a sequence of bools indicating which angles are
+    /// convex.
+    gsCurveLoop(const std::vector<gsVector3d<T> *> points3D, const std::vector<bool> isConvex, T margin, gsVector3d<T> *outNormal);
+
+    /// Constructor from a sequence of angles and lengths (measured in 3D).
+    /// If \a unitSquare==true, when there are four vertices and isConvex are all true, then return the spline curve
+    /// loop of degree 1 of the unit square with each edge being one spline curve.
+    gsCurveLoop(const std::vector<T> angles3D, const std::vector<T> lengths3D, const std::vector<bool> isConvex, T margin, const bool unitSquare=false);
+
+    gsCurveLoop(const gsCurveLoop & other)
+    : m_curves( other.m_curves.size() )
+    {
+        cloneAll( other.m_curves.begin(), other.m_curves.end(),
+                  this->m_curves.begin() );
+    }
+
+    gsCurveLoop& operator=(const gsCurveLoop & other)
+    {
+        freeAll( m_curves );
+        m_curves.resize( other.m_curves.size() );
+        cloneAll( other.m_curves.begin(), other.m_curves.end(),
+                  this->m_curves.begin() );
+        return *this;
+    }
+
+    ~gsCurveLoop()
+    {
+        freeAll( m_curves );
+    }
+
+    //GISMO_CLONE_FUNCTION(gsCurveLoop)
+    uPtr clone() const
+    {
+        return uPtr(new gsCurveLoop(*this));
+    }
+
 public:
-    
+
     bool isInterior ( gsVector<T> const & p, const T& tol);
 
     ///@name isOn
@@ -103,27 +102,27 @@ public:
     /// ideally it should store in \param paramResult the parametric values
     /// associated to those points.
     bool isOn(gsMatrix<T> const &u,T & paramResult, T tol = 1e-3);
-    
+
     ///@name is_ccw
     ///returns true if the curve is oriented counterclockwise
     bool is_ccw();
-    
+
     ///@name reverse
     ///changes the orientation of the curve
     void reverse();
-      
+
     void translate(gsVector<T> const & v);
 
-    void insertCurve( gsCurve<T> * c ) 
+    void insertCurve( gsCurve<T> * c )
     {
-            m_curves.push_back( c ) ;
+        m_curves.push_back( c ) ;
     }
-      
+
     /// Return the curve-loop as a single new B-Spline curve
     typename gsCurve<T>::uPtr singleCurve() const;
-      
+
     int size() const { return m_curves.size(); }
-    
+
     /// Prints the object as a string.
     std::ostream &print(std::ostream &os) const
     { os << "gsCurveLoop with "<< size() <<" curves :\n";
@@ -150,7 +149,7 @@ public:
     /// constant value \a abscissa in \a direction.
     // to do: replace return type with std::multimap<int,T>
     std::vector<T> lineIntersections(int const & direction , T const & abscissa);
-      
+
     std::vector< gsCurve<T> *> & curves() { return m_curves; }
 
     gsCurve<T> & curve(int i)
@@ -174,76 +173,75 @@ public:
     /// Sample \a npoints uniformly distributed (in parameter domain) points on the loop
     gsMatrix<T> sample(int npoints = 50, int numEndPoints=2) const;
 
-      gsMatrix<T> getBoundingBox();
+    gsMatrix<T> getBoundingBox();
 
-      /// Computes the corners of a polygon that matches the specified angles and
-      /// relative side lengths as well as possible. Fits the resulting polygon
-      /// inside the square [0, 1] x [0, 1]. Fills a gsmatrix whose rows are the
-      /// corners of the polygon. Returns true on success.
-      static bool approximatingPolygon(const std::vector<T> &signedAngles, const std::vector<T> &lengths, T margin, gsMatrix<T> &result);
+    /// Computes the corners of a polygon that matches the specified angles and
+    /// relative side lengths as well as possible. Fits the resulting polygon
+    /// inside the square [0, 1] x [0, 1]. Fills a gsmatrix whose rows are the
+    /// corners of the polygon. Returns true on success.
+    static bool approximatingPolygon(const std::vector<T> &signedAngles, const std::vector<T> &lengths, T margin, gsMatrix<T> &result);
 
-      /// return a vector containing whose nth entry is the length of the domin of the nth curve.
-      std::vector<T> domainSizes() const;
-      
-      /// Scale and shift a polygon (represented by a matrix whose rows are its
-      /// corners so that it will be contained in the square
-      ///   [0, 1]x[0, 1].
-      /// Modifies the polygon in place. Specifying margin > 0 will make the
-      /// polygon fit in the interior of the square.
-      static void adjustPolygonToUnitSquare(gsMatrix<T> &corners, T const margin);
-      
-      /// split the \a curveId^th curve in the loop into two curves. Return the point where it splits.
-      /// \param curveId
-      /// \param lengthRatio    the ratio between the lengths of the first new curve and that of the original curve
-      gsMatrix<T> splitCurve(std::size_t curveId, T lengthRatio=.5);
+    /// return a vector containing whose nth entry is the length of the domin of the nth curve.
+    std::vector<T> domainSizes() const;
 
-      /// Initialize a curve loop from some 3d vertices by projecting them onto the plane of best fit and constructing a polygon
-      gsVector3d<T> initFrom3DPlaneFit(const std::vector<gsVector3d<T> *> points3D, T margin);
+    /// Scale and shift a polygon (represented by a matrix whose rows are its
+    /// corners so that it will be contained in the square
+    ///   [0, 1]x[0, 1].
+    /// Modifies the polygon in place. Specifying margin > 0 will make the
+    /// polygon fit in the interior of the square.
+    static void adjustPolygonToUnitSquare(gsMatrix<T> &corners, T const margin);
 
-      /// Initialize a curve loop from some 3d vertices by trying to match the angles as well as possible.
-      /// Return true on success.
-      bool initFrom3DByAngles(const std::vector<gsVector3d<T> *> points3D, const std::vector<bool> isConvex, T margin);
-      
-      /// Initialize a curve loop from a list of bools indicating whether the corners are convex or not
-      void initFromIsConvex(const std::vector<bool> isConvex, T margin);
+    /// split the \a curveId^th curve in the loop into two curves. Return the point where it splits.
+    /// \param curveId
+    /// \param lengthRatio    the ratio between the lengths of the first new curve and that of the original curve
+    gsMatrix<T> splitCurve(std::size_t curveId, T lengthRatio=.5);
 
-      /// flip a curve loop in the u direction
-      void flip1(T minu = 0, T maxu = 1);
-      
-    private:
-      bool initFrom3DByAngles(const std::vector<T>& angles3D, const std::vector<T>& lengths3D, const std::vector<bool>& isConvex, T margin, bool unitsquare=false);
+    /// Initialize a curve loop from some 3d vertices by projecting them onto the plane of best fit and constructing a polygon
+    gsVector3d<T> initFrom3DPlaneFit(const std::vector<gsVector3d<T> *> points3D, T margin);
 
-      bool parameterOf(gsMatrix<T> const &u, int i, T & result, T tol = 1e-5);
-      
-      void removeCurves(iterator begin, iterator end);
+    /// Initialize a curve loop from some 3d vertices by trying to match the angles as well as possible.
+    /// Return true on success.
+    bool initFrom3DByAngles(const std::vector<gsVector3d<T> *> points3D, const std::vector<bool> isConvex, T margin);
 
-      void removeCurve(iterator it)
-        { removeCurves(it, it+1); }
+    /// Initialize a curve loop from a list of bools indicating whether the corners are convex or not
+    void initFromIsConvex(const std::vector<bool> isConvex, T margin);
 
-      // Data members
-      
-      std::vector< gsCurve<T> *> m_curves; // CCW
-      
-  }; // class gsCurveLoop
-  
-  
-  //////////////////////////////////////////////////
-  //////////////////////////////////////////////////
-  
-  template<class T>
-  bool gsCurveLoop<T>::isInterior ( gsVector<T> const & p, const T& tol)
-  {
+    /// flip a curve loop in the u direction
+    void flip1(T minu = 0, T maxu = 1);
+
+private:
+    bool initFrom3DByAngles(const std::vector<T>& angles3D, const std::vector<T>& lengths3D, const std::vector<bool>& isConvex, T margin, bool unitsquare=false);
+
+    bool parameterOf(gsMatrix<T> const &u, int i, T & result, T tol = 1e-5);
+
+    void removeCurves(iterator begin, iterator end);
+
+    void removeCurve(iterator it)
+    { removeCurves(it, it+1); }
+
+    // Data members
+
+    std::vector< gsCurve<T> *> m_curves; // CCW
+
+}; // class gsCurveLoop
+
+
+//////////////////////////////////////////////////
+//////////////////////////////////////////////////
+
+template<class T>
+bool gsCurveLoop<T>::isInterior ( gsVector<T> const & p, const T& tol)
+{
     // to do
     return false;
-  }
-  
-  
-  /// Print (as string) operator to be used by all derived classes
-  template<class T>
-  std::ostream &operator<<(std::ostream &os, const gsCurveLoop<T>& b)
-  {return b.print(os); }
-  
-  
+}
+
+/// Print (as string) operator to be used by all derived classes
+template<class T>
+std::ostream &operator<<(std::ostream &os, const gsCurveLoop<T>& b)
+{return b.print(os); }
+
+
 } // namespace gismo
 
 #ifndef GISMO_BUILD_LIB
