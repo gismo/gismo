c0772a878e07f1b72ae74a1fdce1be793963f0d6
Angelos Mantzaflaris <filiatra@users.noreply.github.com>
2018-02-26 10:15:25 +0100

    Update gsDofMapper.h

--- src/gsCore/gsDofMapper.h
+++ src/gsCore/gsDofMapper.h
@@ -250,8 +250,9 @@ public:
     void setShift(index_t shift);
 
     /// \brief Permutes the mapped free indices according to permutation, i.e.,  dofs_perm[idx] = dofs_old[permutation[idx]]
-    /// Important: Applying a permutation makes the functions regarding coupled dofs (cindex, is_coupled_index,.. ) invalid.
-    /// The dofs are still coupled, but you have now way of extracting them. If you need this functions, first call
+    ///
+    /// \Warning Applying a permutation makes the functions regarding coupled dofs (cindex, is_coupled_index,.. ) invalid.
+    /// The dofs are still coupled, but you have no way of extracting them. If you need this functions, first call
     /// markCoupledAsTagged() and then use the corresponding functions for tagged dofs.
     void permuteFreeDofs(const gsVector<index_t>& permutation);
 
