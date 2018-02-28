535a44b99bd0c9ba05e68708f0ce66685361be7b
Stefan Takacs <26736024+stefantakacs@users.noreply.github.com>
2018-02-28 23:59:53 +0100

    Update gsMatrixOp_test.cpp

--- unittests/gsMatrixOp_test.cpp
+++ unittests/gsMatrixOp_test.cpp
@@ -46,6 +46,20 @@ SUITE(gsMatrixOp_test)
         CHECK( ( A.transpose() - C ).norm() <= 1.e-10 );
     }
 
+    TEST(DenseMatrixSymm2)
+    {
+        gsMatrix<> A (3,3);
+        A << 2,2,3,  4,5,6,  7,8,10;
+        gsMatrix<> B (3,3);
+        B << 1,4,7,  4,5,8,  7,8,10;
+
+        gsMatrix<>::Nested C = A.selfadjointView<Lower>().derived()
+        
+        A(0,0) = 1; // check that C holds no copy
+
+        CHECK( ( B - C ).norm() <= 1.e-10 );
+    }
+
     TEST(DenseMatrixSymm)
     {
         gsMatrix<> A (3,3);
