c5f5404aa7fb38b85eaffedcb2df8f412c5e4d65
Angelos Mantzaflaris <Angelos.Mantzaflaris@oeaw.ac.at>
2018-03-28 11:23:35 +0200

    ctest cleanup

--- cmake/ctest_script.cmake
+++ cmake/ctest_script.cmake
@@ -1,16 +1,18 @@
 ######################################################################
-## ctest_script.cmake
+## ctest_script.txt
+## This file is part of the G+Smo library.
 ## https://raw.githubusercontent.com/gismo/gismo/stable/cmake/ctest_script.cmake
-## This file is part of the G+Smo library. 
-##
+## 
 ## Author: Angelos Mantzaflaris 
-## Copyright (C) 2012 - 2018 RICAM-Linz.
+## Copyright (C) 2012-2018
+######################################################################
+
+######################################################################
 ##
-## To execute (on linux):
+## To execute:
 ##
-##   ctest -j jobs -S ctest_script.cmake
+##   ctest -S ctest_script.cmake
 ##
-## It is recommended to make a copy of the file (especially using git).
 ## The script creates sources and build folders in the same directory
 ##
 ## For more verbosity add the flag
@@ -21,18 +23,18 @@
 ##
 ## Set execution options in the Configuration part.
 ## For multiple tests (eg. different compilers) make multiple copies
-## of this file and adjust options.
+## of this file and adjust options. Few options can be passed by arguments:
+##
+## ctest -S ctest_script.cmake,"Experimental;Release;8;Ninja"
 ##
 ## On linux this script can be invoked in a cronjob. e.g.:
-##    $ >crontab -e
+##    $ crontab -e
 ## Add the line:
-##    0 3 * * * /path/to/script/nightly_cron.sh &>/dev/null
+##    0 3 * * * ctest -S /path/toctest_script.cmake,"Nightly" &>/dev/null
 ## save and exit. Now with
 ##    $ crontab -l
-## you can see the scheduled task.
-##
-## "0 3 * * * " means that the script will be executed
-## every night at 03:00am.
+## you can see the scheduled task. The script will
+## be executed every night at 03:00am.
 ##
 ######################################################################
 
@@ -40,133 +42,165 @@
 ## Configuration
 ## #################################################################
 
-set(CTEST_USE_LAUNCHERS 1)
+# Test model (Nightly, Continuous, Experimental)
+set(CTEST_TEST_MODEL Experimental)
 
-# Test type (Nightly, Continuous, Experimental)
-set(test_model "Experimental")
+# Configuration type (Debug Release RelWithDebInfo MinSizeRel)
+set(CTEST_CONFIGURATION_TYPE Release)
 
-# Build type
-set(CTEST_CONFIGURATION_TYPE RelWithDebInfo)
-
-# The Generator for CMake.
-set(CTEST_CMAKE_GENERATOR "Unix Makefiles")
-#set(CTEST_CMAKE_GENERATOR "Ninja")
-#set(CTEST_CMAKE_GENERATOR "NMake Makefiles")
-#set(CTEST_CMAKE_GENERATOR "NMake Makefiles JOM")
-#set(CTEST_CMAKE_GENERATOR "MinGW Makefiles")
-#set(CTEST_CMAKE_GENERATOR "Visual Studio 12 2013")
-#set(CTEST_CMAKE_GENERATOR "Visual Studio 14 2015")
-#set(CTEST_CMAKE_GENERATOR "Visual Studio 14 2015 Win64")
-#set(CTEST_CMAKE_GENERATOR "Xcode")
-#set(CTEST_CMAKE_GENERATOR "CodeBlocks")
-#set(CTEST_CMAKE_GENERATOR "Sublime Text 2")
-#set(CTEST_CMAKE_GENERATOR "Eclipse CDT4")
+# Number of jobs for build/test
 #set(CTEST_BUILD_JOBS 8)
 #set(CTEST_TEST_JOBS 10)
 
-# Set environment/compiler
-#set(ENV{MAKEFLAGS} "-j12")
-#set(ENV{LD_LIBRARY_PATH} /path/to/vendor/lib)
-#set(ENV{CC}  "gcc")
-#set(ENV{CXX} "g++")
+# The Generator for CMake
+# ("Unix Makefiles", "Ninja", "Xcode", "NMake Makefiles", "NMake Makefiles JOM",
+#  "MinGW Makefiles", "Visual Studio 12 2013", "Visual Studio 14 2015",
+#  "Visual Studio 14 2015 Win64", and so on)
+#set(CTEST_CMAKE_GENERATOR "Ninja")
+
+# The above parameters can be reset by passing upto 4 arguments
+# e.g. as: ctest -S ctest_script.cmake,"Experimental;Release;8;Ninja"
+macro(read_args)
+  set(narg ${ARGC})
+    if (narg GREATER 0)
+      set(CTEST_TEST_MODEL ${ARGV0})
+    endif()
+  if (narg GREATER 1)
+    set(CTEST_CONFIGURATION_TYPE ${ARGV1})
+  endif()
+  if (narg GREATER 2)
+    set(CTEST_BUILD_JOBS "${ARGV2}")
+  endif()
+  if (narg GREATER 3)
+    set(CTEST_CMAKE_GENERATOR "${ARGV3}")
+  endif()
+endmacro(read_args)
+read_args(${CTEST_SCRIPT_ARG})
+
+# C compiler,  e.g. "/usr/bin/gcc", "/usr/bin/icc", "/usr/bin/clang"
+#set(ENV{CC}  "/usr/bin/gcc")
+
+# C++ compiler,  e.g. "/usr/bin/g++", "/usr/bin/icpc", "/usr/bin/clang++"
+#set(ENV{CXX} "/usr/bin/g++")
+
+# Other Environment variables and scripts
 #execute_process(COMMAND source "/path/to/iccvars.sh intel64")
-#set(ENV{CC}  "icc")
-#set(ENV{CXX} "icpc")
-#set(ENV{CC}  "clang")
-#set(ENV{CXX} "clang++")
+#set(ENV{LD_LIBRARY_PATH} /path/to/vendor/lib)
+#set(ENV{MAKEFLAGS} "-j12")
 
 # Build options
-set(gismo_build_options 
+set(gismo_build_options
+    -DGISMO_COEFF_TYPE=double
     -DGISMO_BUILD_LIB=ON
-    -DGISMO_WITH_ONURBS=ON
-    #-DGISMO_WITH_IPOPT=ON
-    #-DIpOpt_DIR=/path/to/ipopt
-    #-DGISMO_WITH_PSOLID=ON
-    #-DParasolid_DIR=/path/to/parasolid
-    #-DGISMO_BUILD_AXL=ON -DAxel_DIR=/path/to/axel
-    #-DGISMO_PLAINDOX=ON
-    #-DGISMO_BUILD_COVERAGE=ON
+    #-DCMAKE_CXX_STANDARD=11
+    -DGISMO_BUILD_EXAMPLES=ON
     -DGISMO_BUILD_UNITTESTS=ON
+    #-DGISMO_WITH_OPENMP=ON
+    #-DGISMO_WITH_MPI=ON
+    #-DGISMO_WITH_SPECTRA=ON
+    #-DGISMO_WITH_IPOPT=ON -DIpOpt_DIR=/path/to/ipopt
+    #-DGISMO_WITH_PSOLID=ON -DParasolid_DIR=/path/to/parasolid
+    #-DGISMO_BUILD_AXL=ON -DAxel_DIR=/path/to/axel
+    -DGISMO_WITH_ONURBS=ON
+    -DGISMO_BUILD_COVERAGE=OFF
+    -DGISMO_EXTRA_DEBUG=OFF
+    #-DGISMO_PLAINDOX=ON
 )
 
-# For continuous builds, number of seconds to stay alive
-set(test_runtime 40000)
-
-# ID for this computer that shows up on the dashboard.
-if(NOT HOSTNAME)
-find_program(HOSTNAME_CMD NAMES hostname)
-execute_process(COMMAND ${HOSTNAME_CMD} OUTPUT_VARIABLE HOSTNAME OUTPUT_STRIP_TRAILING_WHITESPACE)
-set(CTEST_SITE "${HOSTNAME}")
-endif(NOT HOSTNAME)
-
-# Name of this build
-if(NOT CTEST_BUILD_NAME)
-find_program(UNAME NAMES uname)
-execute_process(COMMAND "${UNAME}" "-s" OUTPUT_VARIABLE osname OUTPUT_STRIP_TRAILING_WHITESPACE)
-execute_process(COMMAND "${UNAME}" "-m" OUTPUT_VARIABLE "cpu" OUTPUT_STRIP_TRAILING_WHITESPACE)
-set(CTEST_BUILD_NAME "${osname}-${cpu} ${CTEST_CMAKE_GENERATOR}-${CTEST_CONFIGURATION_TYPE} $ENV{CXX}")
-endif(NOT CTEST_BUILD_NAME)
+# Computer ID shown on the dashboard (will be set automatically)
+# set(CTEST_SITE "Server0407")
   
 # Source folder (defaults inside the script directory)
 set(CTEST_SOURCE_DIRECTORY ${CTEST_SCRIPT_DIRECTORY}/gismo_src)
 
-#Build folder (defaults inside the script directory)
-set(CTEST_BINARY_DIRECTORY ${CTEST_SCRIPT_DIRECTORY}/build_ctest)
+# Build folder (defaults inside the script directory)
+set(CTEST_BINARY_DIRECTORY ${CTEST_SCRIPT_DIRECTORY}/gismo_build)
+
+# Empty existing directory before building (otherwise builds are incremental)
+#ctest_empty_binary_directory(${CTEST_BINARY_DIRECTORY})
+
+# Test timeout in seconds
+set(CTEST_TEST_TIMEOUT 800)
+
+# Coverage analysis
+set(test_coverage FALSE)
+set(CTEST_COVERAGE_COMMAND "/usr/bin/gcov")
+set(CTEST_CUSTOM_COVERAGE_EXCLUDE "${CTEST_SOURCE_DIRECTORY}/external/")
+
+# Memory check with valgrind
+set(test_memcheck FALSE)
+set(CTEST_MEMORYCHECK_COMMAND "/usr/bin/valgrind")
+set(MEMORYCHECK_SUPPRESSIONS_FILE "${CTEST_SOURCE_DIRECTORY}/cmake/valgrind_supp.txt")
+set(MEMORYCHECK_COMMAND_OPTIONS "--error-exitcode=1 --leak-check=yes -q")
+#--tool=memcheck --show-reachable=yes --num-callers=50 --track-origins=yes --trace-children=yes 
 
 # Update type (eg. svn or git)
 set(UPDATE_TYPE git)
-set(CTEST_UPDATE_COMMAND "git")
-set(CTEST_GIT_COMMAND "git")
-
-# Timeouts
-set(CTEST_TEST_TIMEOUT 800 CACHE STRING 
-    "Maximum time allowed before CTest will kill the test.") 
-set(DART_TESTING_TIMEOUT 800 CACHE STRING 
-    "Maximum time allowed before CTest will kill the test." FORCE)
-
-# Empty existing directory before building
-#ctest_empty_binary_directory(${CTEST_BINARY_DIRECTORY})
-
-# Coverage analysis
-#set(test_coverage TRUE)
-#set(CTEST_COVERAGE_COMMAND "/usr/bin/gcov")
-#set(CTEST_CUSTOM_COVERAGE_EXCLUDE "${CTEST_SOURCE_DIRECTORY}/external/")
-
-# Memory check with valgrind
-#set(test_memcheck true)
-#set(CTEST_MEMORYCHECK_COMMAND "/usr/bin/valgrind")
-#set(CTEST_MEMORYCHECK_SUPPRESSIONS_FILE "/path_to/suppression_file.supp")
-#set(CTEST_MEMORYCHECK_COMMAND_OPTIONS "--trace-children=yes --leak-check=full --show-reachable=yes --track-origins=yes")
+set(CTEST_UPDATE_COMMAND "/usr/bin/git")
+set(CTEST_GIT_COMMAND "/usr/bin/git")
 
 # Initial checkout
-set(GISMO_REPOSITORY https://github.com/gismo/gismo.git)
 if(NOT EXISTS "${CTEST_SOURCE_DIRECTORY}")
   message("Initial checkout...")
-  set(CTEST_CHECKOUT_COMMAND "git clone --depth 1 --branch stable ${GISMO_REPOSITORY} gismo_src")
+  set(GISMO_REPOSITORY https://github.com/gismo/gismo.git)
+  set(CTEST_CHECKOUT_COMMAND "${CTEST_GIT_COMMAND} clone --depth 1 --branch stable ${GISMO_REPOSITORY} gismo_src")
 endif()
+
+# For continuous builds, number of seconds to stay alive
+set(test_runtime 43200) #12h by default
 
 ## #################################################################
 ## Test routines
 ## #################################################################
 
-include(ProcessorCount)
-ProcessorCount(NPROC)
-message("Number of processors: ${NPROC}")
-if(${NPROC} EQUAL 0)
-  set(NPROC 1)
+#message(STATUS "Preserve full output (CTEST_FULL_OUTPUT)")
+if("${CTEST_CMAKE_GENERATOR}" MATCHES "Make" OR "${CTEST_CMAKE_GENERATOR}" MATCHES "Ninja")
+ set(CTEST_USE_LAUNCHERS 1)
+else()
+  set(CTEST_USE_LAUNCHERS 0)
+endif()
+
+set(ENV{CTEST_OUTPUT_ON_FAILURE} 1)
+set(DART_TESTING_TIMEOUT ${CTEST_TEST_TIMEOUT})
+
+if(NOT DEFINED CTEST_TEST_MODEL AND DEFINED ENV{CTEST_TEST_MODEL})
+  set(CTEST_TEST_MODEL $ENV{CTEST_TEST_MODEL})
+endif()
+if(NOT DEFINED CTEST_CONFIGURATION_TYPE AND DEFINED ENV{CTEST_CONFIGURATION_TYPE})
+  set(CTEST_CONFIGURATION_TYPE $ENV{CTEST_CONFIGURATION_TYPE})
+endif()
+
+if(NOT DEFINED CTEST_SITE)
+find_program(HOSTNAME_CMD NAMES hostname)
+execute_process(COMMAND ${HOSTNAME_CMD} OUTPUT_VARIABLE HOSTNAME OUTPUT_STRIP_TRAILING_WHITESPACE)
+set(CTEST_SITE "${HOSTNAME}")
+endif()
+
+# Name of this build
+if(NOT DEFINED CTEST_BUILD_NAME)
+find_program(UNAME NAMES uname)
+execute_process(COMMAND "${UNAME}" "-s" OUTPUT_VARIABLE osname OUTPUT_STRIP_TRAILING_WHITESPACE)
+execute_process(COMMAND "${UNAME}" "-m" OUTPUT_VARIABLE "cpu" OUTPUT_STRIP_TRAILING_WHITESPACE)
+set(CTEST_BUILD_NAME "${osname}-${cpu} ${CTEST_CMAKE_GENERATOR}-${CTEST_CONFIGURATION_TYPE} $ENV{CXX}")
 endif()
 
 if(NOT CTEST_BUILD_JOBS)
 include(ProcessorCount)
+ProcessorCount(NPROC)
+#message("Number of processors: ${NPROC}")
+if(${NPROC} EQUAL 0)
+  set(NPROC 1)
+endif()
 if(${NPROC} GREATER 20)
   set(CTEST_BUILD_JOBS 20)
 else()
   set(CTEST_BUILD_JOBS ${NPROC})
 endif()
-endif(NOT CTEST_BUILD_JOBS)
-if(NOT CTEST_TEST_JOBS)
-set(CTEST_TEST_JOBS ${NPROC})
-endif(NOT CTEST_TEST_JOBS)
+endif()
+
+if(NOT DEFINED CTEST_TEST_JOBS)
+set(CTEST_TEST_JOBS ${CTEST_BUILD_JOBS})
+endif()
 
 if(${CTEST_CMAKE_GENERATOR} MATCHES "Unix Makefiles"
   OR ${CTEST_CMAKE_GENERATOR} MATCHES "Ninja")
@@ -176,11 +210,10 @@ endif()
 
 macro(run_ctests)
   ctest_submit(PARTS Update)
-  ctest_configure(OPTIONS "${gismo_build_options}")
+  ctest_configure(OPTIONS "${gismo_build_options};-DCTEST_USE_LAUNCHERS=${CTEST_USE_LAUNCHERS}")
   ctest_submit(PARTS Configure)
   ctest_build(TARGET gsUnitTest) # for older versions of ninja
-  ctest_submit(PARTS Build)
-  ctest_build()
+  ctest_build(APPEND)
   ctest_build(TARGET unittests APPEND)
   ctest_submit(PARTS Build)
   ctest_test(PARALLEL_LEVEL ${CTEST_TEST_JOBS})
@@ -201,9 +234,9 @@ endmacro(run_ctests)
 
 file(MAKE_DIRECTORY "${CTEST_BINARY_DIRECTORY}")
 
-ctest_start(${test_model})
+ctest_start(${CTEST_TEST_MODEL})
 
-if(NOT "${test_model}" STREQUAL "Continuous")
+if(NOT "${CTEST_TEST_MODEL}" STREQUAL "Continuous")
 
 ctest_update()
 run_ctests()
@@ -212,12 +245,14 @@ else() #continuous model
 
 while(${CTEST_ELAPSED_TIME} LESS ${test_runtime})
   set(START_TIME ${CTEST_ELAPSED_TIME})
-  ctest_update(RETURN_VALUE count)
-  #message(STATUS "Found ${count} changed files.")
+  ctest_update()
   if( ${count} GREATER 0 )
     run_ctests()
   endif()
   ctest_sleep(${START_TIME} 300 ${CTEST_ELAPSED_TIME})
 endwhile()
 
-endif(NOT "${test_model}" STREQUAL "Continuous")
+endif(NOT "${CTEST_TEST_MODEL}" STREQUAL "Continuous")
+
+# Cleanup xml files after upload
+file(REMOVE_RECURSE ${CTEST_BINARY_DIRECTORY}/Testing)
