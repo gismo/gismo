675baaf51d1f87478c2d699d478fcbbc2d35808f
Matthias Moeller <m.moller@tudelft.nl>
2018-06-05 11:08:29 +0200

    Fixed bug when mixing GISMO_WITH_MPQ/GISMO_WITH_MPFR with other data types
    G+Smo did not support enabling GISMO_WITH_MPQ/GISMO_WITH_MPFR together
    with order data types, such as, GISMO_WITH_UNUM. Moreover, it was not
    possible to enable GISMO_WITH_MPQ and GISMO_WITH_MPFR without setting
    GISMO_COEFF_TYPE to mpq_class and mpfr::real, respectively.
    
    This is now possible with this commit. If G+Smo is compiled in
    header-only mode, one can now perform all computations using, say,
    doubles but enable MPQ/MPFR support to validate the result.
    
    The main change introduced with this commit it the separation of
    exprtk_X_adaptor.hpp into the two files
    
    exprtk_X_forward.hpp
    
    and
    
    exprtk_X_adaptor.hpp
    
    The forward declaration file only contains specializations of
    is_true(), is_false(), etcetera. The adaptor file provides the actual
    implementation. This splitting was implicitly done in the old version.
    
    Note: This commit does not change legacy behavior, that is, only
    setting GISMO_COEFF_TYPE to mpq_class or mpfr::real and letting G+Smo
    enable the ncessary GISMO_WITH_MPQ/GISMO_WITH_MPFR flags internally.

--- external/exprtk_mpfr_adaptor.hpp
+++ external/exprtk_mpfr_adaptor.hpp
@@ -23,38 +23,7 @@
 #include <string>
 #include <mpreal.h>
 
-
-namespace exprtk
-{
-   namespace details
-   {
-      namespace numeric { namespace details
-      {
-         struct mpfrreal_type_tag;
-
-         template <typename T> inline T const_pi_impl(mpfrreal_type_tag);
-         template <typename T> inline T const_e_impl (mpfrreal_type_tag);
-      }}
-
-      inline bool is_true (const mpfr::mpreal& v);
-      inline bool is_false(const mpfr::mpreal& v);
-
-      template <typename Iterator>
-      inline bool string_to_real(Iterator& itr_external, const Iterator end, mpfr::mpreal& t, numeric::details::mpfrreal_type_tag);
-
-   }
-
-   namespace rtl { namespace io
-   {
-      namespace details
-      {
-         inline void print_type(const std::string&, const mpfr::mpreal& v, exprtk::details::numeric::details::mpfrreal_type_tag);
-      }
-   }}
-
-   using details::is_true;
-}
-
+#include "exprtk_mpfr_forward.hpp"
 #include "exprtk.hpp"
 
 namespace exprtk
