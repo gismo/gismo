474757e68d93964f977e3176ac5944ee16a77593
Stefan Takacs <26736024+stefantakacs@users.noreply.github.com>
2018-06-12 14:57:34 +0200

    smaller improvements in gsBlockOp: (#195)
    * use usual notation for member variables
    * getOperator shall not fail if the block is missing: It is supposed to return a BasePtr which might be nullptr. The caller might use this information to determine with blocks do exist

--- src/gsSolver/gsBlockOp.hpp
+++ src/gsSolver/gsBlockOp.hpp
@@ -17,63 +17,56 @@ namespace gismo
 template<typename T>
 gsBlockOp<T>::gsBlockOp(index_t nRows, index_t nCols)
 {
-    blockPrec.resize(nRows, nCols);
-    blockTargetPositions.setZero(nRows);
-    blockInputPositions.setZero(nCols);
+    m_blockPrec.resize(nRows, nCols);
+    m_blockTargetPositions.setZero(nRows);
+    m_blockInputPositions.setZero(nCols);
     // Fill up all block entries with null pointers.
     for (index_t i = 0; i < nRows; ++i)
         for (index_t j = 0; j < nCols; ++j)
-            blockPrec(i,j).reset();
+            m_blockPrec(i,j).reset();
 }
 
 template<typename T>
 void gsBlockOp<T>::addOperator(index_t row, index_t col, const BasePtr& op)
 {
-    GISMO_ASSERT( row >= 0 && row < blockPrec.rows(), "The given row is not feasible." );
-    GISMO_ASSERT( col >= 0 && col < blockPrec.cols(), "The given column is not feasible." );
-    GISMO_ASSERT( op->rows() == blockTargetPositions[row] || blockTargetPositions[row] == 0,
+    GISMO_ASSERT( row >= 0 && row < m_blockPrec.rows(), "The given row is not feasible." );
+    GISMO_ASSERT( col >= 0 && col < m_blockPrec.cols(), "The given column is not feasible." );
+    GISMO_ASSERT( op->rows() == m_blockTargetPositions[row] || m_blockTargetPositions[row] == 0,
                   "The size of the given preconditioner does not fit to the other preconditioners in the same row." );
-    GISMO_ASSERT( op->cols() == blockInputPositions[col] || blockInputPositions[col] == 0,
+    GISMO_ASSERT( op->cols() == m_blockInputPositions[col] || m_blockInputPositions[col] == 0,
                   "The size of the given preconditioner does not fit to the other preconditioners in the same column." );
 
-    blockPrec(row, col) = op;
-    blockTargetPositions[row] = op->rows();
-    blockInputPositions[col] = op->cols();
+    m_blockPrec(row, col) = op;
+    m_blockTargetPositions[row] = op->rows();
+    m_blockInputPositions[col] = op->cols();
 }
 
 
 template<typename T>
 void gsBlockOp<T>::apply(const gsMatrix<T> & input, gsMatrix<T> & result) const
 {
-    result.setZero(blockTargetPositions.sum(), input.cols());
+    result.setZero(m_blockTargetPositions.sum(), input.cols());
     gsVector<index_t> singleCol(1);
     singleCol <<  input.cols();
-    typename gsMatrix<T>::BlockView resultBlocks = result.blockView(blockTargetPositions, singleCol);
+    typename gsMatrix<T>::BlockView resultBlocks = result.blockView(m_blockTargetPositions, singleCol);
 
-    for (index_t i = 0; i < blockPrec.rows() ; ++i)
+    for (index_t i = 0; i < m_blockPrec.rows() ; ++i)
     {
         index_t inputIndex = 0;
-        for (index_t j = 0; j < blockPrec.cols(); ++j)
+        for (index_t j = 0; j < m_blockPrec.cols(); ++j)
         {
-            if (!blockPrec(i,j))// if the block is a null pointer
+            if (!m_blockPrec(i,j))// if the block is a null pointer
             {
-                inputIndex += blockInputPositions(j);
+                inputIndex += m_blockInputPositions(j);
                 continue;
             }
 
             gsMatrix<T> tmp_result;
-            blockPrec(i,j)->apply(input.block(inputIndex,0,blockInputPositions(j),input.cols()),tmp_result);
+            m_blockPrec(i,j)->apply(input.block(inputIndex,0,m_blockInputPositions(j),input.cols()),tmp_result);
             resultBlocks(i) += tmp_result;
-            inputIndex += blockInputPositions(j);
+            inputIndex += m_blockInputPositions(j);
         }
     }
-}
-
-template<typename T>
-const typename gsBlockOp<T>::BasePtr & gsBlockOp<T>::getOperator(index_t row, index_t col) const
-{
-    GISMO_ASSERT((bool)blockPrec(row, col)!=0, "No linear operator exists in this block");
-    return blockPrec(row,col);
 }
 
 }
