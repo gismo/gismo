b68bf01a5179857aa912eb98d9924b1ed96e7a01
Matthias Möller <mmoelle1@googlemail.com>
2018-07-13 09:43:15 +0200

    Update gsDofMapper.h
    Method isFinalized() should have 'const'

--- src/gsCore/gsDofMapper.h
+++ src/gsCore/gsDofMapper.h
@@ -236,7 +236,7 @@ public:
     void finalize();
 
     /// \brief Checks whether finalize() has been called.
-    bool isFinalized() { return m_curElimId==0; }
+    bool isFinalized() const { return m_curElimId==0; }
 
     /// \brief Returns true iff the mapper is a permuatation
     bool isPermutation() const { return static_cast<std::size_t>(size())==mapSize(); }
