2c162a1784a19c38a1eada284f795e1796264724
Jürgen Vogl <29753931+codewerfer@users.noreply.github.com>
2018-07-25 10:44:02 +0200

    Update FindDbgHelp.cmake
    fix?

--- cmake/FindDbgHelp.cmake
+++ cmake/FindDbgHelp.cmake
@@ -7,12 +7,12 @@ FIND_PATH(DBGHELP_INCLUDE_DIR dbghelp.h
 )
 FIND_LIBRARY(DBGHELP_LIBRARY dbghelp.lib
   PATHS ${DbgHelp_DIR} $ENV{DXSDK_DIR}
-  PATH_SUFFIXES lib Lib lib/x64 Lib/x64
+  #PATH_SUFFIXES lib Lib lib/x86 Lib/x86
   NO_DEFAULT_PATH # Or else CMake 2.6.0 will find the dll in system32 on windows
 )
 FIND_LIBRARY(DBGHELP_LIBRARY dbghelp.lib # search again if the library wasn't found in the dependencies package
   PATHS ${DbgHelp_DIR} $ENV{DXSDK_DIR}
-  PATH_SUFFIXES lib Lib lib/x64 Lib/x64
+  #PATH_SUFFIXES lib Lib lib/x64 Lib/x64
 )
 
 # Handle the REQUIRED argument and set DBGHELP_FOUND
