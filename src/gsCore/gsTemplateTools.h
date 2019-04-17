c7f2b75dfd58dffdce80c823eb4fc870a2d2c830
codewerfer <29753931+codewerfer@users.noreply.github.com>
2019-04-17 12:44:47 +0200

    hotfix replace int64_t with long long

--- src/gsCore/gsTemplateTools.h
+++ src/gsCore/gsTemplateTools.h
@@ -16,7 +16,6 @@
 #include <gsCore/gsExport.h>
 #include <utility>
 #include <complex>
-#include <stdint.h>
 
 namespace gismo
 {
@@ -142,17 +141,18 @@ struct make_unsigned;
 #define GISMO_MAKE_UNSIGNED(signed_type) \
 template<>                               \
 struct make_unsigned<signed_type> {      \
-    typedef u##signed_type type;         \
+    typedef unsigned signed_type type;         \
 };                                       \
 template<>                               \
-struct make_unsigned<u##signed_type> {   \
-    typedef u##signed_type type;         \
+struct make_unsigned<unsigned signed_type> {   \
+    typedef unsigned signed_type type;         \
 };
 
-GISMO_MAKE_UNSIGNED(int8_t)
-GISMO_MAKE_UNSIGNED(int16_t)
-GISMO_MAKE_UNSIGNED(int32_t)
-GISMO_MAKE_UNSIGNED(int64_t)
+GISMO_MAKE_UNSIGNED(char)
+GISMO_MAKE_UNSIGNED(short)
+GISMO_MAKE_UNSIGNED(int)
+GISMO_MAKE_UNSIGNED(long)
+GISMO_MAKE_UNSIGNED(long long)
 #undef GISMO_MAKE_UNSIGNED
 
 #endif
