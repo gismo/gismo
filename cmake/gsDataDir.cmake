33b1e515b247e8a52ded241883f790d0f3ba9001
Angelos Mantzaflaris <filiatra@gmail.com>
2019-04-18 16:39:39 +0200

    minor changes

--- cmake/gsDataDir.cmake
+++ cmake/gsDataDir.cmake
@@ -1,7 +1,7 @@
 ### gsDataDir.cmake ---
-## 
+##
 ## set GISMO_DATA_DIR based on compiler-toolchain
-## 
+##
 ## Author: Harald Weiner
 ## Copyright (C) 2015 - RICAM-Linz.
 ######################################################################
@@ -10,9 +10,9 @@
 if (DEFINED EMSCRIPTEN)
 	set(GISMO_DATA_DIR "filedata/" )
 	message("GISMO_DATA_DIR set to relative path '${GISMO_DATA_DIR}'")
-	set(GISMO_ORIG_DATA_DIR "${PROJECT_SOURCE_DIR}/filedata/") 
+	set(GISMO_ORIG_DATA_DIR "${PROJECT_SOURCE_DIR}/filedata/")
 else()
-	set (GISMO_DATA_DIR   "${PROJECT_SOURCE_DIR}/filedata/") 
+	set (GISMO_DATA_DIR   "${PROJECT_SOURCE_DIR}/filedata/")
 endif()
 
 # Config-files directory
@@ -20,6 +20,6 @@ set(GISMO_CONFIG_DIR "${CMAKE_BINARY_DIR}/config/")
 
 # Set default search paths
 if (NOT DEFINED GISMO_SEARCH_PATHS)
-	set(GISMO_SEARCH_PATHS "${GISMO_DATA_DIR}" CACHE string
+	set(GISMO_SEARCH_PATHS "${GISMO_DATA_DIR}" CACHE STRING
 		"Define paths where files should be searched; seperated by semicolon.")
 endif()
