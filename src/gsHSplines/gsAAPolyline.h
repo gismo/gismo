0048ed4182a3c7b4b3446f1db2f24e3965b4d020
codewerfer <29753931+codewerfer@users.noreply.github.com>
2019-08-07 12:02:07 +0200

    fix compile with index_t := int64_t

--- src/gsHSplines/gsAAPolyline.h
+++ src/gsHSplines/gsAAPolyline.h
@@ -180,7 +180,7 @@ public:
     }
 
     /// Returns the polyline in a format suitable for further treatment by PARASOLID
-    std::vector< std::vector< int > > writeParasolid();
+    std::vector< std::vector< index_t > > writeParasolid();
 
     /// Returns the polyline in a format suitable for further treatment by PARASOLID
     std::vector< std::vector<unsigned int > > writeParasolidUnsigned();
@@ -278,9 +278,9 @@ bool gsAAPolyline<T>::pushFront( T x, T y0, T y1 ) // Symmetric case to push_bac
 }
 
 template<class T>
-std::vector< std::vector< int > > gsAAPolyline<T>::writeParasolid()
+std::vector< std::vector< index_t > > gsAAPolyline<T>::writeParasolid()
 {
-    std::vector< std::vector< int > > result;
+    std::vector< std::vector< index_t > > result;
 
     if( m_vertices.size() < 4 )
     {
@@ -294,7 +294,7 @@ std::vector< std::vector< int > > gsAAPolyline<T>::writeParasolid()
         return result;
     }
 
-    std::vector< int > item(4);
+    std::vector< index_t > item(4);
 
     // it will go through m_vertices; it_fwd will be pointing always to it's next % m_vertices.size().
     typename std::list< std::vector<T> >::iterator it_fwd = m_vertices.begin();
