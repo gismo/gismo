89b6b67541065b3ab71da0fffac6142a7f9963f0
Angelos Mantzaflaris <filiatra@users.noreply.github.com>
2020-02-18 14:55:32 +0100

    small fix

--- src/gsCore/gsGeometry.hpp
+++ src/gsCore/gsGeometry.hpp
@@ -129,7 +129,7 @@ void gsGeometry<T>::evaluateMesh(gsMesh<T>& mesh) const
 
     // For all vertices of the mesh, push forward the value by the
     // geometry mapping
-    if (1==gDim && 3<pDim) // Plot a graph
+    if (1==gDim && 3>pDim) // Plot a graph
         for (size_t i = 0; i!= mesh.numVertices(); ++i)
         {
             eval_into( mesh.vertex(i).topRows(pDim), tmp );
