115107eacc23d173c4369e286935156b64d74f63
Angelos Mantzaflaris <angelos.mantzaflaris@inria.fr>
2020-02-18 10:22:55 +0100

    fix dirichlet values

--- src/gsAssembler/gsDirichletValues.h
+++ src/gsAssembler/gsDirichletValues.h
@@ -7,35 +7,84 @@ template<class T> class gsFeSpace;
 };
 
 template<class T>
+void gsDirichletValues(
+    const gsBoundaryConditions<T> & bc,
+    const index_t dir_values,
+    const expr::gsFeSpace<T> & u)
+{
+    //if ( m_options.getInt("DirichletStrategy") == dirichlet::nitsche)
+    //    return; // Nothing to compute
+
+    //const gsMultiBasis<T> & mbasis = dynamic_cast<const gsMultiBasis<T>&>(u.source());
+
+    // eg. not penalize
+    const gsDofMapper & mapper = u.mapper();
+
+    switch ( dir_values )
+    {
+    case dirichlet::homogeneous :
+    case dirichlet::user :
+        gsMatrix<T> & fixedDofs = const_cast<expr::gsFeSpace<T>&>(u).fixedPart();
+        // If we have a homogeneous problem then fill with zeros
+        fixedDofs.setZero( mapper.boundarySize() );
+        break;
+    case dirichlet::interpolation:
+        gsDirichletValuesByInterpolation(u,bc);
+        break;
+    case dirichlet::l2Projection:
+
+        break;
+    default:
+        GISMO_ERROR("Something went wrong with Dirichlet values: "<< dir_values);
+    }
+
+    /* Corner values -- todo
+       for ( typename gsBoundaryConditions<T>::const_citerator
+       it = bbc.cornerBegin();
+       it != bbc.cornerEnd(); ++it )
+       {
+       if(it->unknown == unk)
+       {
+       const int i  = mbasis[it->patch].functionAtCorner(it->corner);
+       const int ii = mapper.bindex( i , it->patch );
+       u.fixedPart().row(ii).setConstant(it->value);
+       }
+       else
+       continue;
+       }
+    */
+}
+
+template<class T>
 void gsDirichletValuesByInterpolation(const expr::gsFeSpace<T> & u,
-                                      const gsFunction<T> & gmap,
                                       const gsBoundaryConditions<T> & bc)
 {
     const index_t parDim = u.source().domainDim();
     const gsMultiBasis<T> & mbasis =
         *dynamic_cast<const gsMultiBasis<T>*>(&u.source());
 
+    const gsFunctionSet<T> & gmap = bc.geoMap();
+
     std::vector< gsVector<T> > rr;
-    const gsMatrix<unsigned> boundary;
+    gsMatrix<unsigned> boundary;
     gsVector<T> b(1);
     gsMatrix<T> fpts, tmp;
-    // Iterate over all patch-sides with Boundary conditions
-    typedef typename gsBoundaryConditions<T>::bcRefList bcRefList;
-    for ( typename bcRefList::const_iterator iit =  u.bc().begin();
-          iit != u.bc().end()  ; ++iit )
-    {
-        const boundary_condition<T> * it = &iit->get();
 
+    gsMatrix<T> & fixedDofs = const_cast<expr::gsFeSpace<T>&>(u).fixedPart();
+
+    // Iterate over all patch-sides with Boundary conditions
+    typedef gsBoundaryConditions<T> bcList;
+    for ( typename bcList::const_iterator it =  bc.begin("Dirichlet");
+          it != bc.end("Dirichlet") ; ++it )
+    {
         if( it->unknown()!=u.id() ) continue;
 
         const index_t com = it->unkComponent();
         //
         for (index_t r = 0; r!=u.dim(); ++r)
         {
-            if (com!=-1 || r!=com) continue;
-
-            gsMatrix<T> & fixedDofs = const_cast<expr::gsFeSpace<T>&>(u).fixedPart(com);
-            fixedDofs.resize(u.mapper(com).boundarySize(), u.dim() );
+            if (com!=-1 && r!=com) continue;
+            fixedDofs.resize(u.mapper().boundarySize(), u.dim() );
 
             const int k = it->patch();
             const gsBasis<T> & basis = mbasis[k];
@@ -48,8 +97,8 @@ void gsDirichletValuesByInterpolation(const expr::gsFeSpace<T> & u,
             {
                 for (index_t i=0; i!= boundary.size(); ++i)
                 {
-                    const int ii = u.mapper(com).bindex( boundary.at(i) , k );
-                    u.fixedDofs[com].at(ii) = 0;
+                    const int ii = u.mapper().bindex( boundary.at(i) , k, com );
+                    fixedDofs.at(ii) = 0;
                 }
                 continue;
             }
@@ -103,11 +152,11 @@ void gsDirichletValuesByInterpolation(const expr::gsFeSpace<T> & u,
             // Save corresponding boundary dofs
             for (index_t l=0; l!= boundary.size(); ++l)
             {
-                const int ii = u.mapper(com).bindex( boundary.at(l) , it->patch() );
-                u.fixedDofs(com).at(ii) = dVals.at(l);
+                const int ii = u.mapper().bindex( boundary.at(l) , k, com );
+                fixedDofs.at(ii) = dVals.at(l);
             }
         }
     }
-
+}
 
 }; // namespace gismo
