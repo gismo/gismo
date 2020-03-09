0da06ece2574db8a6f7217548edb233473a50048
Andrea Farahat <afarahat@kartoffel.ricint.oeaw.ac.at>
2020-03-09 13:45:25 +0100

    Vertex mapper

--- examples/KirchhoffLoveShell_example.cpp
+++ examples/KirchhoffLoveShell_example.cpp
@@ -126,7 +126,7 @@ int main(int argc, char *argv[])
     gsInfo << numG1Bas.back() << "\n";
 
     gsG1Mapper a(geo, numG1Bas, nPlusDimen);
-    a.printReducedBoundaryBasisEdgeMapper();
+    a.printReducedBasisVertexMapper();
 
 
 
