4740fd220e2bd0103c22397507e3585ef7574b05
weinmueller <pascal.weinmueller@fau.de>
2020-03-11 17:29:09 +0100

    add geometrieeval

---
+++ src/gsG1Basis/gsVisitorNormL2.h
@@ -0,0 +1,132 @@
+/** @file gsNormL2.h
+
+    @brief Computes the L2 norm, needs for the parallel computing.
+
+    This file is part of the G+Smo library.
+
+    This Source Code Form is subject to the terms of the Mozilla Public
+    License, v. 2.0. If a copy of the MPL was not distributed with this
+    file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+    Author(s): P. Weinmüller
+*/
+
+
+#pragma once
+
+namespace gismo
+{
+
+
+template <class T>
+class gsVisitorNormL2
+{
+
+public:
+
+
+    void initialize(const gsBasis<T> & basis,
+                    gsQuadRule<T> & rule,
+                    unsigned      & evFlags) // replace with geoEval ?
+    {
+        // Setup Quadrature
+        const unsigned d = basis.dim();
+        gsVector<index_t> numQuadNodes( d );
+        for (unsigned i = 0; i < d; ++i)
+            numQuadNodes[i] = basis.degree(i) + 1;
+
+        // Setup Quadrature
+        rule = gsGaussRule<T>(numQuadNodes);// harmless slicing occurs here
+
+        // Set Geometry evaluation flags
+        evFlags = NEED_MEASURE| NEED_VALUE;
+    }
+
+    // Evaluate on element.
+    void evaluate(gsGeometryEvaluator<T> & geoEval,
+                  const gsFunction<T>    & _func1,
+                  const gsFunction<T>    & _func2,
+                  gsMatrix<T>            & quNodes)
+    {
+        // Evaluate first function
+        _func1.eval_into(quNodes, f1vals);
+
+        if (g1basis)
+        {
+            index_t n = m_G1Basis.at(geoEval.id()).nPatches();
+
+            for (index_t i = 0; i < n; i++)
+            {
+                f1vals += m_G1Basis.at(geoEval.id()).patch(i).eval(quNodes);
+            }
+        }
+
+        // Compute geometry related values
+        geoEval.evaluateAt(quNodes);
+
+        // Evaluate second function
+        _func2.eval_into( f2param ? quNodes : geoEval.values() , f2vals);
+    }
+
+    // assemble on element
+    T compute(gsDomainIterator<T>    & ,
+              gsGeometryEvaluator<T> & geoEval,
+              gsVector<T> const      & quWeights,
+              T & accumulated)
+    {
+        T sum(0.0);
+        for (index_t k = 0; k < quWeights.rows(); ++k) // loop over quadrature nodes
+        {
+            const T weight = quWeights[k] * geoEval.measure(k);
+            switch (m_p)
+            {
+                case 0: // infinity norm
+                    // .template lpNorm<Eigen::Infinity>();
+                    sum = (f1vals - f2vals).array().abs().maxCoeff();
+                    accumulated = math::max(accumulated, sum);
+                    return sum;
+                    break;
+                case 1:
+                    sum += weight * ( f1vals.col(k) - f2vals.col(k) ).template lpNorm<1>();
+                    break;
+                case 2:
+                    sum += weight * ( f1vals.col(k) - f2vals.col(k) ).squaredNorm();
+                    break;
+                default:
+                    sum += weight * ( f1vals.col(k) - f2vals.col(k) ).array().abs().pow(m_p).sum();
+                    //sum += weight * ( f1vals.col(k) - f2vals.col(k) ).template lpNorm<p>().squared();
+            }
+        }
+
+        accumulated += sum;
+        return sum;
+    }
+
+private:
+
+    index_t m_p;
+
+    gsMatrix<T> f1vals, f2vals;
+
+    bool f2param;
+    bool g1basis;
+
+protected:
+
+    std::vector< gsMultiPatch<>> m_G1Basis;
+    typedef_g1 m_G1Basis_mp;
+};
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
