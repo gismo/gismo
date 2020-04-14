b5d1eea6edebe68ae328b0bfa16d0ea4e094c849
weinmueller <pascal.weinmueller@fau.de>
2020-04-14 16:38:05 +0200

    add bash file

--- filedata/bash_files/run_gismo.sh
+++ filedata/bash_files/run_gismo.sh
@@ -1,8 +1,8 @@
 # Run gismo files
 echo First example 
-make -j g1biharmonic_example && nohup /usr/bin/time -v ./bin/g1biharmonic_example -e 1e-03 -t 12 -g 1 -l 5 -k 4
+make -j g1biharmonic_example && nohup /usr/bin/time -v ./bin/g1biharmonic_example -e 1e-03 -t 12 -g 1 -l 5 -k 4 --latex -p 2
 
 echo Second example
-make -j g1biharmonic_example && nohup /usr/bin/time -v ./bin/g1biharmonic_example -e 1e-03 -t 4 -g 1 -l 1 -k 9 --localEdge
+make -j g1biharmonic_example && nohup /usr/bin/time -v ./bin/g1biharmonic_example -e 1e-03 -t 12 -g 1 -l 5 -k 4 --latex -p 3
 
 echo Finished!
