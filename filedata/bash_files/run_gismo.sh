0f419544faebe5917335b3a9e38cad9b8990c6b7
weinmueller <pascal.weinmueller@fau.de>
2020-04-14 18:07:40 +0200

    change bash file

--- filedata/bash_files/run_gismo.sh
+++ filedata/bash_files/run_gismo.sh
@@ -1,5 +1,5 @@
 # Run gismo files
-echo First example 4a
+echo First example 4b
 make -j g1biharmonic_example && nohup /usr/bin/time -v ./bin/g1biharmonic_example -e 1e-03 -t 12 -g 1 -l 5 -k 4 --latex --localEdge --localGd
 
 echo Second example
