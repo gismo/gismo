f79c05aae4845894229be7b11b9e1599d6ef5ecb
weinmueller <pascal.weinmueller@fau.de>
2020-04-14 18:26:03 +0200

    change bash file

--- filedata/bash_files/run_gismo.sh
+++ filedata/bash_files/run_gismo.sh
@@ -1,14 +1,14 @@
 # Run gismo files
-echo First example 4b
-make -j g1biharmonic_example && nohup /usr/bin/time -v ./bin/g1biharmonic_example -e 1e-03 -t 12 -g 1 -l 5 -k 4 --latex --localEdge --localGd
+echo First example 4d
+make -j g1biharmonic_example && nohup /usr/bin/time -v ./bin/g1biharmonic_example -e 1e-03 -t 12 -g 1 -l 5 -k 4 --latex --localEdge --localVertex --localGd
 
 echo Second example
-make -j g1biharmonic_example && nohup /usr/bin/time -v ./bin/g1biharmonic_example -e 1e-03 -t 12 -g 1 -l 5 -k 4 --latex --localEdge --localGd -p 2
+make -j g1biharmonic_example && nohup /usr/bin/time -v ./bin/g1biharmonic_example -e 1e-03 -t 12 -g 1 -l 5 -k 4 --latex --localEdge --localVertex --localGd -p 2
 
 echo Third example 
-make -j g1biharmonic_example && nohup /usr/bin/time -v ./bin/g1biharmonic_example -e 1e-03 -t 12 -g 1 -l 5 -k 4 --latex --localEdge --localGd -p 3
+make -j g1biharmonic_example && nohup /usr/bin/time -v ./bin/g1biharmonic_example -e 1e-03 -t 12 -g 1 -l 5 -k 4 --latex --localEdge --localVertex --localGd -p 3
 
 echo Forth example
-make -j g1biharmonic_example && nohup /usr/bin/time -v ./bin/g1biharmonic_example -e 1e-03 -t 12 -g 1 -l 5 -k 4 --latex --localEdge --localGd -p 4
+make -j g1biharmonic_example && nohup /usr/bin/time -v ./bin/g1biharmonic_example -e 1e-03 -t 12 -g 1 -l 5 -k 4 --latex --localEdge --localVertex --localGd -p 4
 
 echo Finished!
