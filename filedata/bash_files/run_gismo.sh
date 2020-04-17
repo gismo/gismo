d91debfd5213e60b989689b94b60aac488ce87aa
weinmueller <pascal.weinmueller@fau.de>
2020-04-17 10:38:08 +0200

    change bash file

--- filedata/bash_files/run_gismo.sh
+++ filedata/bash_files/run_gismo.sh
@@ -1,6 +1,6 @@
 # Run gismo files
 echo First example 1a
-make -j g1biharmonic_example && nohup /usr/bin/time -v ./bin/g1biharmonic_example -e 1e-03 -t 12 -g 1 -l 2 -k 79 
+#make -j g1biharmonic_example && nohup /usr/bin/time -v ./bin/g1biharmonic_example -e 1e-03 -t 12 -g 1 -l 2 -k 79 
 
 echo Second example
 make -j g1biharmonic_example && nohup /usr/bin/time -v ./bin/g1biharmonic_example -e 1e-03 -t 12 -g 1 -l 2 -k 79 -p 2
