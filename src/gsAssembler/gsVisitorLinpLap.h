570baaccb50a3d8953ead675a1d51a02a93eae69
Stefan714 <67902268+Stefan714@users.noreply.github.com>
2020-07-31 10:36:41 +0200

    Add files via upload
    Visitor for the linearized p-Laplace equation.

---
+++ src/gsAssembler/gsVisitorLinpLap.h
@@ -0,0 +1,152 @@
+/** @file gsVisitorLinpLap.h
+
+@brief LinpLap equation element visitor.
+
+This file is part of the G+Smo library.
+
+This Source Code Form is subject to the terms of the Mozilla Public
+License, v. 2.0. If a copy of the MPL was not distributed with this
+file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+Author(s): 
+*/
+
+#pragma once
+
+#include <gsAssembler/gsQuadrature.h>
+
+namespace gismo
+{
+
+	/** \brief Visitor for the linearized p-Laplace equation.
+	*
+	* Assembles the bilinear terms
+	* \f[ (\nabla u,\nabla v)_\Omega \text{ and } (f,v)_\Omega \f]
+	* For \f[ u = g \quad on \quad \partial \Omega \f],
+	*
+	*/
+
+	template <class T, bool paramCoef = false>
+	class gsVisitorLinpLap
+	{
+	public:
+
+		/** \brief Constructor for gsVisitorLinpLap.
+		*/
+		gsVisitorLinpLap(const gsPde<T> & pde)
+		{
+			pde_ptr = static_cast<const gsLinpLapPde<T>*>(&pde);
+		}
+
+		void initialize(const gsBasis<T> & basis,
+			const index_t patchIndex,
+			const gsOptionList & options,
+			gsQuadRule<T>    & rule)
+		{
+			// Grab right-hand side for current patch
+			rhs_ptr = &pde_ptr->rhs()->piece(patchIndex);
+
+			// Setup Quadrature
+			rule = gsQuadrature::get(basis, options); // harmless slicing occurs here
+
+													  // Set Geometry evaluation flags
+			md.flags = NEED_VALUE | NEED_MEASURE | NEED_GRAD_TRANSFORM;
+		}
+
+		// Evaluate on element.
+		inline void evaluate(const gsBasis<T>       & basis,
+			const gsGeometry<T>    & geo,
+			const gsMatrix<T>      & quNodes)
+		{
+			md.points = quNodes;
+			// Compute the active basis functions
+			// Assumes actives are the same for all quadrature points on the elements
+			basis.active_into(md.points.col(0), actives);
+			numActive = actives.rows();
+
+			// Evaluate basis functions on element
+			basis.evalAllDers_into(md.points, 1, basisData);
+
+			// Compute image of Gauss nodes under geometry mapping as well as Jacobians
+			geo.computeMap(md);
+
+			// Evaluate right-hand side at the geometry points paramCoef
+			// specifies whether the right hand side function should be
+			// evaluated in parametric(true) or physical (false)
+			rhs_ptr->eval_into((paramCoef ? md.points : md.values[0]), rhsVals);
+
+			// Initialize local matrix/rhs
+			localMat.setZero(numActive, numActive);
+			localRhs.setZero(numActive, rhsVals.rows());//multiple right-hand sides
+		}
+
+		inline void assemble(gsDomainIterator<T>    &,
+			gsVector<T> const      & quWeights)
+		{
+
+			gsMatrix<T> & bVals = basisData[0];
+			gsMatrix<T> & bGrads = basisData[1];
+
+			for (index_t k = 0; k < quWeights.rows(); ++k) // loop over quadrature nodes
+			{
+				// Multiply weight by the geometry measure
+				const T weight = quWeights[k] * md.measure(k);
+
+				// Compute physical gradients at k as a Dim x NumActive matrix
+				transformGradients(md, k, bGrads, physGrad);
+
+				//Access the coefficients of w which correspond to the active basis functions
+				gsMatrix<T> w_(numActive, 1);
+				for (index_t i = 0; i < numActive; i++)
+				{
+					w_(i, 0) = pde_ptr->w(actives(i), 0);
+				}
+
+				//Compute the Gradient of the approximative function w by multiplying the coefficients of w with the physical gradients
+				const gsMatrix<T> wGrad = physGrad * w_;
+
+				localRhs.noalias() += weight * (bVals.col(k) * rhsVals.col(k).transpose());
+				localMat.noalias() += weight * pow(pde_ptr->eps * pde_ptr->eps + (wGrad.transpose() * wGrad).value(),(pde_ptr->p-2)/2) * (physGrad.transpose() * physGrad);
+			}
+		}
+
+		inline void localToGlobal(const index_t                     patchIndex,
+			const std::vector<gsMatrix<T> > & eliminatedDofs,
+			gsSparseSystem<T>               & system)
+		{
+			// Map patch-local DoFs to global DoFs
+			system.mapColIndices(actives, patchIndex, actives);
+
+			// Add contributions to the system matrix and right-hand side
+			system.push(localMat, localRhs, actives, eliminatedDofs.front(), 0, 0);
+		}
+
+	protected:
+		// Pointer to the pde data
+		const gsLinpLapPde<T> * pde_ptr;
+
+	protected:
+		// Basis values
+		std::vector<gsMatrix<T> > basisData;
+		gsMatrix<T>        physGrad;
+		gsMatrix<index_t> actives;
+		index_t numActive;
+
+	protected:
+		// Right hand side ptr for current patch
+		const gsFunction<T> * rhs_ptr;
+
+		// Local values of the right hand side
+		gsMatrix<T> rhsVals;
+
+	protected:
+		// Local matrices
+		gsMatrix<T> localMat;
+		gsMatrix<T> localRhs;
+
+		gsMapData<T> md;
+	};
+
+
+} // namespace gismo
+
