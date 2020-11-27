18ec8ae8e4c61a5cc77b075cc90db39c0beb965c
roeltielen <roeltielen@hotmail.com>
2020-11-27 10:16:41 +0100

    heatEquation_example2 merged into xbraid_heatEquation_example

--- examples/xbraid_heatEquation_example.cpp
+++ examples/xbraid_heatEquation_example.cpp
@@ -39,12 +39,12 @@ class gsXBraid_app : public gsXBraid<T>
     std::string fn("pde/poisson2d_bvp.xml");
     
     // Spatial discretisation parameters
-    index_t numRefine  = 5;
+    index_t numRefine  = 2;
     index_t numElevate = 0;
 
     // Temporal discretisation parameters
-    index_t numTime    = 1;
-    T       tfinal     = 1.0;
+    index_t numTime    = 40;
+    T       tfinal     = 0.1;
     
     // Parallel-in-time multigrid parameters
     index_t CFactor    = 2;
@@ -133,6 +133,106 @@ class gsXBraid_app : public gsXBraid<T>
     if (sequential) app.SetSeqSoln(1); else app.SetSeqSoln(0);
     if (skip) app.SetSkip(1); else app.SetSkip(0);
 
+    /////////////////////////////////////////////////////////////////////////////////////////////
+    //                           Code for heat equation starts here                            //
+    /////////////////////////////////////////////////////////////////////////////////////////////
+
+    // Source function
+    gsConstantFunction<> f(1,2);
+    gsInfo<<"Source function is: "<< f << "\n";
+
+    // Define Geometry, must be a gsMultiPatch object
+    gsMultiPatch<> patches(*gsNurbsCreator<>::BSplineSquareDeg(2));
+    patches.computeTopology();
+
+    // Boundary conditions
+    gsBoundaryConditions<> bcInfo;
+    gsConstantFunction<> g_N(1,2); // Neumann
+    gsConstantFunction<> g_D(0,2); // Dirichlet
+    bcInfo.addCondition(0, boundary::west,  condition_type::neumann  , &g_N);
+    bcInfo.addCondition(0, boundary::east,  condition_type::dirichlet, &g_D);
+    bcInfo.addCondition(0, boundary::north, condition_type::dirichlet, &g_D);
+    bcInfo.addCondition(0, boundary::south, condition_type::dirichlet, &g_D);
+
+    gsMultiBasis<> refine_bases( patches );
+
+    // Elevate and p-refine the basis to order k + numElevate
+    // where k is the highest degree in the bases
+    if ( numElevate > -1 )
+    {
+        // Find maximum degree with respect to all the variables
+        int tmp = refine_bases.maxDegree(0);
+        for (short_t j = 1; j < patches.parDim(); ++j )
+            if ( tmp < refine_bases.maxDegree(j) )
+                tmp = refine_bases.maxDegree(j);
+
+        // Elevate all degrees uniformly
+        tmp += numElevate;
+        refine_bases.setDegree(tmp);
+    }
+
+    // h-refine the basis
+    for (int i = 0; i < numRefine; ++i)
+        refine_bases.uniformRefine();
+
+    // A Conjugate Gradient linear solver with a diagonal (Jacobi) preconditionner
+    gsSparseSolver<>::CGDiagonal solver;
+
+    real_t theta = 0.0;
+    gsMatrix<> Sol;
+    real_t Dt = tfinal / numTime ;
+
+    // Generate system matrix and load vector
+    gsInfo<<"Assembling mass and stiffness...\n";
+   
+    gsExprAssembler<> K(1,1);
+    gsExprAssembler<> M(1,1);
+    
+    typedef gsExprAssembler<>::geometryMap geometryMap;
+    typedef gsExprAssembler<>::variable    variable;
+    typedef gsExprAssembler<>::space       space;
+    typedef gsExprAssembler<>::solution    solution;
+
+    K.setIntegrationElements(refine_bases);
+    M.setIntegrationElements(refine_bases);   
+    gsExprEvaluator<> ev_K(K);
+    gsExprEvaluator<> ev_M(M);
+
+    // Set the geometry map
+    geometryMap G_K = K.getMap(patches);
+    geometryMap G_M = M.getMap(patches);
+
+    // Set the discretization space
+    space u_K = K.getSpace(refine_bases);
+    space u_M = M.getSpace(refine_bases);
+    u_K.setInterfaceCont(0);
+    u_M.setInterfaceCont(0);
+    u_K.addBc( bcInfo.get("Dirichlet") );
+    u_M.addBc( bcInfo.get("Dirichlet") );
+
+    // Set the source term
+    variable ff_K = K.getCoeff(f, G_K);
+    variable ff_M = M.getCoeff(f, G_M);
+
+    K.initSystem();
+    M.initSystem();
+    K.assemble( igrad(u_K, G_K) * igrad(u_K, G_K).tr() * meas(G_K), u_K * ff_K * meas(G_K) );
+    M.assemble( u_M * u_M.tr() * meas(G_M), u_M * ff_M * meas(G_M) );
+
+    // Enforce Neumann conditions to right-hand side
+    variable g_Neumann = K.getBdrFunction();
+    K.assembleRhsBc(u_K * g_Neumann.val() * nv(G_K).norm(), bcInfo.neumannSides() );
+
+      for ( int i = 1; i<=numTime; ++i) // for all timesteps
+    {
+        // Compute the system for the timestep i (rhs is assumed constant wrt time)
+        gsInfo<<"Solving timestep "<< i*Dt<<".\n";
+        Sol = solver.compute(M.matrix()+Dt*theta*K.matrix()).solve(Dt*K.rhs()+(M.matrix()-Dt*(1-theta)*K.matrix())*Sol);
+    }
+
+    gsInfo << "Norm of the solution" << std::endl;
+    gsInfo << Sol.norm() << std::endl;
+   
     return app;
   }
   
