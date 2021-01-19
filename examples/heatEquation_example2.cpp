c4558f29deddbdc78de01cfdc844fa54ad4748c1
Matthias Moeller <m.moller@tudelft.nl>
2021-01-19 14:54:15 +0100

    Small improvements of sequantial heat equation

--- examples/heatEquation_example2.cpp
+++ examples/heatEquation_example2.cpp
@@ -18,10 +18,11 @@ using namespace gismo;
 
 int main(int argc, char *argv[])
 {
-    gsCmdLine cmd("Testing the heat equation.");
+  gsCmdLine cmd("Testing the heat equation.");
+  
     // Source function
     gsConstantFunction<> f(1,2);
-    gsInfo<<"Source function is: "<< f << "\n";
+    gsInfo << "Source function is: " << f << "\n";
 
     // Define Geometry, must be a gsMultiPatch object
     gsMultiPatch<> patches(*gsNurbsCreator<>::BSplineSquareDeg(2));
@@ -36,7 +37,7 @@ int main(int argc, char *argv[])
     bcInfo.addCondition(0, boundary::north, condition_type::dirichlet, &g_D);
     bcInfo.addCondition(0, boundary::south, condition_type::dirichlet, &g_D);
 
-    gsMultiBasis<> refine_bases( patches );
+    gsMultiBasis<> bases( patches );
 
     // Number for h-refinement of the computational (trial/test) basis.
     int numRefine  = 2;
@@ -49,25 +50,21 @@ int main(int argc, char *argv[])
     if ( numElevate > -1 )
     {
         // Find maximum degree with respect to all the variables
-        int tmp = refine_bases.maxDegree(0);
+        int tmp = bases.maxDegree(0);
         for (short_t j = 1; j < patches.parDim(); ++j )
-            if ( tmp < refine_bases.maxDegree(j) )
-                tmp = refine_bases.maxDegree(j);
+            if ( tmp < bases.maxDegree(j) )
+                tmp = bases.maxDegree(j);
 
         // Elevate all degrees uniformly
         tmp += numElevate;
-        refine_bases.setDegree(tmp);
+        bases.setDegree(tmp);
     }
 
     // h-refine the basis
     for (int i = 0; i < numRefine; ++i)
-        refine_bases.uniformRefine();
-
-    // A Conjugate Gradient linear solver with a diagonal (Jacobi) preconditionner
-    gsSparseSolver<>::CGDiagonal solver;
+        bases.uniformRefine();
 
     real_t theta = 0.0;
-    gsMatrix<> Sol;
     real_t endTime = 0.1;
     int numSteps = 40;
     
@@ -76,10 +73,8 @@ int main(int argc, char *argv[])
     const std::string baseName("heat_eq_solution");
     gsParaviewCollection collection(baseName);
 
-    std::string fileName;
-
     // Generate system matrix and load vector
-    gsInfo<<"Assembling mass and stiffness...\n";
+    gsInfo << "Assembling mass and stiffness...\n";
    
     gsExprAssembler<> K(1,1);
     gsExprAssembler<> M(1,1);
@@ -89,18 +84,16 @@ int main(int argc, char *argv[])
     typedef gsExprAssembler<>::space       space;
     typedef gsExprAssembler<>::solution    solution;
 
-    K.setIntegrationElements(refine_bases);
-    M.setIntegrationElements(refine_bases);   
-    gsExprEvaluator<> ev_K(K);
-    gsExprEvaluator<> ev_M(M);
+    K.setIntegrationElements(bases);
+    M.setIntegrationElements(bases);   
 
     // Set the geometry map
     geometryMap G_K = K.getMap(patches);
     geometryMap G_M = M.getMap(patches);
 
     // Set the discretization space
-    space u_K = K.getSpace(refine_bases);
-    space u_M = M.getSpace(refine_bases);
+    space u_K = K.getSpace(bases);
+    space u_M = M.getSpace(bases);
     u_K.setInterfaceCont(0);
     u_M.setInterfaceCont(0);
     u_K.addBc( bcInfo.get("Dirichlet") );
@@ -118,12 +111,19 @@ int main(int argc, char *argv[])
     // Enforce Neumann conditions to right-hand side
     variable g_Neumann = K.getBdrFunction();
     K.assembleRhsBc(u_K * g_Neumann.val() * nv(G_K).norm(), bcInfo.neumannSides() );
+
+    // A Conjugate Gradient linear solver with a diagonal (Jacobi) preconditionner
+    gsSparseSolver<>::CGDiagonal solver;
+    gsMatrix<> Sol(M.numDofs(), 1);
     
     for ( int i = 1; i<=numSteps; ++i) // for all timesteps
     {
         // Compute the system for the timestep i (rhs is assumed constant wrt time)
-        gsInfo<<"Solving timestep "<< i*Dt<<".\n";
-        Sol = solver.compute(M.matrix()+Dt*theta*K.matrix()).solve(Dt*K.rhs()+(M.matrix()-Dt*(1-theta)*K.matrix())*Sol);
+        gsInfo << "Solving timestep " << i*Dt << ".\n";
+        Sol = solver.compute(M.matrix() +
+                             Dt*theta*K.matrix()
+                             ).solve(Dt*K.rhs() +
+                                     (M.matrix()-Dt*(1.0-theta)*K.matrix())*Sol);
     }
 
     gsInfo << "Norm of the solution" << std::endl;
