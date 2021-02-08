9972a9b47cff966cb5e4d0cf3d1e02293067630b
Stefan Takacs <26736024+stefantakacs@users.noreply.github.com>
2021-02-08 23:42:58 +0100

    Update multiGrid_example.cpp

--- examples/multiGrid_example.cpp
+++ examples/multiGrid_example.cpp
@@ -92,7 +92,7 @@ int main(int argc, char *argv[])
         return EXIT_FAILURE;
     }
 
-    gsInfo << "Run multiGrid_example with options:\n" << opt << std::endl;
+    gsInfo << "Run multiGrid_example with options:\n" << opt << "\n";
 
     /******************* Define geometry ********************/
 
