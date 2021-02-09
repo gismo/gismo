91232f1ae62d3afc02b9465838c32b7bc74d4102
Stefan Takacs <26736024+stefantakacs@users.noreply.github.com>
2021-02-09 09:06:31 +0100

    Update multiGrid_example.cpp

--- examples/multiGrid_example.cpp
+++ examples/multiGrid_example.cpp
@@ -159,7 +159,7 @@ int main(int argc, char *argv[])
             ++i;
         }
         if ( len > i )
-            gsInfo << "\nToo much boundary conditions have been specified. Ingnoring the remaining ones.\n";
+            gsInfo << "\nToo many boundary conditions have been specified. Ingnoring the remaining ones.\n";
         gsInfo << "done. "<<i<<" boundary conditions set.\n";
     }
 
