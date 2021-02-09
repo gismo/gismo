1897b55c343e05d1ab7dfe4174b3f447ec851511
Stefan Takacs <stefan.takacs@ricam.oeaw.ac.at>
2021-02-09 14:52:52 +0100

    fixes

--- src/gsMultiGrid/gsGridHierarchy_.cpp
+++ src/gsMultiGrid/gsGridHierarchy_.cpp
@@ -1,16 +1,4 @@
-/** @file gsGridHierarchy_.cpp
-
-    @brief Coarsening algorithms for knot vectors and bases.
-
-    This file is part of the G+Smo library.
-
-    This Source Code Form is subject to the terms of the Mozilla Public
-    License, v. 2.0. If a copy of the MPL was not distributed with this
-    file, You can obtain one at http://mozilla.org/MPL/2.0/.
-
-    Author(s): C. Hofreither, S. Takacs
-*/
-
+#include <gsMultiGrid/gsGridHierarchy.h>
 #include <gsMultiGrid/gsGridHierarchy.hpp>
 
 namespace gismo
