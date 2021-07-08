69a085775fc330f12195aca0aa219dd19d400235
Stefan Takacs <26736024+stefantakacs@users.noreply.github.com>
2021-07-08 23:05:07 +0200

    Improvements for gsVisitorDg and gsVisitorNitsche and others (#462)
    Improvements for gsVisitorDg and gsVisitorNitsche:
     * By default, the grid size on the physical domain should be considered.
     * Make penalty coefficient freely configurable
     * Introduce additional parameters alpha and beta to make other DG-types (like NIPG) possible
    
    Improvements for gsGenericAssembler:
     * Allow to assemble all terms of interest separately
     * Allow to suppress refreshing, which allows successive assembling
     * Allow to provide custom gsDofMapper
    
    Follow-up changes for assemblers:
     * Provide default options from gsVisitorDg and gsVisitorNitsche, if applicable
    
    Moreover:
     * Improve doxygen (make visitors visible and put them now consistently to assembler module)
     * Provide hpp-file now for all assemblers (so there should be no header-dependence of examples on visitors, which should improve compilation times.)

--- src/gsAssembler/gsPoissonAssembler.h
+++ src/gsAssembler/gsPoissonAssembler.h
@@ -40,6 +40,9 @@ public:
 
 public:
 
+    /// Returns the list of default options for assembly
+    static gsOptionList defaultOptions();
+
     gsPoissonAssembler()
     { }
 
@@ -51,6 +54,7 @@ public:
     gsPoissonAssembler( const gsPoissonPde<T>          & pde,
                         const gsMultiBasis<T>          & bases)
     {
+        m_options = defaultOptions();
         Base::initialize(pde, bases, m_options);
     }
 
@@ -67,6 +71,7 @@ public:
                         dirichlet::strategy           dirStrategy,
                         iFace::strategy               intStrategy = iFace::glue)
     {
+        m_options = defaultOptions();
         m_options.setInt("DirichletStrategy", dirStrategy);
         m_options.setInt("InterfaceStrategy", intStrategy);
 
@@ -92,6 +97,7 @@ public:
                         dirichlet::strategy           dirStrategy = dirichlet::elimination,
                         iFace::strategy               intStrategy = iFace::glue)
     {
+        m_options = defaultOptions();
         m_options.setInt("DirichletStrategy", dirStrategy);
         m_options.setInt("InterfaceStrategy", intStrategy);
 
--- src/gsAssembler/gsVisitorDg.h
+++ src/gsAssembler/gsVisitorDg.h
@@ -1,6 +1,7 @@
 /** @file gsVisitorDg.h
 
-    @brief A DG interface visitor for the Poisson problem .
+    @brief Visitor for adding the interface conditions for the interior
+    penalty methods of the Poisson problem.
 
     This file is part of the G+Smo library.
 
@@ -15,56 +16,121 @@
 
 namespace gismo
 {
-/** @brief
-    Implementation of a interface condition for the
-    discontinuous Galerkin Assembler.
-
-    It sets up an assembler and assembles the system patch wise and
-    combines the patch-local stiffness matrices into a global system.
-    Dirichlet boundary can also be imposed weakly (i.e. Nitsche).
-*/
+   /** \brief Visitor for adding the interface conditions for the interior
+     * penalty methods of the Poisson problem.
+     *
+     * This visitor adds the following term to the left-hand side (bilinear form):
+     * \f[
+     *     s_{k,\ell}(u,v) :=
+     *        - \alpha \int_{\Gamma^{(k,\ell)}}  \{\nabla u\} \cdot \mathbf{n} [ v ]
+     *        - \beta  \int_{\Gamma^{(k,\ell)}}  \{\nabla v\} \cdot \mathbf{n} [ u ]
+     *        + \delta ( h_k^{-1} + h_\ell^{-1}) \int_{\Gamma^{(k,\ell)}}  [ u ][ v ],
+     * \f]
+     * where \f$ v \f$ is the test function and \f$ u \f$ is trial function,
+     * \f$ [u] = u^{(k)} - u^{(\ell)} \f$ denotes the jump accross the interface
+     * and \f$ \{ u \} = (u^{(k)} + u^{(\ell)})/2\f$ denotes the average between
+     * the two patches.
+     *
+     * The default values for DG.Alpha and DG.Beta are \f$\alpha=\beta=1\f$,
+     * which corresponds to the Symmetric Interior Penalty discontinuous Galerkin
+     * (SIPG) method. The default value for DG.Penalty is -1, which yields
+     * \f$\delta=4(p+d)(p+1)\f$. If a positive value for DG.Penalty is chosen,
+     * that value is taken for \f$\delta\f$.
+     *
+     * The (normal) grid sizes are estimated based on the geometry function. Use
+     * the option DG.ParameterGridSize to just use the grid size on the parameter
+     * domain.
+     *
+     * An analogous visitor for handling the Dirichlet boundary conditions weakly,
+     * is the \a gsVisitorNitsche.
+     *
+     * \ingroup Assembler
+     */
 
 template <class T>
 class gsVisitorDg
 {
 public:
 
-   /** \brief Visitor for adding the interface conditions for the
-     * interior penalty method of the Poisson problem.
-     *
-     * This visitor adds the following term to the left-hand side (bilinear form).
-     * \f[ - \{\nabla u\} \cdot \mathbf{n} [ v ]
-     *     - \{\nabla v\} \cdot \mathbf{n} [ u ]
-     *     + \alpha [ u ][  v ] \f].
-     * Where \f[v\f] is the test function and \f[ u \f] is trial function.
-     */
-
-    gsVisitorDg(const gsPde<T> &)
+    /// Constructor
+    gsVisitorDg()
+    : m_pde(NULL)
     {}
 
+    /// @brief Constructor
+    ///
+    /// @param pde     Reference to \a gsPde object
+    gsVisitorDg(const gsPde<T> & pde)
+    : m_pde(&pde)
+    {}
+
+    /// Default options
+    static gsOptionList defaultOptions()
+    {
+        gsOptionList options;
+        options.addReal  ("DG.Alpha",
+                          "Parameter alpha for dG scheme; use 1 for SIPG and NIPG.",                              1);
+        options.addReal  ("DG.Beta",
+                          "Parameter beta for dG scheme; use 1 for SIPG and -1 for NIPG",                         1);
+        options.addReal  ("DG.Penalty",
+                          "Penalty parameter penalty for dG scheme; if negative, default 4(p+d)(p+1) is used.",  -1);
+        options.addSwitch("DG.ParameterGridSize",
+                          "Use grid size on parameter domain for the penalty term.",                          false);
+        return options;
+    }
+
+    /// Initialize
     void initialize(const gsBasis<T> & basis1,
-                    const gsBasis<T> & ,
+                    const gsBasis<T> & basis2,
                     const boundaryInterface & bi,
                     const gsOptionList & options,
                     gsQuadRule<T> & rule)
     {
-        side1 = bi.first().side();
+        m_side1 = bi.first();
+        m_side2 = bi.second();
 
         // Setup Quadrature
-        rule = gsQuadrature::get(basis1, options, side1.direction());
+        rule = gsQuadrature::get(basis1, options, m_side1.direction());
 
-        // Compute penalty parameter
-        const int deg = basis1.maxDegree();
-        penalty = (deg + basis1.dim()) * (deg + 1) * T(2.0);
+        m_penalty     = options.askReal("DG.Penalty",-1);
+        // If not given, use default
+        if (m_penalty<0)
+        {
+            const index_t deg = math::max( basis1.maxDegree(), basis2.maxDegree() );
+            m_penalty = T(4) * (deg + basis1.dim()) * (deg + 1);
+        }
+
+        m_alpha     = options.askReal("DG.Alpha", 1);
+        m_beta      = options.askReal("DG.Beta" , 1);
+
+        if (options.getSwitch("DG.ParameterGridSize"))
+        {
+            m_h1    = basis1.getMinCellLength();
+            m_h2    = basis2.getMinCellLength();
+        }
+        else
+        {
+            GISMO_ENSURE (m_pde, "gsVisitorDg::initialize: No PDE given.");
+            m_h1    = estimateSmallestPerpendicularCellSize(
+                          basis1,
+                          m_pde->patches()[m_side1.patch],
+                          m_side1
+                      );
+            m_h2    = estimateSmallestPerpendicularCellSize(
+                          basis2,
+                          m_pde->patches()[m_side2.patch],
+                          m_side2
+                      );
+        }
 
         // Set Geometry evaluation flags
         md1.flags = md2.flags = NEED_VALUE|NEED_JACOBIAN|NEED_GRAD_TRANSFORM;
     }
 
-    // Evaluate on element.
-    inline void evaluate(const gsBasis<T>       & B1, // to do: more unknowns
+    /// Evaluate on element
+    inline void evaluate(const gsBasis<T>       & B1,
                          const gsGeometry<T>    & geo1,
-                         const gsBasis<T>       & B2, // to do: more unknowns
+                         const gsBasis<T>       & B2,
                          const gsGeometry<T>    & geo2,
                          const gsMatrix<T>      & quNodes1,
                          const gsMatrix<T>      & quNodes2)
@@ -78,8 +144,8 @@ public:
         const index_t numActive2 = actives2.rows();
 
         // Evaluate basis functions and their first derivatives
-        B1.evalAllDers_into( md1.points, 1, basisData1);
-        B2.evalAllDers_into( md2.points, 1, basisData2);
+        B1.evalAllDers_into(md1.points, 1, basisData1);
+        B2.evalAllDers_into(md2.points, 1, basisData2);
 
         // Compute image of Gauss nodes under geometry mapping as well as Jacobians
         geo1.computeMap(md1);
@@ -92,7 +158,7 @@ public:
         E22.setZero(numActive2, numActive2); E21.setZero(numActive2, numActive1);
     }
 
-    // assemble on element
+    /// Assemble on element
     inline void assemble(gsDomainIterator<T>    & element1,
                          gsDomainIterator<T>    & element2,
                          gsVector<T>            & quWeights)
@@ -103,8 +169,9 @@ public:
         for (index_t k = 0; k < quWeights.rows(); ++k) // loop over quadrature nodes
         {
             // Compute the outer normal vector from patch1
-            outerNormal(md1, k, side1, unormal);
+            outerNormal(md1, k, m_side1, unormal);
 
+            // Multiply quadrature weight by the geometry measure
             // Integral transformation and quadrature weight (patch1)
             // assumed the same on both sides
             const T weight = quWeights[k] * unormal.norm();
@@ -113,10 +180,8 @@ public:
             unormal.normalize();
 
             // Take blocks of values and derivatives of basis functions
-            //const typename gsMatrix<T>::Column val1 = basisData1.col(k);//bug
             const typename gsMatrix<T>::Block val1 = basisData1[0].block(0,k,numActive1,1);
             gsMatrix<T> & grads1 = basisData1[1];// all grads
-            //const typename gsMatrix<T>::Column val2 = basisData2.col(k);//bug
             const typename gsMatrix<T>::Block val2 = basisData2[0].block(0,k,numActive2,1);
             gsMatrix<T> & grads2 = basisData2[1];// all grads
 
@@ -125,26 +190,26 @@ public:
             transformGradients(md2, k, grads2, phGrad2);
 
             // Compute element matrices
-            const T c1     = weight * T(0.5);
+            const T c1     = weight / T(2);
             N1.noalias()   = unormal.transpose() * phGrad1;
             N2.noalias()   = unormal.transpose() * phGrad2;
+
             B11.noalias() += c1 * ( val1 * N1 );
             B12.noalias() += c1 * ( val1 * N2 );
-            B22.noalias() -= c1 * ( val2 * N2 );
             B21.noalias() -= c1 * ( val2 * N1 );
+            B22.noalias() -= c1 * ( val2 * N2 );
 
-            const T h1     = element1.getCellSize();
-            const T h2     = element2.getCellSize();
-            // Maybe, the h should be scaled with the patch diameter, since its the h from the parameterdomain.
-            const T c2     = weight * penalty * 2*(1./h1 + 1./h2);
+            const T c2     = weight * m_penalty * (1./m_h1 + 1./m_h2);
 
             E11.noalias() += c2 * ( val1 * val1.transpose() );
             E12.noalias() += c2 * ( val1 * val2.transpose() );
             E22.noalias() += c2 * ( val2 * val2.transpose() );
             E21.noalias() += c2 * ( val2 * val1.transpose() );
+
         }
     }
-    
+
+    /// Adds the contributions to the sparse system
     inline void localToGlobal(const index_t                     patch1,
                               const index_t                     patch2,
                               const std::vector<gsMatrix<T> > & eliminatedDofs,
@@ -157,26 +222,67 @@ public:
         m_localRhs1.setZero(actives1.rows(),system.rhsCols());
         m_localRhs2.setZero(actives2.rows(),system.rhsCols());
 
-        system.push(-B11 - B11.transpose() + E11, m_localRhs1,actives1,actives1,eliminatedDofs.front(),0,0);
-        system.push(-B21 - B12.transpose() - E21, m_localRhs2,actives2,actives1,eliminatedDofs.front(),0,0);
-        system.push(-B12 - B21.transpose() - E12, m_localRhs1,actives1,actives2,eliminatedDofs.front(),0,0);
-        system.push(-B22 - B22.transpose() + E22, m_localRhs2,actives2,actives2,eliminatedDofs.front(),0,0);
-
+        system.push(-m_alpha*B11 - m_beta*B11.transpose() + E11, m_localRhs1,actives1,actives1,eliminatedDofs.front(),0,0);
+        system.push(-m_alpha*B21 - m_beta*B12.transpose() - E21, m_localRhs2,actives2,actives1,eliminatedDofs.front(),0,0);
+        system.push(-m_alpha*B12 - m_beta*B21.transpose() - E12, m_localRhs1,actives1,actives2,eliminatedDofs.front(),0,0);
+        system.push(-m_alpha*B22 - m_beta*B22.transpose() + E22, m_localRhs2,actives2,actives2,eliminatedDofs.front(),0,0);
     }
+
+    /// Estimates the gird size perpendicular to the given side on the physical domain, as required for SIPG and Nitsche
+    static T estimateSmallestPerpendicularCellSize(const gsBasis<T> & basis,
+                                                   const gsGeometry<T> & geo,
+                                                   patchSide side)
+    {
+        T result = 0;
+        bool first = true;
+        typename gsDomainIterator<T>::uPtr domIt = basis.makeDomainIterator(side);
+
+        for (gsDomainIterator<T>& element = *domIt; element.good(); element.next() )
+        {
+            gsVector<T> parameterCenter = element.centerPoint();
+            const gsMatrix<T> center1 = geo.eval(parameterCenter);
+
+            parameterCenter(side.direction()) += ( side.parameter() == 0 ? 1 : -1 )
+                                                 * element.getPerpendicularCellSize();
+            const gsMatrix<T> center2 = geo.eval(parameterCenter);
+
+            const real_t diff = (center1 - center2).norm();
+
+            if (first || result > diff)
+                result = diff;
+
+            first = false;
+        }
+        return result;
+    }
+
+
 
 private:
 
-    // Penalty constant
-    T penalty;
+    /// The underlying PDE
+    const gsPde<T> * m_pde;
 
-    // Side
-    boxSide side1;
+    /// Parameter \f$\alpha\f$ for the bilinear form
+    T m_alpha;
+
+    /// Parameter \f$\beta\f$ for the bilinear form
+    T m_beta;
+
+    /// Parameter \f$\delta\f$ for the bilinear form
+    T m_penalty;
+
+    /// Side on first patch that corresponds to interface
+    patchSide m_side1;
+
+    /// Side on second patch that corresponds to interface
+    patchSide m_side2;
 
 private:
 
     // Basis values etc
     std::vector<gsMatrix<T> > basisData1, basisData2;
-    gsMatrix<T>        phGrad1   , phGrad2;
+    gsMatrix<T>       phGrad1   , phGrad2;
     gsMatrix<index_t> actives1  , actives2;
 
     // Outer normal
@@ -185,10 +291,13 @@ private:
     // Auxiliary element matrices
     gsMatrix<T> B11, B12, E11, E12, N1,
                 B22, B21, E22, E21, N2;
-                
+
     gsMatrix<T> m_localRhs1, m_localRhs2;
 
     gsMapData<T> md1, md2;
+
+    // Grid sizes
+    T m_h1, m_h2;
 };
 
 
