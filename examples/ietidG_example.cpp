69440abb787086f533aec0d708ac4b5d990661d1
Stefan Takacs <stefan.takacs@numa.uni-linz.ac.at>
2021-12-29 10:25:30 +0100

    merge related fixes

--- examples/ietidG_example.cpp
+++ examples/ietidG_example.cpp
@@ -68,8 +68,6 @@ int main(int argc, char *argv[])
 
     try { cmd.getValues(argc,argv); } catch (int rv) { return rv; }
 
-    gsOptionList opt = cmd.getOptionList();
-
     if ( ! gsFileManager::fileExists(geometry) )
     {
         gsInfo << "Geometry file could not be found.\n";
@@ -77,7 +75,7 @@ int main(int argc, char *argv[])
         return EXIT_FAILURE;
     }
 
-    gsInfo << "Run ieti_example with options:\n" << opt << std::endl;
+    gsInfo << "Run ieti_example with options:\n" << cmd << std::endl;
 
     /******************* Define geometry ********************/
 
@@ -423,7 +421,7 @@ int main(int argc, char *argv[])
     // This is the main cg iteration
     //! [Solve]
     gsConjugateGradient<> PCG( ieti.schurComplement(), prec.preconditioner() );
-    PCG.setOptions( opt.getGroup("Solver") ).solveDetailed( rhsForSchur, lambda, errorHistory );
+    PCG.setOptions( cmd.getGroup("Solver") ).solveDetailed( rhsForSchur, lambda, errorHistory );
     //! [Solve]
 
     gsInfo << "done.\n    Reconstruct solution from Lagrange multipliers... " << std::flush;
@@ -458,7 +456,7 @@ int main(int argc, char *argv[])
     {
         gsFileData<> fd;
         std::time_t time = std::time(NULL);
-        fd.add(opt);
+        fd.add(cmd);
         fd.add(uVec);
         gsMatrix<> mat; ieti.saddlePointProblem()->toMatrix(mat); fd.add(mat);
         fd.addComment(std::string("ietidG_example   Timestamp:")+std::ctime(&time));
