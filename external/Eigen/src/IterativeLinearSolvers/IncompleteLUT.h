1b7231aae0a357213286117cf8186afe1f0d56f9
Matthias Möller <m.moller@tudelft.nl>
2022-05-17 07:44:15 +0200

    Changed some attributes from protected to public as they are needed by the p-multigrid solver

--- external/Eigen/src/IterativeLinearSolvers/IncompleteLUT.h
+++ external/Eigen/src/IterativeLinearSolvers/IncompleteLUT.h
@@ -197,14 +197,20 @@ protected:
       }
     };
 
-protected:
-
+public:
+  
     FactorType m_lu;
+  
+protected:
+  
     RealScalar m_droptol;
     int m_fillfactor;
     bool m_analysisIsOk;
     bool m_factorizationIsOk;
     ComputationInfo m_info;
+
+public:
+  
     PermutationMatrix<Dynamic,Dynamic,StorageIndex> m_P;     // Fill-reducing permutation
     PermutationMatrix<Dynamic,Dynamic,StorageIndex> m_Pinv;  // Inverse permutation
 };
