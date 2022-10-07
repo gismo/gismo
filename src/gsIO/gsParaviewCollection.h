24da830f70343b57de2ca52ff94190aa8685c341
Christos Karampatzakis <chriskarampatzakis@gmail.com>
2022-10-07 17:59:26 +0300

    Added gsParaviewDataSet class.

--- src/gsIO/gsParaviewCollection.h
+++ src/gsIO/gsParaviewCollection.h
@@ -14,7 +14,9 @@
 #pragma once
 
 #include <gsCore/gsForwardDeclarations.h>
-#include <gsAssembler/gsExpressions.h>
+#include <gsMSplines/gsMappedBasis.h>
+#include <gsAssembler/gsExprHelper.h>
+#include <gsAssembler/gsExprEvaluator.h>
 
 #include<fstream>
 
@@ -27,44 +29,79 @@ class gsParaviewDataSet // a collection of .vts files
 
 public:
     typedef std::string String;
-    int time;
+    int m_numPatches;
 private:
-    int numPatches;
+    index_t plotPrec;
     int part;
-    String filename;
+    String m_basename;
     gsExprHelper<real_t>::geometryMap m_geoMap;
+    gsExprEvaluator<real_t> m_evaltr;
     // Will only be called by save() function, to make sure it is the last element in the file
     void outputGeometry(){
         // file << "</PointData>/n";
-
+        // m_evaltr.geo2points(m_geoMap);
     }
 public:
 
-    gsParaviewDataSet(gsExprHelper<real_t>::geometryMap geoMap):m_geoMap(geoMap)
+    gsParaviewDataSet(std::string basename, gsExprHelper<real_t>::geometryMap geoMap,
+                      gsExprEvaluator<real_t> eval)
+                      :m_basename(basename),
+                       m_geoMap(geoMap),
+                       m_evaltr(eval)
     {
-        numPatches = m_geoMap.source().nPieces();
-        gsDebugVar( numPatches);
-        // initializes individual vts files
-        // for every patch
-        // {
-        //     std::ofstream file(filename.c_str());
-        //     file << std::fixed; // no exponents
-        //     file << std::setprecision (PLOT_PRECISION);
-        //     file <<"<?xml version=\"1.0\"?>\n";
-        //     file <<"<VTKFile type=\"StructuredGrid\" version=\"0.1\">\n";
-        //     file <<"<StructuredGrid WholeExtent=\"0 "<< np(0)-1<<" 0 "<< np1 <<" 0 "
-        //         << np2 <<"\">\n";
-        //     file <<"<Piece Extent=\"0 "<< np(0)-1<<" 0 "<<np1<<" 0 "
-        //         << np2 <<"\">\n";
-        //     file <<"<PointData>\n";
-        // }
+        m_numPatches = m_geoMap.source().nPieces(); // Number of patches
+        //gsDebugVar( numPatches);
+
+        unsigned nPts = m_evaltr.options().askInt("plot.npts", 1000);
+
+        // QUESTION: Can I be certain that the ids are consecutive?
+        for ( index_t k=0; k!=m_numPatches; k++) // For every patch.
+        {
+            gsMatrix<real_t> activeBases = m_geoMap.source().piece(k).support();
+            gsGridIterator<real_t,CUBE> pt(activeBases, nPts);
+
+            const gsVector<index_t> & np( pt.numPointsCwise() );
+            index_t np1 = (np.size()>1 ? np(1)-1 : 0);
+            index_t np2 = (np.size()>2 ? np(2)-1 : 0);
+
+            // initializes individual .vts files
+            // for every patch
+            std::string filename;
+            filename = m_basename + "_patch" +std::to_string(k)+".vts";
+            std::ofstream file(filename.c_str());
+            file << std::fixed; // no exponents
+            file << std::setprecision(5); // PLOT_PRECISION
+            file <<"<?xml version=\"1.0\"?>\n";
+            file <<"<VTKFile type=\"StructuredGrid\" version=\"0.1\">\n";
+            file <<"<StructuredGrid WholeExtent=\"0 "<< np(0)-1<<" 0 "<< np1 <<" 0 "
+                << np2 <<"\">\n";
+            file <<"<Piece Extent=\"0 "<< np(0)-1<<" 0 "<<np1<<" 0 "
+                << np2 <<"\">\n";
+            file <<"<PointData>\n";
+            file.close();
+        }
+
     }
 
-    // void addField(expr, String label){
-    //     // evaluates the expression and appends it to the vts files
+    template<class E>
+    void addField(const expr::_expr<E> & expr, String label){
+        // evaluates the expression and appends it to the vts files
+        //for every patch
+        std::vector<std::string> tags = m_evaltr.expr2vtk(expr, label);
+        for ( index_t k=0; k!=m_numPatches; k++) // For every patch.
+        {
+            std::string filename;
+            filename = m_basename + "_patch" +std::to_string(k)+".vts";
+            std::ofstream file;
+            file.open(filename.c_str(), std::ios_base::app); // Append to file
+            file << std::fixed; // no exponents
+            file << std::setprecision(5); // PLOT_PRECISION
 
-    //     //for every patch
-    // };
+            file << tags[k];
+            file.close(); 
+        }
+
+    };
 
     // void addFields( expr... )
     // {
@@ -73,8 +110,34 @@ public:
     //     // see: https://stackoverflow.com/questions/3386861/converting-a-variable-name-to-a-string-in-c
     // }
 
+
+    std::vector<std::string> filenames()
+    {   std::vector<std::string> names;
+        for ( index_t k=0; k!=m_numPatches; k++) // For every patch.
+        {
+            names.push_back( m_basename + "_patch" +std::to_string(k)+".vts" );
+        }
+        return names;
+    }
+
+
     void save(){
-        outputGeometry();
+        // QUESTION: Can I be certain that the ids are consecutive?
+        for ( index_t k=0; k!=m_numPatches; k++) // For every patch.
+        {
+            std::string filename;
+            filename = m_basename + "_patch" +std::to_string(k)+".vts";
+            std::ofstream file;
+            file.open(filename.c_str(), std::ios_base::app); // Append to file 
+            file << std::fixed; // no exponents
+            file << std::setprecision(5); // PLOT_PRECISION
+
+
+            file <<"</PointData>\n";
+            outputGeometry();
+            file << "</Piece>\n</StructuredGrid>\n</VTKFile>";
+            file.close();
+        }
         // output text files for each part.
     };
 
@@ -114,7 +177,16 @@ public:
 public:
 
     /// Constructor using a filename ( without extension ).
-    gsParaviewCollection(String const  &fn/*,gsExprEvaluator<> evaluator*/)
+    gsParaviewCollection(String const  &fn)
+    : mfn(fn), counter(0) //, m_evaluator(evaluator)
+    {
+        mfile <<"<?xml version=\"1.0\"?>\n";
+        mfile <<"<VTKFile type=\"Collection\" version=\"0.1\">";
+        mfile <<"<Collection>\n";
+    }
+
+    /// Constructor using a filename ( without extension ) and an evaluator.
+    gsParaviewCollection(String const  &fn, gsExprEvaluator<> evaluator)
     : mfn(fn), counter(0), m_evaluator(evaluator)
     {
         mfile <<"<?xml version=\"1.0\"?>\n";
@@ -169,16 +241,18 @@ public:
 
     // A data set is meant to be an abstraction for multiple <DataSet> tags in paraview, 
     // that all stem from the same gsGeometryMap, and refer to the same timestep.
-    void addDataSet(gsParaviewDataSet dataSet)
+    void addDataSet(gsParaviewDataSet dataSet, real_t time)
     {
         dataSet.save(); // the actual files are written to disk/finalized
-        for (index_t i=0; i!=dataSet.numPatches; i++)
+        std::vector<std::string> filenames( dataSet.filenames() );
+
+        for (index_t i=0; i!=filenames.size(); i++)
         {
-            addPart( mfn + std::to_string(i), dataSet.time);
+            addPart( mfn + std::to_string(i), time);
         }
     }
 
-    gsParaviewDataSet newTimeStep(real_t time=-1)
+    void /*gsParaviewDataSet*/ newTimeStep(real_t time=-1)
     {
         // TODO:
         // This will return an empty dataSet that has a proper filename according
@@ -186,6 +260,7 @@ public:
 
         // The user will then add all the desired fields to it, and then execute
         // addDataSet() to append it to the pvd file.
+        // return gsParaviewDataSet("", ,m_evaluator); 
     }
 
     /// Finalizes the collection by closing the XML tags, always call
@@ -234,16 +309,16 @@ private:
 /// collection is just fn.pvd and contains only the file fn.ext
 inline void makeCollection(std::string const & fn, std::string const & ext, int n = 0)
 {
-    // gsParaviewCollection pc(fn);
-    // if ( n > 0)
-    // {
-    //     for (int i=0; i<n ; i++)
-    //         pc.addPart(fn, i, ext);
-    // }
-    // else
-    //     pc.addPart(fn, ext);
+    gsParaviewCollection pc(fn);
+    if ( n > 0)
+    {
+        for (int i=0; i<n ; i++)
+            pc.addPart(fn, i, ext);
+    }
+    else
+        pc.addPart(fn, ext);
 
-    // pc.save();
+    pc.save();
 }
 
 
