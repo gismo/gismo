fcd9e3c712a846faa8a442adade01bea52eafbb9
Christos Karampatzakis <chriskarampatzakis@gmail.com>
2022-12-15 13:46:34 +0200

    Implemented gsParaviewDataSet::toVTK.

--- src/gsIO/gsParaviewDataSet.h
+++ src/gsIO/gsParaviewDataSet.h
@@ -92,5 +92,56 @@ public:
 
     gsOptionList & options() {return m_options;}
 
+
+    /// @brief  Evaluates gsFunctionSet over all pieces( patches ) and returns all <DataArray> xml tags as a vector of strings
+    /// @tparam T 
+    /// @param funSet gsFunctionSet to be evaluated
+    /// @param nPts   Number of evaluation points, per patch.
+    /// @param precision Number of decimal points in xml output
+    /// @return Vector of strings of all <DataArrays>
+    template< class T>
+    static std::vector<std::string> toVTK(const gsFunctionSet<T> & funSet, unsigned nPts=1000, unsigned precision=5)
+    {   
+        std::vector<std::string> out;
+        std::stringstream dataArray;
+        dataArray.setf( std::ios::fixed ); // write floating point values in fixed-point notation.
+        dataArray.precision(precision);
+
+        gsMatrix<T> evalPoint;
+
+        // Loop over all patches
+        for ( index_t i=0; i != funSet.nPieces(); ++i )
+        {
+            gsGridIterator<T,CUBE> grid(funSet.basis(i).support(), nPts);
+
+            // Evaluate the MultiPatch for every parametric point of the grid iterator
+            gsMatrix<T> xyzPoints( funSet.targetDim(), grid.numPoints());
+            index_t col = 0;
+            for( grid.reset(); grid; ++grid )
+            {
+                evalPoint = *grid; // ..
+                xyzPoints.col(col) =  funSet.piece(i).eval(evalPoint);
+                col++;
+            }
+
+            // Format as vtk xml string
+            dataArray <<"<DataArray type=\"Float32\" NumberOfComponents=\"3\">\n";
+            // For every point
+            for ( index_t j=0; j<xyzPoints.cols(); ++j)
+            {
+                for ( index_t i=0; i!=xyzPoints.rows(); ++i)
+                    dataArray<< xyzPoints(i,j) <<" ";
+                for ( index_t i=xyzPoints.rows(); i<3; ++i)
+                    dataArray<<"0 ";
+            }
+            dataArray <<"\n</DataArray>\n";
+
+            out.push_back( dataArray.str() );
+            dataArray.str(std::string()); // Clear the dataArray stringstream
+        }
+        return out; 
+    }
+    
+
 };
 } // End namespace gismo
