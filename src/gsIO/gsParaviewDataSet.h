7282957565b65bccd99a7137a8751544eb9de0f5
Christos Karampatzakis <chriskarampatzakis@gmail.com>
2022-12-28 13:16:56 +0200

    Introduced new options to gsParaviewDataSet

--- src/gsIO/gsParaviewDataSet.h
+++ src/gsIO/gsParaviewDataSet.h
@@ -162,6 +162,8 @@ public:
         gsOptionList opt;
         opt.addInt("numPoints", "Number of points per-patch.", 1000);
         opt.addInt("precision", "Number of decimal digits.", 5);
+        opt.addInt("plotElements.resolution", "Drawing resolution for element mesh.", -1);
+        opt.addString("subfolder","Name of subfolder where the vtk files will be stored.", "");
         opt.addSwitch("plotElements", "Controls plotting of element mesh.", false);
         opt.addSwitch("plotControlNet", "Controls plotting of control point grid.", false);
         return opt;
