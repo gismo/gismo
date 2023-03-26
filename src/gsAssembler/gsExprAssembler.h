3e352f4be2d811daaac1d16f212c1826cfb8a02a
Hugo Verhelst <hugoverhelst1995@gmail.com>
2023-03-26 19:07:01 +0200

    Revert "parallel assembleifc"
    This reverts commit 223b7a41447a67277cd086a080ff35f6b25238b5.

--- src/gsAssembler/gsExprAssembler.h
+++ src/gsAssembler/gsExprAssembler.h
@@ -793,8 +793,6 @@ void gsExprAssembler<T>::assembleIfc(const ifContainer & iFaces, expr... args)
 {
     GISMO_ASSERT(matrix().cols()==numDofs(), "System not initialized");
 
-#pragma omp parallel
-{
     typedef typename gsFunction<T>::uPtr ifacemap;
 
     auto arg_tpl = std::make_tuple(args...);
@@ -809,7 +807,6 @@ void gsExprAssembler<T>::assembleIfc(const ifContainer & iFaces, expr... args)
     const bool flipSide = m_options.askSwitch("flipSide", false);
 
     ifacemap interfaceMap;
-#   pragma omp for
     for (gsBoxTopology::const_iiterator it = iFaces.begin();
          it != iFaces.end(); ++it )
     {
@@ -857,7 +854,6 @@ void gsExprAssembler<T>::assembleIfc(const ifContainer & iFaces, expr... args)
         }
     }
 
-}//omp parallel
     m_matrix.makeCompressed();
 }
 
