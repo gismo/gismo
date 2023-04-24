05c0b2d85440745d0d68a80c6a673385104c6ecd
Angelos Mantzaflaris <angelos.mantzaflaris@inria.fr>
2023-04-24 17:27:13 +0200

    improve initmat

--- src/gsAssembler/gsPoissonAssembler.hpp
+++ src/gsAssembler/gsPoissonAssembler.hpp
@@ -83,58 +83,81 @@ void gsPoissonAssembler<T>::assemble()
 template <class T>
 void gsPoissonAssembler<T>::initMatrix()
 {
-    //gsSparseEntries<T> entries;
+    const gsDofMapper & rowMap = m_system.rowMapper(0);
+    GISMO_ASSERT( &rowMap == &m_system.colMapper(0), "Error");
+    std::vector< std::map<index_t,bool> > colv(rowMap.freeSize());//m_system.matrix().cols()
+
 #pragma omp parallel
 {
     gsMatrix<index_t> actives;
     gsMatrix<T> cpt;
+    //gsSparseEntries<T> entries;
 
 #ifdef _OPENMP
     const int tid = omp_get_thread_num();
     const int nt  = omp_get_num_threads();
+#   pragma omp for
 #endif
-
     for (size_t np = 0; np < m_pde_ptr->domain().nPatches(); ++np)
     {
         // Initialize domain element iterator -- using unknown 0
         const gsBasis<T> & basis = m_bases[0][np];
+
         typename gsBasis<T>::domainIter domIt = basis.makeDomainIterator();
 
         // Start iteration over elements
 #ifdef _OPENMP
         for ( domIt->next(tid); domIt->good(); domIt->next(nt) )
+//#       pragma omp critical(localMat)
 #else
         for (; domIt->good(); domIt->next() )
 #endif
         {
             cpt = domIt->center;
             actives = basis.active(cpt);
-            const index_t numActive = actives.rows();
-            const gsDofMapper & rowMap = m_system.rowMapper(0);
+            m_system.mapColIndices(actives, np, actives);
 
-            GISMO_ASSERT( &rowMap == &m_system.colMapper(0), "Error");
+            const index_t numActive = actives.rows();
+
+
             GISMO_ASSERT( m_system.matrix().cols() == m_system.rhs().rows(), "gsSparseSystem is not allocated");
 
-            for (index_t i = 0; i != numActive; ++i)
+            for (index_t i = 0; i < numActive; ++i)
+                //for (index_t i = 0; i != sz; ++i)
             {
                 const int ii =  actives(i);
                 if ( rowMap.is_free_index(actives.at(i)) )
                 {
+#                   pragma omp parallel for
                     for (index_t j = 0; j < numActive; ++j)
                     {
                         const int jj = actives(j);
                         if ( rowMap.is_free_index(actives.at(j)) )
                         {
                             // Matrix is symmetric, we store only lower
-                            // triangular part
-#                           pragma omp critical(localMat)
-                            m_system.matrix().coeffRef(ii, jj) += 0.0;
+                           // triangular part
+                            auto it = colv[ii].find(jj);
+                            if (colv[ii].end()==it)
+                            {
+#                               pragma omp critical (colv_indices)
+//                            m_system.matrix().coeffRef(ii, jj) += 0.0;
+                            //entries.add(ii,jj, 0.0 );
+                                colv[ii][jj] = true;
+                            }
                         }
                     }
                 }
             }
         }
     }//for patches
+
+#pragma omp barrier
+
+#   pragma omp parallel for
+    for (index_t c = 0; c!=(index_t)colv.size();++c)
+        for ( const auto & t : colv[c] )
+            m_system.matrix().coeffRef(c, t.first) = (T)(0);
+    
 }//omp parallel
 
     Base::finalize();
