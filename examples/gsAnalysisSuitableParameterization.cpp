24945e7ea367d2afce6c21700b58a952cfb61609
jiyess <jiye@mail.dlut.edu.cn>
2023-06-23 11:55:03 +0200

    first commit for cleaning up analysis-suitable parameterization, here I create this branch from stable (version 20230623), now I just put the example file here.

--- examples/gsAnalysisSuitableParameterization.cpp
+++ examples/gsAnalysisSuitableParameterization.cpp
@@ -1,6 +1,15 @@
-//
-// Created by 纪野 on 2022/10/2.
-//
+/** @file biharmonic_example.cpp
+
+    @brief Analysis-suitable parameterization example.
+
+    This file is part of the G+Smo library.
+
+    This Source Code Form is subject to the terms of the Mozilla Public
+    License, v. 2.0. If a copy of the MPL was not distributed with this
+    file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+    Author(s): Ye Ji
+*/
 
 #include <gismo.h>
 #include <gsHLBFGS/gsHLBFGS.h>
