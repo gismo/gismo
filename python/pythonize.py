58a2d3553bb6b0a83f26b6736e2c35bd1ea4a82c
Felix Scholz <felix.scholz@tafsm.org>
2023-10-25 14:59:10 +0200

    Start pythonizations. Enable conversion of gsMatrix to numpy array as member of gsMatrix.

---
+++ python/pythonize.py
@@ -0,0 +1,30 @@
+""""
+    @file Pythonizations for cppyy bindings
+
+    @brief Improves the usability of the Python bindings
+
+    This file is part of the G+Smo library.
+
+    This Source Code Form is subject to the terms of the Mozilla Public
+    License, v. 2.0. If a copy of the MPL was not distributed with this
+    file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+    Author(s): F. Scholz
+"""
+
+# Pythonizations for the gismo namespace
+def pythonize_gismo(klass, name):
+    # Enable conversion to numpy
+    if name.startswith("gsMatrix<double"):
+        def tonumpy(self):
+            import numpy as np
+            rows = self.rows()
+            cols = self.cols()
+            numpyarray = np.frombuffer(self.data(), dtype=np.double, count=rows*cols).reshape(rows, cols)
+            return numpyarray
+        try:
+            #klass.__call__ = klass.__[dict]['operator()']
+            setattr(klass, "tonumpy", tonumpy)
+            #klass.tonumpy = tonumpy
+        except KeyError:
+            pass
