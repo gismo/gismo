0df35f0587ff4b6a43b140edcd3de4679b129d8d
Veronika <travnikova@cats.rwth-aachen.de>
2024-04-04 14:11:17 +0200

    Add Stokes example

---
+++ examples/poisson2Dirichlet_example.cpp
@@ -0,0 +1,225 @@
+/** @file poisson2_example.cpp
+
+    @brief Tutorial on how to use expression assembler to solve the Poisson equation
+
+    This file is part of the G+Smo library.
+
+    This Source Code Form is subject to the terms of the Mozilla Public
+    License, v. 2.0. If a copy of the MPL was not distributed with this
+    file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+    Author(s): A. Mantzaflaris
+*/
+
+//! [Include namespace]
+#include <gismo.h>
+
+using namespace gismo;
+//! [Include namespace]
+
+int main(int argc, char *argv[])
+{
+    //! [Parse command line]
+    bool plot = false;
+    index_t numRefine  = 5;
+    index_t numElevate = 0;
+    bool last = false;
+    std::string fn("pde/poisson2d_bvp.xml");
+
+    gsCmdLine cmd("Tutorial on solving a Poisson problem.");
+    cmd.addInt( "e", "degreeElevation",
+                "Number of degree elevation steps to perform before solving (0: equalize degree in all directions)", numElevate );
+    cmd.addInt( "r", "uniformRefine", "Number of Uniform h-refinement loops",  numRefine );
+    cmd.addString( "f", "file", "Input XML file", fn );
+    cmd.addSwitch("last", "Solve solely for the last level of h-refinement", last);
+    cmd.addSwitch("plot", "Create a ParaView visualization file with the solution", plot);
+
+    try { cmd.getValues(argc,argv); } catch (int rv) { return rv; }
+    //! [Parse command line]
+
+    //! [Read input file]
+
+    gsFileData<> fd(fn);
+    gsInfo << "Loaded file "<< fd.lastPath() <<"\n";
+
+    gsMultiPatch<> mp;
+    fd.getId(0, mp); // id=0: Multipatch domain
+
+    gsFunctionExpr<> f;
+    fd.getId(1, f); // id=1: source function
+    gsInfo<<"Source function "<< f << "\n";
+
+    gsBoundaryConditions<> bc;
+    fd.getId(2, bc); // id=2: boundary conditions
+    bc.setGeoMap(mp);
+    gsInfo<<"Boundary conditions:\n"<< bc <<"\n";
+
+    gsFunctionExpr<> ms;
+    fd.getId(3, ms); // id=3: reference solution
+
+    gsOptionList Aopt;
+    fd.getId(4, Aopt); // id=4: assembler options
+
+    //! [Read input file]
+
+    //! [Refinement]
+    gsMultiBasis<> dbasis(mp, true);//true: poly-splines (not NURBS)
+
+    // Elevate and p-refine the basis to order p + numElevate
+    // where p is the highest degree in the bases
+    dbasis.setDegree( dbasis.maxCwiseDegree() + numElevate);
+
+    // h-refine each basis
+    if (last)
+    {
+        for (int r =0; r < numRefine; ++r)
+            dbasis.uniformRefine();
+        numRefine = 0;
+    }
+
+    gsInfo << "Patches: "<< mp.nPatches() <<", degree: "<< dbasis.minCwiseDegree() <<"\n";
+#ifdef _OPENMP
+    gsInfo<< "Available threads: "<< omp_get_max_threads() <<"\n";
+#endif
+    //! [Refinement]
+
+    //! [Problem setup]
+    gsExprAssembler<> A(1,1);
+    A.setOptions(Aopt);
+
+    gsInfo<<"Active options:\n"<< A.options() <<"\n";
+
+    typedef gsExprAssembler<>::geometryMap geometryMap;
+    typedef gsExprAssembler<>::variable    variable;
+    typedef gsExprAssembler<>::space       space;
+    typedef gsExprAssembler<>::solution    solution;
+
+    // Elements used for numerical integration
+    A.setIntegrationElements(dbasis);
+    gsExprEvaluator<> ev(A);
+
+    // Set the geometry map
+    geometryMap G = A.getMap(mp);
+
+    // Set the discretization space
+    space u = A.getSpace(dbasis);
+
+    // Set the source term
+    auto ff = A.getCoeff(f, G);
+
+    // Recover manufactured solution
+    auto u_ex = ev.getVariable(ms, G);
+
+    // Solution vector and solution variable
+    gsMatrix<> solVector;
+    solution u_sol = A.getSolution(u, solVector);
+
+    //! [Problem setup]
+
+    //! [Solver loop]
+    gsSparseSolver<>::CGDiagonal solver;
+
+    gsVector<> l2err(numRefine+1), h1err(numRefine+1);
+    gsInfo<< "(dot1=assembled, dot2=solved, dot3=got_error)\n"
+        "\nDoFs: ";
+    double setup_time(0), ma_time(0), slv_time(0), err_time(0);
+    gsStopwatch timer;
+    for (int r=0; r<=numRefine; ++r)
+    {
+        dbasis.uniformRefine();
+
+//        u.setup(bc, dirichlet::interpolation, 0);
+        u.setup(bc, dirichlet::l2Projection, 0);
+
+        // Initialize the system
+        A.initSystem();
+        setup_time += timer.stop();
+
+        gsInfo<< A.numDofs() <<std::flush;
+
+        timer.restart();
+        // Compute the system matrix and right-hand side
+        A.assemble(
+            igrad(u, G) * igrad(u, G).tr() * meas(G) //matrix
+            ,
+            u * ff * meas(G) //rhs vector
+            );
+
+        ma_time += timer.stop();
+
+        // gsDebugVar(A.matrix().toDense());
+        // gsDebugVar(A.rhs().transpose()   );
+
+        gsInfo<< "." <<std::flush;// Assemblying done
+
+        timer.restart();
+        solver.compute( A.matrix() );
+        solVector = solver.solve(A.rhs());
+        slv_time += timer.stop();
+
+        gsInfo<< "." <<std::flush; // Linear solving done
+
+        // omp_set_dynamic(0);     // Explicitly disable dynamic teams
+        // omp_set_num_threads(1); // Use these threads for later parallel regions
+
+        timer.restart();
+        l2err[r]= math::sqrt( ev.integral( (u_ex - u_sol).sqNorm() * meas(G) ) );
+        
+        h1err[r]= l2err[r] +
+            math::sqrt(ev.integral( ( igrad(u_ex) - igrad(u_sol,G) ).sqNorm() * meas(G) ));
+        err_time += timer.stop();
+        gsInfo<< ". " <<std::flush; // Error computations done
+
+    } //for loop
+
+    //! [Solver loop]
+
+    timer.stop();
+    gsInfo<<"\n\nTotal time: "<< setup_time+ma_time+slv_time+err_time <<"\n";
+    gsInfo<<"     Setup: "<< setup_time <<"\n";
+    gsInfo<<"  Assembly: "<< ma_time    <<"\n";
+    gsInfo<<"   Solving: "<< slv_time   <<"\n";
+    gsInfo<<"     Norms: "<< err_time   <<"\n";
+
+    //! [Error and convergence rates]
+    gsInfo<< "\nL2 error: "<<std::scientific<<std::setprecision(3)<<l2err.transpose()<<"\n";
+    gsInfo<< "H1 error: "<<std::scientific<<h1err.transpose()<<"\n";
+
+    if (!last && numRefine>0)
+    {
+        gsInfo<< "\nEoC (L2): " << std::fixed<<std::setprecision(2)
+              <<  ( l2err.head(numRefine).array()  /
+                   l2err.tail(numRefine).array() ).log().transpose() / std::log(2.0)
+                   <<"\n";
+
+        gsInfo<<   "EoC (H1): "<< std::fixed<<std::setprecision(2)
+              <<( h1err.head(numRefine).array() /
+                  h1err.tail(numRefine).array() ).log().transpose() / std::log(2.0) <<"\n";
+    }
+    //! [Error and convergence rates]
+
+    //! [Export visualization in ParaView]
+    if (plot)
+    {
+        gsInfo<<"Plotting in Paraview...\n";
+
+        gsParaviewCollection collection("ParaviewOutput/solution", &ev);
+        collection.options().setSwitch("plotElements", true);
+        collection.options().setInt("plotElements.resolution", 16);
+        collection.newTimeStep(&mp);
+        collection.addField(u_sol,"numerical solution");
+        collection.addField(u_ex, "exact solution");
+        collection.saveTimeStep();
+        collection.save();
+
+
+        gsFileManager::open("ParaviewOutput/solution.pvd");
+    }
+    else
+        gsInfo << "Done. No output created, re-run with --plot to get a ParaView "
+                  "file containing the solution.\n";
+    //! [Export visualization in ParaView]
+
+    return EXIT_SUCCESS;
+
+}// end main
---
+++ examples/stokes2_example.cpp
@@ -0,0 +1,295 @@
+/** @file stokes2_example.cpp
+
+    @brief Tutorial on how to use expression assembler to solve the Stokes equation in 2D
+
+    This file is part of the G+Smo library.
+
+    This Source Code Form is subject to the terms of the Mozilla Public
+    License, v. 2.0. If a copy of the MPL was not distributed with this
+    file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+    Author(s): V. Travnikova
+*/
+
+//! [Include namespace]
+#include <gismo.h>
+
+using namespace gismo;
+//! [Include namespace]
+
+int main(int argc, char *argv[])
+{
+    // field IDs
+    constexpr index_t PRESSURE_ID = 0;
+    constexpr index_t VELOCITY_ID = 1;
+    // field dimensions
+    constexpr index_t PRESSURE_DIM = 1;
+    // number of solution and test spaces
+    //constexpr index_t NUM_TRIAL = 2;
+    //constexpr index_t NUM_TEST = 2;
+
+    // Setup values for timing
+    double setup_time(0), assembly_time_ls(0), solving_time_ls(0),
+      plotting_time(0);
+    gsStopwatch timer;
+    timer.restart();
+
+    //! [Parse command line]
+    bool plot = false;
+    bool last = false;
+    bool compute_error{false};
+    std::string file_name("pde/stokes_fullCircle.xml");
+
+    gsCmdLine cmd("Tutorial on solving a two-dimensional Stokes problem.");
+    cmd.addString( "f", "file", "Input XML file", file_name );
+    cmd.addSwitch("last", "Solve solely for the last level of h-refinement", last);
+    cmd.addSwitch("plot", "Create a ParaView visualization file with the solution", plot);
+    cmd.addSwitch("compute-error",
+                "Evaluate the error with respect to the analytical solution "
+                "(evaluation with default options and default file required)",
+                compute_error);
+
+    
+    // Material constants
+    real_t viscosity{1e-3};
+    //real_t density{1e3};
+    //cmd.addReal("v", "visc", "Viscosity", viscosity);
+    
+
+    // Mesh options
+    index_t numElevate = 0;
+    cmd.addInt("e", "degreeElevate", "Number of uniform degree elevations",
+             numElevate);
+    index_t numRefine = 0;
+    cmd.addInt("r", "uniformRefine", "Number of uniform h-refinement loops",
+             numRefine);
+
+    //! [Parse command line]
+    try { cmd.getValues(argc,argv); } catch (int rv) { return rv; }
+
+    //! [Read input file]
+
+    gsFileData<> file_data(file_name);
+    gsInfo << "Loaded file "<< file_data.lastPath() <<"\n";
+
+    gsMultiPatch<> multi_patch;
+    file_data.getId(0, multi_patch); // id=0: Multipatch domain
+
+    //gsFunctionExpr<> source_function_expression;
+    //file_data.getId(1, source_function_expression); // id=1: source function
+    //gsInfo<<"Source function "<< source_function_expression << "\n";
+
+    gsBoundaryConditions<> boundary_conditions;
+    file_data.getId(2, boundary_conditions); // id=2: boundary conditions
+    boundary_conditions.setGeoMap(multi_patch);
+    gsInfo<<"Boundary conditions:\n"<< boundary_conditions <<"\n";
+
+    //gsFunctionExpr<> ref_sol_expression;
+    //file_data.getId(3, ref_sol_expression); // id=3: reference solution
+
+    gsOptionList assembler_options;
+    file_data.getId(4, assembler_options); // id=4: assembler options
+
+    //! Define function bases for velocity and pressure
+    gsMultiBasis<> function_basis_velocity(multi_patch, true);//true: poly-splines (not NURBS)
+    gsMultiBasis<> function_basis_pressure(multi_patch, true);//true: poly-splines (not NURBS)
+
+    // Elevate the degree and increase the degree one 
+    // additional time for the velocity to obtain Taylor-Hood elements
+    function_basis_velocity.setDegree( function_basis_velocity.maxCwiseDegree() + numElevate + 1);
+    function_basis_pressure.setDegree( function_basis_pressure.maxCwiseDegree() + numElevate);
+
+    const int geometric_dimension = multi_patch.geoDim();
+
+    // h-refine each basis
+    for (int r =0; r < numRefine; ++r) {
+        function_basis_velocity.uniformRefine();
+        function_basis_pressure.uniformRefine();
+    }
+
+    // Output information
+    gsInfo << "Summary Velocity:" << std::endl
+           << "Patches:" << multi_patch.nPatches() 
+           << ", \nMin-degree: " << function_basis_velocity.minCwiseDegree()
+           << ", \nMax-degree: " << function_basis_velocity.maxCwiseDegree()
+           << std::endl << std::endl;
+    gsInfo << "Summary Pressure:" << std::endl
+           << "Patches:" << multi_patch.nPatches() 
+           << ", \nMin-degree: " << function_basis_pressure.minCwiseDegree()
+           << ", \nMax-degree: " << function_basis_pressure.maxCwiseDegree()
+           << std::endl << std::endl;
+    
+#ifdef _OPENMP
+    gsInfo<< "Available threads: "<< omp_get_max_threads() <<"\n";
+#endif
+
+    //! [Problem setup]
+    gsExprAssembler<> expression_assembler(2,2);
+    expression_assembler.setOptions(assembler_options);
+
+    gsInfo<<"Active options:\n"<< expression_assembler.options() <<"\n";
+
+    typedef gsExprAssembler<>::geometryMap geometryMap;
+    typedef gsExprAssembler<>::variable    variable;
+    typedef gsExprAssembler<>::space       space;
+    typedef gsExprAssembler<>::solution    solution;
+
+    // Elements used for numerical integration
+    expression_assembler.setIntegrationElements(function_basis_velocity);
+    gsExprEvaluator<> ev(expression_assembler);
+
+    // Set the geometry map
+    geometryMap geometry_map = expression_assembler.getMap(multi_patch);
+
+    // Set the discretization spaces
+    space velocity_trial_space = expression_assembler.getSpace(
+        function_basis_velocity, geometric_dimension, VELOCITY_ID
+        );
+    space pressure_trial_space = expression_assembler.getSpace(
+        function_basis_pressure, PRESSURE_DIM, PRESSURE_ID
+        );
+    
+
+    // Set the source term
+    //auto source_function = expression_assembler.getCoeff(source_function_expression, geometry_map);
+
+    // Solution vector and solution variables
+    gsMatrix<> solution_vector;
+    solution pressure_sol_expression = 
+        expression_assembler.getSolution(pressure_trial_space, solution_vector);
+    solution velocity_sol_expression = 
+        expression_assembler.getSolution(velocity_trial_space, solution_vector);
+
+    // Set Dirichlet BCs for velocity
+    velocity_trial_space.setup(boundary_conditions, dirichlet::l2Projection, 0);
+    // Initialize interfaces for pressure field?
+
+    // Initialize the system
+    expression_assembler.initSystem();
+    setup_time += timer.stop();
+
+    gsInfo << "Number of degrees of freedom : " << expression_assembler.numDofs()
+         << std::endl;
+    gsInfo << "Number of blocks in the system matrix : "
+         << expression_assembler.numBlocks() << std::endl;
+    
+    
+    // Assembly
+    gsInfo << "Starting assembly of linear system ..." << std::flush;
+    timer.restart();
+    
+    // Compute the system matrix and right-hand side
+    // Compute the system matrix and right-hand side
+    auto phys_jacobian = ijac(velocity_trial_space, geometry_map);
+    auto bilin_conti = pressure_trial_space * idiv(velocity_trial_space, geometry_map).tr() * meas(geometry_map);
+    auto bilin_press = -idiv(velocity_trial_space, geometry_map) * pressure_trial_space.tr() * meas(geometry_map);
+    auto bilin_mu_1 = viscosity * (phys_jacobian.cwisetr() % phys_jacobian.tr()) *
+                    meas(geometry_map);
+    auto bilin_mu_2 =
+      viscosity * (phys_jacobian % phys_jacobian.tr()) * meas(geometry_map);
+
+    expression_assembler.assemble(bilin_conti, bilin_press, bilin_mu_1, bilin_mu_2);
+
+    assembly_time_ls += timer.stop();
+    gsInfo << "\t\tFinished" << std::endl;
+
+    assembly_time_ls += timer.stop();
+
+    // gsDebugVar(A.matrix().toDense());
+    // gsDebugVar(A.rhs().transpose()   );
+
+    gsInfo << "\t\tFinished" << std::endl;
+
+  ///////////////////
+  // Linear Solver //
+  ///////////////////
+
+  gsInfo << "Solving the linear system of equations ..." << std::flush;
+  timer.restart();
+
+  const auto& system_matrix = expression_assembler.matrix();
+  // TEST: boundary conditions -- 20240403
+//   gsInfo << (system_matrix).rows() << "\n";
+//   gsInfo << (system_matrix).cols() << "\n";
+//     auto bdrytest = multi_patch.getBoundary(1) << "\n";
+//     gsInfo << multi_patch.boundaries <<"\n";
+//    gsInfo << multi_patch.patch(0).boundary(1) << "\n";
+
+// create multipatch geometry of full circle
+// gsInfo << multi_patch << "\n";
+// gsTensorBSpline<2,real_t> geomTest = static_cast<gsTensorBSpline<2,real_t>&>(multi_patch.patch(0));
+// geomTest.rotate(M_PI_2);
+// multi_patch.addPatch(geomTest);
+// geomTest.rotate(M_PI_2);
+// multi_patch.addPatch(geomTest);
+// geomTest.rotate(M_PI_2);
+// multi_patch.addPatch(geomTest);
+// multi_patch.computeTopology();
+// gsWriteParaview(multi_patch, "testmp", 1000);
+// gsWrite(multi_patch, "wholeGeom");
+
+////////////////////////////////////////////////////////////////////////////////////
+
+  const auto& rhs_vector = expression_assembler.rhs();
+
+  // Initialize linear solver
+  //gsSparseSolver<>::BiCGSTABILUT solver;
+  gsSparseSolver<>::BiCGSTABIdentity solver;
+  //gsSparseSolver<>::GMRES solver;
+  //gsSparseSolver<>::LeastSquaresCG solver;
+  solver.compute(system_matrix);
+  solution_vector = solver.solve(rhs_vector);
+
+  solving_time_ls += timer.stop();
+  gsInfo << "\tFinished" << std::endl;
+
+  // Export and visualization
+  gsExprEvaluator<> expression_evaluator(expression_assembler);
+  gsInfo << "\nStarting the paraview export ..." << std::flush;
+  timer.restart();
+
+  gsParaviewCollection collection("ParaviewOutput/solutionfull_elim_intf",
+                                    &expression_evaluator);
+  collection.options().setSwitch("plotElements", true);
+  collection.options().setInt("plotElements.resolution", 1000);
+  collection.newTimeStep(&multi_patch);
+  collection.addField(pressure_sol_expression, "pressure");
+  collection.addField(velocity_sol_expression, "velocity");
+  collection.saveTimeStep();
+  collection.save();
+
+  plotting_time += timer.stop();
+  gsInfo << "\tFinished" << std::endl;
+  
+  gsInfo << "Starting the xml export ..." << std::flush;
+
+  // Export pressure
+  gsMatrix<> solution_pressure;
+  gsFileData<> output_pressure;
+    pressure_sol_expression.extractFull(
+        solution_pressure);  // patch-wise solution with BCs
+    output_pressure << solution_pressure;
+    output_pressure.save("pressure_field.xml");
+
+    // Export velocity
+    gsMatrix<> solution_velocity;
+    gsFileData<> output;
+    velocity_sol_expression.extractFull(
+        solution_velocity);  // patch-wise solution with BCs
+    output << solution_velocity;
+    output.save("velocity_field.xml");
+
+// User output infor timings
+  gsInfo << "\n\nTotal time: "
+         << setup_time + assembly_time_ls + solving_time_ls + plotting_time
+         << std::endl;
+  gsInfo << "                       Setup: " << setup_time << std::endl;
+  gsInfo << "      Assembly Linear System: " << assembly_time_ls << std::endl;
+  gsInfo << "       Solving Linear System: " << solving_time_ls << std::endl;
+  gsInfo << "                    Plotting: " << plotting_time << std::endl
+         << std::flush;
+        
+
+    return EXIT_SUCCESS;
+
+}// end main
