787ad1d42c8314195e10ade709f14db32e42b094
Stefan Takacs <stefan.takacs@numa.uni-linz.ac.at>
2024-05-14 09:39:36 +0200

    fix paraview output

--- examples/stokes_ieti_example.cpp
+++ examples/stokes_ieti_example.cpp
@@ -1,7 +1,7 @@
 /** @file stokes_ieti_example.cpp
 
     @brief Provides an example for the IETI solver for a Stokes problem
-    
+
     This example file illustrates how to set up a IETI solver for systems
     of PDEs.
 
@@ -44,7 +44,7 @@ int main(int argc, char *argv[])
     index_t maxIterations = 300;
     std::string fn;
     bool plot = false;
-    
+
     gsCmdLine cmd("Solves the Stokes system with an isogeometric discretization using an isogeometric tearing and interconnecting (IETI) solver.");
     cmd.addInt   ("",  "SplitPatches",          "Split every patch that many times in 2^d patches", splitPatches);
     cmd.addInt   ("r", "Refinements",           "Number of uniform h-refinement steps to perform before solving", refinements);
@@ -71,7 +71,7 @@ int main(int argc, char *argv[])
     /******************* Define geometry ********************/
 
     gsInfo << "Define geometry... " << std::flush;
-    
+
     gsMultiPatch<>::uPtr mpPtr = gsReadFile<>(geometry);
     if (!mpPtr)
     {
@@ -79,7 +79,7 @@ int main(int argc, char *argv[])
         return EXIT_FAILURE;
     }
     gsMultiPatch<>& mp = *mpPtr;
-    
+
     GISMO_ENSURE (mp.geoDim() == 2, "Expect 2D geometry.");
     const std::size_t dim = mp.geoDim();
 
@@ -98,7 +98,7 @@ int main(int argc, char *argv[])
     // Some functions
     gsConstantFunction<> zero( 0.0, mp.geoDim() );
     gsFunctionExpr<> inflow( "sin(pi*(2+y)/4)", dim );
-    
+
     // Boundary conditions
     std::vector<gsBoundaryConditions<>> bc;
     bc.resize(dim+1);
@@ -123,18 +123,18 @@ int main(int argc, char *argv[])
     }
     for (std::size_t r=0; r<dim+1; ++r)
         bc[r].setGeoMap(mp);
-    
+
     gsInfo << "done.\n";
-     
-    
+
+
     /************ Setup bases and adjust degree *************/
     std::vector<gsMultiBasis<>> mb;
     for (std::size_t r=0; r<dim+1; ++r)
         mb.emplace_back(mp,true);
     const index_t nPatches = mp.nPatches();
-    
+
     gsInfo << "Setup bases and adjust degree (Taylor-Hood)... " << std::flush;
-    
+
     for (std::size_t r=0; r<dim; ++r)
     {
         for (index_t k=0; k<nPatches; ++k)
@@ -144,16 +144,16 @@ int main(int argc, char *argv[])
         for (index_t k=0; k<nPatches; ++k)
             mb[r][k].reduceContinuity(1);
     }
-    
+
     for (index_t k=0; k<nPatches; ++k)
         mb[dim][k].setDegreePreservingMultiplicity(degree);
     for ( index_t i=0; i<refinements; ++i )
         mb[dim].uniformRefine();
-    
+
     gsInfo << "done.\n";
-    
+
     /********* Setup assembler and assemble matrix **********/
-    
+
     gsInfo << "Setup assembler and assemble matrix for patch... " << std::flush;
 
     std::vector<gsIetiMapper<>> ietiMapper;
@@ -169,8 +169,8 @@ int main(int argc, char *argv[])
         gsExprAssembler<>::space u = assembler.getSpace(mb[r]);
         u.setup(bc[r], dirichlet::interpolation, continuity);
         ietiMapper[r].init( mb[r], u.mapper(), u.fixedPart() );
-    }    
-        
+    }
+
     // Compute the jump matrices (Fully redundant, exclude corners)
     for (size_t r=0; r<dim+1; ++r)
         ietiMapper[r].computeJumpMatrices(true, true);
@@ -205,7 +205,7 @@ int main(int argc, char *argv[])
     for (index_t k=0; k<nPatches; ++k)
     {
         gsInfo << "[" << k << "] " << std::flush;
-        
+
         // We use the local variants of everything
         gsMultiPatch<>                      mp_local = mp[k];
         std::vector<gsMultiBasis<>>         mb_local;
@@ -223,12 +223,12 @@ int main(int argc, char *argv[])
         typedef gsExprAssembler<>::variable    variable;
         typedef gsExprAssembler<>::space       space;
         typedef gsExprAssembler<>::solution    solution;
-        
+
         gsExprAssembler<> assembler(ietiMapper.size(),ietiMapper.size());
         assembler.setIntegrationElements(mb_local[0]);
         gsExprEvaluator<> ev(assembler);
         geometryMap G = assembler.getMap(mp_local);
-        
+
         // Velocity space
         std::vector<std::remove_cv<space>::type> v;
         for (std::size_t r=0; r<dim; ++r)
@@ -237,35 +237,35 @@ int main(int argc, char *argv[])
             v[r].setup(bc_local[r], dirichlet::interpolation, 0);
             ietiMapper[r].initFeSpace(v[r],k);
         }
-        
+
         // Presure space
         space p = assembler.getSpace(mb_local[dim],1,dim);
         p.setup(bc_local[dim], dirichlet::interpolation, 0);
         ietiMapper[dim].initFeSpace(p,k);
-        
+
         assembler.initSystem();
-        
+
         for (std::size_t r=0; r<dim; ++r)
         {
             assembler.assemble( igrad(v[r], G)   * igrad(v[r], G).tr()   * meas(G) );
             assembler.assemble( igrad(v[r],G)[r] * p.tr()                * meas(G) );
             assembler.assemble( p                * igrad(v[r],G)[r].tr() * meas(G) );
         }
-        
+
         // Fetch data
         gsSparseMatrix<>                 localMatrix   = assembler.matrix();
         gsMatrix<>                       localRhs      = assembler.rhs();
         gsSparseMatrix<real_t,RowMajor>  jumpMatrix    = combinedJumpMatrix(ietiMapper, k);
-        
+
         // For preconditioning, we only use the Poisson part
         {
             const index_t sz = ietiMapper[0].jumpMatrix(k).cols() + ietiMapper[1].jumpMatrix(k).cols();
             gsSparseMatrix<> velocityJumpMatrix = jumpMatrix.block(0,0,jumpMatrix.rows(),sz);
             gsSparseMatrix<> poissonMatrix = localMatrix.block(0,0,sz,sz);
-            
+
             std::vector<index_t> skeletonDofs      = combinedSkeletonDofs(ietiMapper,k);
             gsScaledDirichletPrec<>::Blocks blocks = gsScaledDirichletPrec<>::matrixBlocks( poissonMatrix, skeletonDofs );
-            
+
             prec.addSubdomain(
                 gsScaledDirichletPrec<>::restrictJumpMatrix( velocityJumpMatrix, skeletonDofs ).moveToPtr(),
                 gsScaledDirichletPrec<>::schurComplement( blocks, makeSparseCholeskySolver(blocks.A11) )
@@ -275,7 +275,7 @@ int main(int argc, char *argv[])
         // Collecting the primal DoFs
         std::vector<index_t> primalDofIndices                 = combinedPrimalDofIndices(ietiMapper,k);
         std::vector<gsSparseVector<real_t>> primalConstraints = combinedPrimalConstraints(ietiMapper,k);
-    
+
         // This function writes back to jumpMatrix, localMatrix, and localRhs,
         // so it must be called after prec.addSubdomain().
         primal.handleConstraints(primalConstraints, primalDofIndices, jumpMatrix, localMatrix, localRhs);
@@ -283,10 +283,10 @@ int main(int argc, char *argv[])
         // Register local problem to ieti system
         ieti.addSubdomain(jumpMatrix.moveToPtr(),
                           makeMatrixOp(localMatrix.moveToPtr()),
-                          give(localRhs), 
+                          give(localRhs),
                           makeSparseLUSolver(localMatrix)
                          );
-        
+
     } // End of local assemblers
 
     // Add the primal problem
@@ -298,7 +298,7 @@ int main(int argc, char *argv[])
             makeMatrixOp(primal.localMatrix().moveToPtr()),
             give(primal.localRhs()),
             localSolver
-        ); 
+        );
     }
     gsInfo << "done.\n";
 
@@ -309,12 +309,12 @@ int main(int argc, char *argv[])
 
     // Tell the preconditioner to set up the scaling
     prec.setupMultiplicityScaling();
-        
+
     gsInfo << "done.\n    Setup CG solver and solve... \n" << std::flush;
-    
+
     // Solve for Lagrange multipliers
     gsMatrix<> lambda;
-    lambda.setRandom( ieti.nLagrangeMultipliers(), 1 );    
+    lambda.setRandom( ieti.nLagrangeMultipliers(), 1 );
     gsConjugateGradient<> solver( ieti.schurComplement(), prec.preconditioner() );
     solver.setCalcEigenvalues(true);
     solver.setOptions(cmd.getGroup("Solver"));
@@ -347,7 +347,7 @@ int main(int argc, char *argv[])
         solutions.push_back( ietiMapper[r].constructGlobalSolutionFromLocalSolutions(decomposeSolution(ietiMapper, solutionPatches, r)));
 
     gsInfo << "done.\nDone.\n";
-    
+
     /******************** Print end Exit ********************/
     if (!fn.empty())
     {
@@ -360,25 +360,24 @@ int main(int argc, char *argv[])
         fd.save(fn);
         gsInfo << "Write solution to file " << fn << ".\n";
     }
-    
+
     if (plot)
-    {        
-        gsInfo << "Write Paraview data to files ieti_velocity_x.pvd, ieti_velocity_y.pvd and ieti_pressure.pvd.\n";
-        
-        const char* filenames[] = {"ieti_velocity_x", "ieti_velocity_y", "ieti_pressure"};
-	GISMO_ENSURE(ietiMapper.size()==(std::size_t)3, "Dimension mismatch.");
+    {
         for (std::size_t r=0; r<ietiMapper.size(); ++r)
         {
+            std::string filename(r==dim?"ieti_pressure":"ieti_velocity_x");
+            if (r<dim) filename[14]+=r;
+            gsInfo << "Write Paraview data to file \"" << filename << ".pvd\".\n";
             gsConstantFunction<> zero(0.0, 2);
-            gsPoissonAssembler<> assembler(mp, mb[r], bc[r], zero, dirichlet::elimination, iFace::glue);
+            gsPoissonAssembler<> assembler(mp, mb[r], bc[r], zero, dirichlet::elimination, r<dim ? iFace::glue : iFace::none);
             assembler.options().setInt("DirichletValues", dirichlet::interpolation);
             assembler.computeDirichletDofs();
             gsMultiPatch<> mpsol;
             assembler.constructSolution(solutions[r], mpsol);
-            gsWriteParaview<>( gsField<>( assembler.patches(), mpsol ), filenames[r], 1000);
+            gsWriteParaview<>( gsField<>( assembler.patches(), mpsol ), filename.c_str(), 1000);
         }
     }
-    
+
     if (!plot&&fn.empty())
     {
         gsInfo << "No output created, re-run with --plot to get a ParaView "
@@ -409,7 +408,7 @@ typename std::iterator_traits<Iterator>::value_type blockDiagonal( Iterator begi
         rows += sm.rows();
         cols += sm.cols();
     }
-    sparseMatrix result(rows, cols);    
+    sparseMatrix result(rows, cols);
     result.setFrom(se);
     return result;
 }
@@ -479,7 +478,7 @@ std::vector<index_t> combinedSkeletonDofs(const Container& ietiMapper, index_t k
 
 template<class Container>
 std::vector<gsMatrix<>> decomposeSolution(const Container& ietiMapper, const std::vector<gsMatrix<>>& solution, index_t component)
-{   
+{
     std::vector<gsMatrix<>> result;
     result.reserve(solution.size());
     for (std::size_t k=0; k<solution.size(); ++k)
@@ -492,4 +491,3 @@ std::vector<gsMatrix<>> decomposeSolution(const Container& ietiMapper, const std
     }
     return result;
 }
-
