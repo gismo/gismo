44f6189ff5a3cf25a33d60bc962f6ed66ec0e4f2
Angelos Mantzaflaris <angelos.mantzaflaris@inria.fr>
2024-06-13 07:36:10 +0200

    add move sem

--- src/gsNurbs/gsTensorBSplineBasis.h
+++ src/gsNurbs/gsTensorBSplineBasis.h
@@ -88,6 +88,13 @@ public:
 #if !EIGEN_HAS_RVALUE_REFERENCES
     gsTensorBSplineBasis & operator=(gsTensorBSplineBasis other)
     { gsTensorBSplineBasis::swap(other); return *this;}
+#else
+    gsTensorBSplineBasis(gsTensorBSplineBasis && other) : Base(give(other)) { }
+    gsTensorBSplineBasis(const gsTensorBSplineBasis &  other) : Base(other) { }
+    gsTensorBSplineBasis & operator=(gsTensorBSplineBasis&& other)
+    { return (gsTensorBSplineBasis &)Base::operator=(give(other)); }
+    gsTensorBSplineBasis & operator=(const gsTensorBSplineBasis& other)
+    { return (gsTensorBSplineBasis &)Base::operator=(other); }
 #endif
     
     /**
