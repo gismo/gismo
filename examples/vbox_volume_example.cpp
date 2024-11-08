33ccfc7a0b8758af42e43143ee95802e8b2060c8
sofimp23 <sofia.imperatore@gmail.com>
2024-11-08 13:33:23 +0100

    vboxes volume general formulation

--- examples/vbox_volume_example.cpp
+++ examples/vbox_volume_example.cpp
@@ -22,9 +22,11 @@ using namespace gismo;
 int main(int argc, char *argv[])
 {
     std::string fn;
-    fn = "../filedata/vboxdata/posB_lpsp_fit.xml";
+    fn = "../filedata/surfaces/simple.xml";
+    index_t addRef = 1;
 
     gsCmdLine cmd("Input: .xml file with gsTensorProductBspline geometry.");
+    cmd.addInt("r", "ref", "Uniform refinement", addRef);
     cmd.addString("m", "measuredata", "Input measure data", fn);
 
     try { cmd.getValues(argc,argv); } catch (int rv) { return rv; }
@@ -36,49 +38,80 @@ int main(int argc, char *argv[])
         return 0;
     }
 
-
-    gsMatrix<> pltCoefs;
-
     gsFileData<>  data( fn );
     data.save();
 
-
-    gsInfo<< "Measurement file contains "<< data.numTags() <<" gsTensorBSpline objects.\n";
+    gsInfo<< "Measurement file contains "<< data.numTags() <<" Geometric objects.\n";
     memory::unique_ptr<gsGeometry<> > surf = data.getFirst< gsGeometry<> >();
-    
-    // resetting the coordinate system for the integral computation;
-    surf->coefs().col(2).array() *= -1;
-    surf->coefs().col(2).array() += 1.; // to be scaled properly, accordin to the the data magnitude
-
-    surf->coefs().col(0).array() *= -1;
-    surf->coefs().col(0).array() += 1; // to be scaled properly, accordin to the the data magnitude
-    
     gsInfo << *surf << "\n";
+    
     gsWriteParaview(*surf, "surf", 10000, false, true);
 
-    gsFileData<> tmp;
-    tmp << *surf;
-    tmp.dump("posB");
+    gsMultiBasis<> mb(surf->basis()); // get the basis
 
-    pltCoefs = surf->coefs().transpose();
-    gsWriteParaviewPoints(pltCoefs, "surf_net");
-
-    gsMultiBasis<> mb(*surf);
-    gsExprEvaluator<> ev;
-    ev.setIntegrationElements(mb);
+    // Method 1.
+    gsExprEvaluator<> ev1;
+    ev1.setIntegrationElements(mb);
 
     typedef typename gsExprEvaluator<>::geometryMap geometryMap;
-    geometryMap Gmeasure = ev.getMap(*surf);
+    geometryMap Gmeasure1 = ev1.getMap(*surf);
 
     gsVector<> ez_vec(3);
     ez_vec<<0,0,1;
     gsConstantFunction<> ez_fun(ez_vec,2);
-    auto ez = ev.getVariable(ez_fun);
+    auto ez = ev1.getVariable(ez_fun);
 
-    real_t volume = ev.integral(Gmeasure.tr() * ez * meas(Gmeasure));
-    // real_t volume = ev.integral(Gmeasure.norm()*meas(Gmeasure));
+    real_t volume1 = ev1.integral(Gmeasure1.tr() * ez * meas(Gmeasure1));
 
-    gsInfo<< "Volume = " << volume << "\n";
+    gsInfo<< "Volume_1 = " << volume1 << "\n\n";
+    gsInfo<< "---------------------\n\n";
+
+
+    // Method 2.
+    //1. Take the projection in (x,y). This will be a planar B-spline, lets call it G.
+    //2. Take a spline function only being the z-part. This is a scalar B-spline, let's call it f.
+    //3. Use this planar domain as your geometry map in the gsExprEvaluator
+    //4. Compute the integral:    integral( f * meas(G) ) .
+    //   To get better accuracy for the computed integral, you can uniformly refine the G if needed
+
+    real_t volume2 = 0.;
+
+    // 1. Take the projection in (x,y). This will be a planar B-spline, lets call it G.
+    gsMatrix<> dcoefs(surf->coefs().rows(), 2);
+    dcoefs.col(0) = surf->coefs().col(0);
+    dcoefs.col(1) = surf->coefs().col(1);
+
+    // assemble integration domain
+    auto domainG = surf->basis().makeGeometry(dcoefs);
+
+    //2. Take a spline function only being the z-part. This is a scalar B-spline, let's call it f.
+    gsMatrix<> zcoefs(surf->coefs().rows(), 1);
+    zcoefs = surf->coefs().col(2);
+
+    // define integrand
+    auto f = surf->basis().makeGeometry(zcoefs);
+
+
+    gsMatrix<> domainCoefs = domainG->coefs().transpose();
+    gsWriteParaviewPoints(domainCoefs, "domainCoefs");
+    gsWriteParaview(*domainG, "domain");
+            
+    
+    //3. Use this planar domain as your geometry map in the gsExprEvaluator
+    gsExprEvaluator<> ev;
+    ev.setIntegrationElements(mb);
+    typedef typename gsExprEvaluator<>::geometryMap geometryMap;
+    geometryMap Gmeasure = ev.getMap(*domainG);
+
+    //4. Compute the integral:    integral( f * meas(G) ) .
+    auto fz = ev.getVariable(*f);
+    volume2 = ev.integral(fz * meas(Gmeasure));
+    gsInfo<< "Volume_2 = " << volume2 << "\n";
+
+
+     // If we consider the ingral to be always the [0,1]^2 domain;
+    real_t volume3 = ev.integral(fz);
+    gsInfo<< "Volume_3 = " << volume2 << "\n";
 
     return EXIT_SUCCESS;
 }
