085ed9103612384ed255a3aeb118b3e9c1d8cb13
Hugo Verhelst <hugoverhelst1995@gmail.com>
2024-11-21 11:01:17 +0100

    update embedding example

--- examples/embeddedCurve_assembly_example.cpp
+++ examples/embeddedCurve_assembly_example.cpp
@@ -310,37 +310,39 @@ public:
 
         if (_S.source().targetDim() == 2)
         {
-            gsMatrix<Scalar> res = gsMatrix<Scalar>::Zero();
+            res.setZero();
             return res;
         }
         else if (_S.source().targetDim()==3)
         {
-        const index_t A = _u.cardinality()/_u.dim();
-        bGrads = _u.data().values[1].col(k);
-        theta  = _C.data().values[0].col(k); 
-        sJac  = Spatch.deriv(theta);
-        sJac.transposeInPlace();
-        sJac.resize(_S.source().domainDim(),_S.source().targetDim());
-        curve_normal_expr<Scalar> cnv(_S,_C);
-        cnvMat = cnv.eval(k);
-        const Scalar measure = cnvMat.norm();
-        cnvMat.normalize();
+            const index_t A = _u.cardinality()/_u.dim();
+            bGrads = _u.data().values[1].col(k);
+            theta  = _C.data().values[0].col(k);
+            sJac  = Spatch.deriv(theta);
+            sJac.transposeInPlace();
+            sJac.resize(_S.source().domainDim(),_S.source().targetDim());
+            curve_normal_expr<Scalar> cnv(_S,_C);
+            cnvMat = cnv.eval(k);
+            const Scalar measure = cnvMat.norm();
+            cnvMat.normalize();
 
-        for (index_t d = 0; d!= cols(); ++d) // for all basis function components
-        {
-            const short_t s = d*A;
-            for (index_t j = 0; j!= A; ++j) // for all active basis functions
+            for (index_t d = 0; d!= cols(); ++d) // for all basis function components
             {
-                //first variation of non-unit normal vector (divided by measure)
-                var =  (vecFun(d, bGrads.at(2*j  )).cross(sJac.col3d(1))
-                      - vecFun(d, bGrads.at(2*j+1)).cross(sJac.col3d(0)))/measure;
+                const short_t s = d*A;
+                for (index_t j = 0; j!= A; ++j) // for all active basis functions
+                {
+                    //first variation of non-unit normal vector (divided by measure)
+                    var =  (vecFun(d, bGrads.at(2*j  )).cross(sJac.col3d(1))
+                        - vecFun(d, bGrads.at(2*j+1)).cross(sJac.col3d(0)))/measure;
 
-                //first variation of unit normal vector
-                res.row(s+j) = (var - ((cnvMat.col3d(0)*var.tr()) * cnvMat.col3d(0))).tr();
+                    //first variation of unit normal vector
+                    res.row(s+j) = (var - ((cnvMat.col3d(0)*var.transpose()) * cnvMat.col3d(0))).transpose();
+                }
             }
+            return res;
         }
-        return res;
-        }
+        else
+            GISMO_ERROR("The target dimension of the surface must be 2 or 3, but is "<<_S.source().targetDim());
     }
 
     index_t rows() const { return 1; }
@@ -474,25 +476,31 @@ public:
 
         curve_tangent_expr<Scalar> ctv(_S,_C);
         ctvMat = ctv.eval(k);
+        gsDebug<<"1\n";
         curve_normal_expr<Scalar> cnv(_S,_C);
         cnvMat = cnv.eval(k);
+        gsDebug<<"2\n";
         curve_tangent_var1_expr<E> ctv_var1(_u,_S,_C);
-        ctv1Mat = ctv_var1.eval(k);
+        ctv1Mat = ctv_var1.eval(k).transpose();
+        gsDebug<<"3\n";
         curve_normal_var1_expr<E> cnv_var1(_u,_S,_C);
-        cnv1Mat = cnv_var1.eval(k);
+        cnv1Mat = cnv_var1.eval(k).transpose();
+        gsDebug<<"4\n";
         curve_binormal_expr<Scalar> cbv(_S,_C);
         cbvMat = cbv.eval(k);
+        gsDebug<<"5\n";
         const Scalar measure = cbvMat.norm();
         cbvMat.normalize();
 
+        // Loop over rows of (ctv1Mat or cnv1Mat) == cardinality of space
         for (index_t i = 0; i!= _u.cardinality(); ++i)
         {
             //first variation of non-unit binormal vector (divided by measure)
-            var.row(i) = (cnv1Mat.row3d(i).tr().cross(ctvMat.col3d(0)) +
-                         cnvMat.col3d(0).cross(ctv1Mat.row3d(i).tr()))/measure;
+            var.row(i) = (cnv1Mat.col3d(i).cross(ctvMat.col3d(0)) +
+                         cnvMat.col3d(0).cross(ctv1Mat.col3d(i)))/measure;
 
             //first variation of unit binormal vector
-            res.row(i) = (var.row(i).tr() - (cbvMat.col3d(0)*var.row(i)) *cbvMat.col3d(0)).tr();            
+            res.row(i) = (var.row(i).transpose() - (cbvMat.col3d(0)*var.row(i)) *cbvMat.col3d(0)).transpose();
         }
         return res;
     }
@@ -503,15 +511,12 @@ public:
 
     void parse(gsExprHelper<Scalar> & evList) const
     {
-        evList.add(_u);
-        _u.data().flags |= NEED_ACTIVE | NEED_GRAD;
-
-        evList.add(_S);
-        _S.data().flags |= NEED_DERIV;
-
-        evList.add(_C);
-        _C.data().flags |= NEED_VALUE | NEED_DERIV;
-
+        // For pre-computation of expressions for curve_normal_expr, curve_tangent_expr, curve_tangent_var1_expr, curve_normal_var1_expr, curve_binormal_expr
+        ctv(_S,_C).parse(evList);
+        cnv(_S,_C).parse(evList);
+        ctv_var1(_u,_S,_C).parse(evList);
+        cnv_var1(_u,_S,_C).parse(evList);
+        cbv(_S,_C).parse(evList);
     }
 
     const gsFeSpace<Scalar> & rowVar() const { return _u.rowVar(); }
@@ -558,22 +563,78 @@ T curveCoordinate(const gsGeometry<T> & geometry, const T & value, const short_t
 // template <class T>
 // T findRoot();
 
+template <class T = real_t>
+class cnv_fun : public gsFunction<T>
+{
+public:
+    // Constructor
+    cnv_fun(const gsMultiPatch<T> & mp_surf,
+            const gsMultiPatch<T> & mp_curve,
+            const gsVector<T> & gamma)
+    :
+    m_surf(mp_surf),
+    m_curve(mp_curve),
+    m_gamma(gamma)
+    {
+        GISMO_ASSERT(m_surf.nPatches()==1, "The surface must be a single patch");
+        GISMO_ASSERT(m_curve.nPatches()==1, "The curve must be a single patch");
+        GISMO_ASSERT(m_gamma.rows()==1, "The gamma vector must be  scalar coordinates");
+    }
+
+    short_t domainDim() const override { return m_surf.patch(0).coefs().rows()*m_surf.patch(0).coefs().cols(); }
+
+    short_t targetDim() const override { return 3; } // = gismo::expr::cnv().rows()
+
+    void eval_into(const gsMatrix<T>  & u, gsMatrix<T> &result) const override
+    {
+        // We need to evaluate the cnv expression at point gamma.
+        result.resize(targetDim(),u.cols());
+
+        for (index_t k = 0; k!= u.cols(); ++k)
+        {
+            // Deform the surface with u.col(k)
+            gsMultiPatch<T> surf_def = m_surf;
+            surf_def.patch(0).coefs() += u.reshapeCol(k,m_surf.patch(0).coefs().rows(),m_surf.patch(0).coefs().cols());
+
+            // Construct expression evaluator
+            gsExprEvaluator<T> exprEvaluator;
+            auto G_curve = exprEvaluator.getMap(m_curve);
+            auto G_surf  = exprEvaluator.getMap(surf_def);
+
+            // Evaluate expression
+            result.col(k) = exprEvaluator.eval( gismo::expr::cnv(G_surf, G_curve), m_gamma);
+        }
+    }
+
+protected:
+
+    const gsMultiPatch<T> & m_surf;
+    const gsMultiPatch<T> & m_curve;
+    const gsVector<T>     & m_gamma;
+
+};
+
+
 int main(int argc, char *argv[])
 {
 
     gsInfo<<"To do list:\n";
-    gsInfo<<"- Parse expressions as compositions, i.e. pre-compute the coordinates of the curve and pre-compute the surface at this point.\n";
-    gsInfo<<"- Parse dependent expressions (e.g., the use of curve_normal_expr in curve_tangent_expr in the curve_binormal_expr)\n";
-    gsInfo<<"- Make the expressions work for 2D and 3D surfaces using enable_if\n";
+    gsInfo<<"- [ ] Parse expressions as compositions, i.e. pre-compute the coordinates of the curve and pre-compute the surface at this point.\n";
+    gsInfo<<"- [ ] Parse dependent expressions (e.g., the use of curve_normal_expr in curve_tangent_expr in the curve_binormal_expr)\n";
+    gsInfo<<"- [ ] Make the expressions work for 2D and 3D surfaces using enable_if\n";
 
     // bool plot = false; // If set to true, paraview file is generated and launched on exit
     // bool trim = false; // If set to true, trim/merge operations are displayed
     // bool intersect = false; // If set to true, intersection example is displayed
 
+    bool surface = false;
+    // std::string filename;
+
     gsCmdLine cmd("TODO");
     // cmd.addSwitch("plot", "Plot result in ParaView format", plot);
     // cmd.addSwitch("trim", "Basic trim/merge operations", trim);
-    // cmd.addSwitch("intersect", "Intersection operations", intersect);
+    cmd.addSwitch("surface", "Model the geometry as a surface", surface);
+    // cmd.addString("file", "Path to the file to be read", filename);
 
     try { cmd.getValues(argc,argv); } catch (int rv) { return rv; }
 
@@ -582,7 +643,6 @@ int main(int argc, char *argv[])
     gsMatrix<> coefs_c(5, 2); //ξ,η;..
     coefs_c << 0, 0,
              0.2618, 0.053,
-
              0.5, 0.5,
              0.738, 0.9465,
              1,1;
@@ -595,6 +655,9 @@ int main(int argc, char *argv[])
     // 2D->2D
     gsMultiPatch<> mp_surf;
     mp_surf.addPatch(gsNurbsCreator<>::BSplineSquare());
+    if (surface)
+        mp_surf.embed(3);
+
     gsInfo<<"The surface (R^"<<mp_surf.patch(0).domainDim()<<" -> R^"<<mp_surf.patch(0).targetDim()<<") is:\n"<<mp_surf.patch(0)<<"\n";
     // 1D->2D
     gsMultiPatch<> mp_curve;
@@ -602,27 +665,62 @@ int main(int argc, char *argv[])
     gsInfo<<"The curve (R^"<<mp_curve.patch(0).domainDim()<<" -> R^"<<mp_curve.patch(0).targetDim()<<") is:\n"<<mp_curve.patch(0)<<"\n";
 
     gsMultiBasis<> basis_curve(mp_curve);
+    gsMultiBasis<> basis_surf(mp_surf);
 
-    gsExprEvaluator<> ev;
-    ev.setIntegrationElements(basis_curve); // ONLY NEEDED WHEN CALLING ev.integral
-    auto G_curve = ev.getMap(mp_curve);
-    auto G_surf = ev.getMap(mp_surf);
+    // Declare the expression assembler
+    gsExprAssembler<> exprAssembler(1,1);
+    // Set integration elements
+    exprAssembler.setIntegrationElements(basis_curve); // maybe surf?
+    // Register expressions inside assembler
+    auto G_curve = exprAssembler.getMap(mp_curve);
+    auto G_surf  = exprAssembler.getMap(mp_surf);
+    // auto u_curve = exprAssembler.getSpace(basis_curve); // not needed...
+    auto u_surf  = exprAssembler.getSpace(basis_surf,mp_surf.geoDim());
+    // Declare expression evaluator from assembler
+    gsExprEvaluator<> exprEvaluator(exprAssembler);
 
     gsVector<> gamma(1);
     gamma << 0.5;
     gsInfo<<"Values at point "<<gamma<<"\n";
-    gsInfo << "theta           = "<<ev.eval( G_curve, gamma ).transpose() << "\n";
-    gsInfo << "normal vector   = "<<ev.eval( gismo::expr::cnv(G_surf, G_curve), gamma ).transpose() << "\n";
-    gsInfo << "tangent vector  = "<<ev.eval( gismo::expr::ctv(G_surf, G_curve), gamma ).transpose() << "\n";
-    gsInfo << "binormal vector = "<<ev.eval( gismo::expr::cbv(G_surf, G_curve), gamma ).transpose() << "\n";
+    gsInfo << "theta           = "<<exprEvaluator.eval( G_curve, gamma ).transpose() << "\n";
+    gsInfo << "normal vector   = "<<exprEvaluator.eval( gismo::expr::cnv(G_surf, G_curve), gamma ).transpose() << "\n";
+    gsInfo << "tangent vector  = "<<exprEvaluator.eval( gismo::expr::ctv(G_surf, G_curve), gamma ).transpose() << "\n";
+    gsInfo << "binormal vector = "<<exprEvaluator.eval( gismo::expr::cbv(G_surf, G_curve), gamma ).transpose() << "\n";
+    // gsInfo << "variation of tv = "<<exprEvaluator.eval( gismo::expr::ctv_var1(u_surf,G_surf, G_curve), gamma ) << "\n";
+    // gsInfo << "variation of nv = "<<exprEvaluator.eval( gismo::expr::cnv_var1(u_surf,G_surf, G_curve), gamma ) << "\n";
+    // BREAKS:
+    // gsInfo << "variation of bv = "<<exprEvaluator.eval( gismo::expr::cbv_var1(u_surf,G_surf, G_curve), gamma ) << "\n";
 
-    //gsDebugVar(ev.eval(jac(u),point));
 
-    // gsMatrix<> normal = ev.eval( sn(G_surf) );
-    // gsMatrix<> tangent = ev.eval( tv(G_curve) );
+    /*
+        What we have is `cnv(G_surf,G_curve)`. G_curve is fixed, so it does not change with the displacement of the surface.
+        Hence, cnv(G_surf | G_curve). We want to find the variation of cnv w.r.t. G_surf or the displacement vector.
+
+        How do we validate this?
+        - We write a "function" that computes cnv given a 'perturbation vector'.
+        - If we take the finite difference of that "function", we should get the same result as the variation of cnv.
+
+        How to do this in G+Smo?
+        - We write a gsFunction-derived class that takes the role of "function" above
+        - Inside this gsFunction, we implement eval_into() that computes the normal vector given a perturbation vector
+        - Given eval_into, gsFunction implements finite difference automatically in deriv_into()
+     */
+    GISMO_ENSURE(mp_surf.nPatches()==1, "The surface must be a single patch");
+    gsMatrix<> u(mp_surf.patch(0).coefs().rows()*mp_surf.patch(0).coefs().cols(),1);
+    u.setZero();
+
+    // We test our derivative in gamma
+    cnv_fun<real_t> cnvFun(mp_surf,mp_curve,gamma);
+    gsDebug<<cnvFun.eval(u); // should give the same result as "normal vector   = "<<exprEvaluator.eval( gismo::expr::cnv(G_surf, G_curve), gamma ).transpose() << "\n";
+    gsDebug<<cnvFun.deriv(u); // should give the same result as "variation of nv = "<<exprEvaluator.eval( gismo::expr::cnv_var1(u_surf,G_surf, G_curve), gamma ) << "\n";
+
+    //gsDebugVar(exprEvaluator.eval(jac(u),point));
+
+    // gsMatrix<> normal = exprEvaluator.eval( sn(G_surf) );
+    // gsMatrix<> tangent = exprEvaluator.eval( tv(G_curve) );
     // gsMatrix<> binormal = normal.cross(tangent);
 
-    // ev.integral()
+    // exprEvaluator.integral()
 
 
     gsWriteParaview(mp_surf,  "surf",  1000, true,  false); // multi-patch, file name, number of points, mesh, control net
