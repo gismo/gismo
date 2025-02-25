4d1b114264a45f897f52b8f316d7992b5b07679a
Angelos Mantzaflaris <filiatra@gmail.com>
2025-02-25 11:45:29 +0100

    minor

--- src/gsDomain/gsDomainIterator.h
+++ src/gsDomain/gsDomainIterator.h
@@ -8,7 +8,7 @@
     License, v. 2.0. If a copy of the MPL was not distributed with this
     file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-    Author(s): C. Hofreither
+    Author(s): C. Hofreither, H.M. Verhelst, A. Mantzaflaris
 */
 
 #pragma once
