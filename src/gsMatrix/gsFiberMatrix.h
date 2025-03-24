eece69a973826e7df5cdde5fad1191c1472557ad
Mattia Matucci <matucci.mattia@gmail.com>
2025-03-24 09:12:57 +0100

    Add operator[]

--- src/gsMatrix/gsFiberMatrix.h
+++ src/gsMatrix/gsFiberMatrix.h
@@ -39,6 +39,9 @@ public:
         typedef typename Fiber::InnerIterator Base;
         iterator() = default;
         iterator(const gsFiberMatrix & fm, index_t j) : Base(fm.fiber(j)) { }
+
+        T operator[](index_t i)
+        { return this->operator+(i).value(); }
     };
 
     struct RowBlockXpr;
