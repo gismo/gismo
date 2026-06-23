176ea5274ae422dc02dabcc151651cec552376d1
Angelos Mantzaflaris <filiatra@gmail.com>
2026-06-23 11:08:45 +0200

    replace patch with patchIndex

--- src/gsAssembler/gsExprEvaluator.h
+++ src/gsAssembler/gsExprEvaluator.h
@@ -466,9 +466,9 @@ T gsExprEvaluator<T>::compute_impl(const expr::_expr<E> & expr)
 
         for ( auto & elem : m_exprdata->domain().allElements() )
         {
-            if (changeQuadrature || QuPatch!=elem.patch())
+            if (changeQuadrature || QuPatch!=elem.patchIndex())
             {
-                QuPatch = elem.patch();
+                QuPatch = elem.patchIndex();
                 // get Degree of the domain
                 QuRule = gsQuadrature::getPtr(*m_exprdata->domain().subdomain(QuPatch), m_options);
             }
