39dca92b649f5081f11a7148b84807662db55f3d
Hugo Verhelst <hugoverhelst1995@gmail.com>
2026-09-04 10:33:13 +0200

    fix windows runner

--- src/gsIO/gsParaview_.cpp
+++ src/gsIO/gsParaview_.cpp
@@ -22,12 +22,12 @@ namespace gismo
 STRUCT_TEMPLATE_INST gsParaview<real_t>;
 
 // Explicit instantiation of template member functions for gsHBox (d=2,3 only)
-template void gsParaview<real_t>::write<2>(const gsHBox<2,real_t> &, const std::string &) const;
-template void gsParaview<real_t>::write<3>(const gsHBox<3,real_t> &, const std::string &) const;
+TEMPLATE_INST void gsParaview<real_t>::write<2>(const gsHBox<2,real_t> &, const std::string &) const;
+TEMPLATE_INST void gsParaview<real_t>::write<3>(const gsHBox<3,real_t> &, const std::string &) const;
 
 // Explicit instantiation of template member functions for gsHBoxContainer (d=2,3 only)
-template void gsParaview<real_t>::write<2>(const gsHBoxContainer<2,real_t> &, const std::string &) const;
-template void gsParaview<real_t>::write<3>(const gsHBoxContainer<3,real_t> &, const std::string &) const;
+TEMPLATE_INST void gsParaview<real_t>::write<2>(const gsHBoxContainer<2,real_t> &, const std::string &) const;
+TEMPLATE_INST void gsParaview<real_t>::write<3>(const gsHBoxContainer<3,real_t> &, const std::string &) const;
 
 #ifdef GISMO_WITH_PYBIND11
 
